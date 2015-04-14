4f50fa0bb9a30a84a67b104f3d580740d2070cfb
John Cappiello <jcappiello@healthverity.com>
2015-04-14 18:13:24 -0400

    clean up the files first

--- icd9/update.sh
+++ icd9/update.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 echo "Loading Diagnosis Codes..."
-psql -U $1 -d hvdb -c "COPY diagnosis_codes (code, long_description, short_description) FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER ',')" < $2
+sed -e 's/^M/\n/g' $2 | awk -F',' '{print $1 "," $2 "," $3}' | psql -U $1 -d hvdb -c "COPY diagnosis_codes (code, long_description, short_description) FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER ',')" 
 
 echo "Loading Procedure Codes..."
-psql -U $1 -d hvdb -c "COPY procedure_codes (code, long_description, short_description) FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER ',')" < $3
+sed -e 's/^M/\n/g' $3 | awk -F',' '{print $1 "," $2 "," $3}' | psql -U $1 -d hvdb -c "COPY procedure_codes (code, long_description, short_description) FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER ',')" 
 
 
 
