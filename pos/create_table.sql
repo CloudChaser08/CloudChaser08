a1c8b8ccdecab23813f780224f8520cb73309787
John Cappiello <jcappiello@healthverity.com>
2015-04-14 15:38:22 -0400

    type

--- pos/create_table.sql
+++ pos/create_table.sql
@@ -3,6 +3,7 @@
 CREATE TABLE place_of_service (
   id SMALLINT,
   pos_group VARCHAR(64),
+  pos_type  VARCHAR(64),
   description VARCHAR(1024),
   start_date DATE,
   end_date DATE,
--- pos/update.sh
+++ pos/update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Loading Place of Service..."
-psql -U $1 -d hvdb -c "COPY place_of_service (id, pos_group, description, start_date, end_date, other) FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER ',')"
+psql -U $1 -d hvdb -c "COPY place_of_service (id, pos_group, pos_type, description, start_date, end_date, other) FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER ',')" < $2
 
 
 
