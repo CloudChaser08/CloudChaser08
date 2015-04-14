c23593477d8488cc2a6c88e00da44a48c24aedd3
John Cappiello <jcappiello@healthverity.com>
2015-04-14 18:30:25 -0400

    excel encodes like windows

--- icd9/update.sh
+++ icd9/update.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 echo "Loading Diagnosis Codes..."
-sed -e 's/,/\n/g' $2 | psql -U $1 -d hvdb -c "COPY diagnosis_codes (code, long_description, short_description) FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER ',')" 
+sed -e 's/,/\n/g' $2 | psql -U $1 -d hvdb -c "COPY diagnosis_codes (code, long_description, short_description) FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER ',', encoding 'windows-1251')" 
 
 echo "Loading Procedure Codes..."
-sed -e 's/,/\n/g' $3 | psql -U $1 -d hvdb -c "COPY procedure_codes (code, long_description, short_description) FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER ',')" 
+sed -e 's/,/\n/g' $3 | psql -U $1 -d hvdb -c "COPY procedure_codes (code, long_description, short_description) FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER ',', encoding 'windows-1251')" 
 
 
 
