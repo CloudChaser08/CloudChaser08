d4bc3bb8b7ee13cfebda2fbd14426d87cc5023f2
John Cappiello <jcappiello@healthverity.com>
2015-04-15 00:06:06 -0400

    fixup procedure

--- ccs/create_table.sql
+++ ccs/create_table.sql
@@ -14,13 +14,11 @@ CREATE TABLE ccs_multi_diagnosis_codes (
 );
 
 CREATE TABLE ccs_multi_procedure_codes (
-  diagnosis_code VARCHAR(6),
+  procedure_code VARCHAR(6),
   level_1    VARCHAR(32),
   level_1_label VARCHAR(255),
   level_2    VARCHAR(32),
   level_2_label VARCHAR(255),
   level_3    VARCHAR(32),
-  level_3_label VARCHAR(255),
-  level_4    VARCHAR(32),
-  level_4_label VARCHAR(255)
+  level_3_label VARCHAR(255)
 );
