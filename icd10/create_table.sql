a10d1ef29e876175bf73796947b69d5f315ccbc4
John Cappiello <jcappiello@healthverity.com>
2015-04-21 13:44:28 -0400

    order

--- icd10/create_table.sql
+++ icd10/create_table.sql
@@ -1,7 +1,7 @@
 
 
 CREATE TABLE icd10_diagnosis_codes (
-  order VARCHAR(5),
+  ordernum VARCHAR(5),
   code VARCHAR(7),
   header CHAR(1),
   short_description VARCHAR(60),
@@ -10,7 +10,7 @@ CREATE TABLE icd10_diagnosis_codes (
 
 
 CREATE TABLE icd10_procedure_codes (
-  order VARCHAR(5),
+  ordernum VARCHAR(5),
   code VARCHAR(7),
   header CHAR(1),
   short_description VARCHAR(60),
