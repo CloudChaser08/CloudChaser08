5942d2f834737bf92d0efaf740b3a32baa98e198
John Cappiello <jcappiello@healthverity.com>
2015-05-20 10:30:48 -0400

    updated schema and copy routines to be more efficient

--- wxloc/copy.sql
+++ wxloc/copy.sql
@@ -1,4 +1,6 @@
 
 COPY weather_pings FROM 's3://healthverity/incoming/weatherchannel' 
 WITH CREDENTIALS :'AWS_CREDENTIALS'
-JSON as 's3://vhxwdata/jsonpaths';
+JSON as 's3://vhxwdata/jsonpaths'
+TIMEFORMAT AS 'epochmillisecs'
+ROUNDEC;
