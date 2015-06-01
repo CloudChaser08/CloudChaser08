9fbf71d86f5658f5d80167e1e21aba358a23dbbf
John Cappiello <jcappiello@healthverity.com>
2015-06-01 09:41:49 -0400

    all files, allow for those bad files

--- wxloc/copy.sql
+++ wxloc/copy.sql
@@ -1,8 +1,9 @@
 
-COPY weather_pings FROM 's3://salusv/provider/weatherchannel/2015/03'
+COPY weather_pings FROM 's3://salusv/provider/weatherchannel/'
 WITH CREDENTIALS :'AWS_CREDENTIALS'
 JSON as 's3://vhxwdata/jsonpaths'
 TIMEFORMAT AS 'epochmillisecs'
 ROUNDEC
 COMPUPDATE ON
+MAXERROR 20
 GZIP;
