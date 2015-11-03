af6912db5d19ba66950587a4f6d6747602a3c7c1
Jim Snavely <jsnavely@shell.aws.healthverity.com>
2015-11-03 20:06:23 +0000

    fix typo "-" ->  "_"

--- market-search/copy_market_search.sql
+++ market-search/copy_market_search.sql
@@ -1,4 +1,4 @@
-COPY market-search FROM 's3://salusv/provider/linked/marketplace/v1/medical/2015-11-02:19:47:25/patients.json'
+COPY market_search FROM 's3://salusv/provider/linked/marketplace/v1/medical/2015-11-02:19:47:25/patients.json'
 WITH CREDENTIALS :'AWS_CREDENTIALS'
 format as json 's3://healthveritydev/jsnavely/marketplace-search/marketplace-jsonpaths.json'
 MAXERROR 50
