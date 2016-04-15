50f96ff3919a736267fb7fce3a0e4bb8ef1ca4a1
Jim Snavely <jsnavely@shell.aws.healthverity.com>
2016-04-15 15:51:46 +0000

    normalized columns

--- emdeon/copy_claims_norm.sql
+++ emdeon/copy_claims_norm.sql
@@ -1,6 +1,9 @@
-COPY emdeon_claim_norm FROM 's3://salusv/provider/linked/marketplace/v1/medical/2016-04-04:11:50:02/emdeon_professional.avro/part-r-00000-6fc1937b-8f23-4a4e-8bd3-a82696276b18.avro'
+COPY emdeon_claim_norm FROM 's3://salusv/provider/linked/marketplace/v1/medical/2016-04-14:18:56:40/related/emdeon_professional.avro/part-r-00000-7520b479-699c-47a9-b6e3-3c2e1894d65a.avro'
 WITH CREDENTIALS :'AWS_CREDENTIALS'
 DATEFORMAT 'YYYYMMDD'
+ACCEPTANYDATE
+BLANKSASNULL
+EMPTYASNULL
 MAXERROR 100000
 COMPUPDATE ON
 format as avro 'auto';
