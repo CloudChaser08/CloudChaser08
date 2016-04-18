827aa6fdeab2dfe31bc05a77a2e10acf330e9a08
Jim Snavely <jsnavely@shell.aws.healthverity.com>
2016-04-18 13:38:39 +0000

    schema with new columns names

--- emdeon/copy_claims_norm.sql
+++ emdeon/copy_claims_norm.sql
@@ -1,4 +1,4 @@
-COPY emdeon_claim_norm FROM 's3://salusv/provider/linked/marketplace/v1/medical/2016-04-14:18:56:40/related/emdeon_professional.avro/part-r-00000-7520b479-699c-47a9-b6e3-3c2e1894d65a.avro'
+COPY emdeon_claim_norm FROM 's3://salusv/provider/linked/marketplace/v1/medical/2016-04-15:17:15:45/unrelated/emdeon_professional.avro/'
 WITH CREDENTIALS :'AWS_CREDENTIALS'
 DATEFORMAT 'YYYYMMDD'
 ACCEPTANYDATE
