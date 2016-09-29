c249a46d9819a2b561ade670c7cb950afb3ce183
Matt Usifer <musifer@healthverity.com>
2016-09-29 15:35:49 -0400

    Add hll unloading script, remove unnecessary cols

--- providers/quest/normalize.sql
+++ providers/quest/normalize.sql
@@ -9,20 +9,10 @@ INSERT INTO lab_common_model (
         date_service,
         date_specimen,
         loinc_code,
-        lab_id,
-        test_id,
-        test_number,
-        test_battery_local_id,
-        test_battery_std_id,
-        test_battery_name,
         test_ordered_local_id,
         test_ordered_std_id,
         test_ordered_name,
-        result,
         result_name,
-        result_unit_of_measure,
-        result_desc,
-        result_comments,
         diagnosis_code,
         diagnosis_code_qual
         )
@@ -41,27 +31,17 @@ SELECT TRIM(q.accn_id),                                                 --claim_
     END,                                                                --patient_year_of_birth
     mp.threeDigitZip,                                                   --patient_zip3
     mp.state,                                                           --patient_state
-    service.formatted,                                                                --date_service
-    collected.formatted,                                                                --date_specimen
+    service.formatted,                                                  --date_service
+    collected.formatted,                                                --date_specimen
     q.loinc_code,                                                       --loinc_code
-    null,                                                               --lab_id ** we don't have this yet
-    null,                                                               --test_id
-    null,                                                               --test_number
-    null,                                                               --test_battery_local_id
-    null,                                                               --test_battery_std_id
-    null,                                                               --test_battery_name
-    null,                                                               --test_ordered_local_id
-    null,                                                               --test_ordered_std_id
-    null,                                                               --test_ordered_name
-    null,                                                               --result
-    null,                                                               --result_name
-    null,                                                               --result_unit_of_measure
-    null,                                                               --result_desc
-    null,                                                               --result_comments
+    q.local_order_code,                                                 --test_ordered_local_id
+    q.standard_order_code,                                              --test_ordered_std_id
+    q.order_name,                                                       --test_ordered_name
+    q.result_name,                                                      --result_name
     split_part(UPPER(TRIM(replace(q.diagnosis_code,'.',''))),'^',n.n),  --diagnosis_code
     q.icd_codeset_ind                                                   --diagnosis_code_qual
 FROM transactional_raw q
-    LEFT JOIN matching_payload mp on q.accn_id = mp.claimid
+    LEFT JOIN matching_payload mp ON q.accn_id = mp.claimid
     LEFT JOIN dates service ON q.date_of_service = service.date
     LEFT JOIN dates collected ON q.date_collected = collected.date
     CROSS JOIN diagnosis_exploder n
---
+++ providers/quest/unload_hlls.sql
@@ -0,0 +1,1 @@
+UNLOAD(:select_for_hvid) TO :output_path credentials :credentials BZIP2 DELIMITER '|' NULL AS '' ALLOWOVERWRITE;
