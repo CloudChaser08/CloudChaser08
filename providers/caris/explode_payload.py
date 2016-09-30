e167a269419846cb4ede17ffa029a73bbcf9cbfc
Matt Usifer <musifer@healthverity.com>
2016-09-30 16:24:10 -0400

    Refactor exploder script

--- providers/caris/explode_payload.py
+++ providers/caris/explode_payload.py
@@ -1,10 +1,8 @@
 import sys
-import json 
+import json
 
 transactional_root = sys.argv[1]
 payload = sys.argv[2]
-
-
 
 # write out list of all patient_key values found in all transactional files
 patient_key_counts = {}
@@ -22,5 +20,3 @@ with open(payload, 'r') as payload_json, open('exploded.json', 'w') as output:
         for i in range(patient_key_counts.get(lineJson['hvJoinKey'], 0)):
             lineJson['pk'] = lineJson['hvJoinKey'] + '_' + str(i)
             output.write(json.dumps(lineJson) + "\n")
-
-# transactional_root="/Users/musifer/src/hv/dewey/providers/amazingcharts/data/transactional"
