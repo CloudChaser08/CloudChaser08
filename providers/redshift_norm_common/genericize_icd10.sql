9e5d9c49f426b786d667a68b80122cfb20887039
Ilia Fishbein <ifishbein@healthverity.com>
2016-10-05 13:35:52 -0400

    Privacy filtering: genericize diagnosis codes that may be to specific

---
+++ providers/redshift_norm_common/genericize_icd10.sql
@@ -0,0 +1,7 @@
+-- These codes are specific enough that along with other public fields they pose a
+-- re-identification risk, make them more generic
+-- Z68.41 - Z68.45 Body Mass Index 40 and over
+
+UPDATE :table_name
+SET :column_name=Z684
+WHERE regexp_count(:column_name, '^Z684[1-5]$') > 0;
---
+++ providers/redshift_norm_common/genericize_icd9.sql
@@ -0,0 +1,7 @@
+-- These codes are specific enough that along with other public fields they pose a
+-- re-identification risk, make them more generic
+-- V85.41 - V85.45 Body Mass Index 40 and over
+
+UPDATE :table_name
+SET :column_name=V854
+WHERE regexp_count(:column_name, '^V854[1-5]$') > 0;
