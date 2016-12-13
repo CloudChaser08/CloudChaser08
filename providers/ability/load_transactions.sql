c14ebb6a4091274f8f9f59647904c30bdca6e73b
Matt Usifer <musifer@healthverity.com>
2016-12-13 11:24:38 -0500

    Increase error on diagnosis load

--- providers/ability/load_transactions.sql
+++ providers/ability/load_transactions.sql
@@ -213,7 +213,7 @@ CREATE TABLE transactional_diagnosis_dupes (
         processdate         text ENCODE lzo
         ) DISTKEY(claimid) SORTKEY(claimid);
 
-COPY transactional_diagnosis_dupes FROM :diagnosis_path CREDENTIALS :credentials EMPTYASNULL ACCEPTINVCHARS IGNOREHEADER 1 MAXERROR 10 DELIMITER '|';
+COPY transactional_diagnosis_dupes FROM :diagnosis_path CREDENTIALS :credentials EMPTYASNULL ACCEPTINVCHARS IGNOREHEADER 1 MAXERROR 100 DELIMITER '|';
 
 DROP TABLE IF EXISTS transactional_diagnosis CASCADE;
 CREATE TABLE transactional_diagnosis (
