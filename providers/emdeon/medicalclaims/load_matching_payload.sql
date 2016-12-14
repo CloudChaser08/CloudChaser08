15f052ad6fa936d42b6c03cdf76221e62f9e8073
Ilia Fishbein <ifishbein@healthverity.com>
2016-12-14 15:25:41 -0500

    Updated payload loading script to match current expected payload format

--- providers/emdeon/medicalclaims/load_matching_payload.sql
+++ providers/emdeon/medicalclaims/load_matching_payload.sql
@@ -1,25 +1,27 @@
 -- Create a table for the matching payload data
 DROP TABLE IF EXISTS matching_payload_broken;
 CREATE TABLE matching_payload_broken (
-claimid varchar ENCODE lzo,
+claimId varchar ENCODE lzo,
 hvid text ENCODE lzo,
 parentid text ENCODE lzo,
-threeDigitZip char(3) ENCODE lzo) DISTKEY(claimid) SORTKEY(claimid);
+threeDigitZip char(3) ENCODE lzo)
+DISTKEY(claimId) SORTKEY(hvid);
 
 -- Load matching payload data into table
-copy matching_payload_broken from :matching_path credentials :credentials BZIP2 format as JSON 's3://healthveritydev/ifishbein/payloadpaths.json';
+copy matching_payload_broken from :matching_path credentials :credentials BZIP2 format as JSON 's3://healthveritydev/ifishbein/emdeon_new_payloadpaths.json';
 
 -- Create a table for the matching payload data
 DROP TABLE IF EXISTS matching_payload;
 CREATE TABLE matching_payload (
-claimid varchar ENCODE lzo,
+claimId varchar ENCODE lzo,
 hvid text ENCODE lzo,
-threeDigitZip char(3) ENCODE lzo) DISTKEY(claimid) SORTKEY(claimid);
+threeDigitZip char(3) ENCODE lzo)
+DISTKEY(claimId) SORTKEY(claimId);
 
 INSERT INTO matching_payload
 SELECT
 claimId,
-CASE WHEN par.parentid IS NOT NULL THEN par.parentid ELSE hvid END,
+COALESCE(mat.parentid, hvid),
 threeDigitZip
 FROM
-matching_payload_broken mat LEFT JOIN hvid_parentid par USING (hvid);
+matching_payload_broken mat;-- LEFT JOIN hvid_parentid par USING (hvid);
