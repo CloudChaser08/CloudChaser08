d6a5d0c9a15f8d5845d26c4658a4b43f78b31478
Ilia Fishbein <ifishbein@healthverity.com>
2017-01-03 09:24:37 -0500

    Refactored Emdeon DX normalization routine

--- providers/emdeon/medicalclaims/rsNormalizeEmdeonDX.py
+++ providers/emdeon/medicalclaims/rsNormalizeEmdeonDX.py
@@ -6,93 +6,123 @@ import time
 
 TODAY = time.strftime('%Y-%m-%d', time.localtime())
 S3_EMDEON_IN = 's3://salusv/incoming/medicalclaims/emdeon/'
-S3_EMDEON_OUT = 's3://salusv/processed/medicalclaims/emdeon/'
-S3_EMDEON_MPL = 's3://salusv/matching/payload/medicalclaims/emdeon/'
+S3_EMDEON_OUT = 's3://salusv/warehouse/text/medicalclaims/emdeon/'
+S3_EMDEON_MATCHING = 's3://salusv/matching/payload/medicalclaims/emdeon/'
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input_path', type=str)
-parser.add_argument('--matching_path', type=str)
-parser.add_argument('--output_path', type=str)
-parser.add_argument('--database', type=str, nargs='?')
+parser.add_argument('--date', type=str)
 parser.add_argument('--setid', type=str)
-parser.add_argument('--cluster_endpoint', type=str, nargs='?')
 parser.add_argument('--s3_credentials', type=str)
-parser.add_argument('--rs_user', type=str, nargs='?')
-parser.add_argument('--rs_password', type=str, nargs='?')
 parser.add_argument('--first_run', default=False, action='store_true')
+parser.add_argument('--debug', default=False, action='store_true')
 args = parser.parse_args()
 
-db = args.database if args.database else 'dev'
+def run_psql_script(script, variables=[]):
+    args = ['psql', '-f', script]
+    for var in variables:
+        args.append('-v')
+        args.append('{}={}'.format(var[0], var[1]))
+    subprocess.check_call(args)
 
-psql = ['psql', '-p', '5439']
-if args.cluster_endpoint:
-    psql.extend(['-h', args.cluster_endpoint])
-if args.rs_user:
-    psql.extend(['-U', args.rs_user, '-W'])
+def run_psql_query(query, return_output=False):
+    args = ['psql', '-c', query]
+    if return_output:
+        return subprocess.check_output(args)
+    subprocess.check_call(args)
+
+psql_scripts = []
+psql_variables = []
+def enqueue_psql_script(script, variables=[]):
+    global psql_scripts, psql_variables
+    psql_scripts.append(script)
+    psql_variables.append(variables)
+
+def execute_queue(debug=False):
+    global psql_scripts, psql_variables
+    if debug:
+        for i in xrange(len(psql_scripts)):
+            run_psql_script(psql_scripts[i], psql_variables[i])
+    else:
+        v = reduce(lambda x,y: x.extend(y), [])
+        with open('full_normalization_routine.sql', 'w') as fout:
+            subprocess.check_call(['cat'] + psql_scripts, stdout=fout)
+        run_psql_script('full_normalization_routine.sql', v)
 
 if args.first_run:
-    subprocess.call(' '.join(psql + [db, '<', 'user_defined_functions.sql']), shell=True)
-    subprocess.call(' '.join(psql + [db, '<', 'create_helper_tables.sql']), shell=True)
-    subprocess.call(' '.join(psql + [db, '<', '../../redshift_norm_common/zip3_to_state.sql']), shell=True)
-    subprocess.call(' '.join(psql + ['-v', 'credentials="\'' + args.s3_credentials + '\'"'] +
-        [db, '<', 'load_payer_mapping.sql']), shell=True)
+    enqueue_psql_script('user_defined_functions.sql')
+    enqueue_psql_script('create_helper_tables.sql')
+    enqueue_psql_script('../../redshift_norm_common/zip3_to_state.sql')
+    enqueue_psql_script('load_payer_mapping.sql', [
+        ['credentials', args.s3_credentials]
+    ])
 
     # Create a table for valid dates and their correct format
     min_date = '2012-01-01'
-    subprocess.call(' '.join(psql + [db, '<', '../../redshift_norm_common/prep_date_offset_table.sql']), shell=True)
+    run_psql_script('../../redshift_norm_common/prep_date_offset_table.sql')
     while True:
-        res1 = subprocess.check_output(['psql', '-c', 'SELECT count(*) FROM tmp;']);
-        res2 = subprocess.check_output(['psql', '-c', 'SELECT extract(\'days\' FROM (getdate() - \'' + min_date + '\'))::int;']);
+        res1 = run_psql_query('SELECT count(*) FROM tmp;', True)
+        res2 = run_psql_query('SELECT extract(\'days\' FROM (getdate() - \'' + min_date + '\'))::int;')
         rows = int(res1.split("\n")[2].lstrip().rstrip())
         target = int(res2.split("\n")[2].lstrip().rstrip())
         if rows > target + 1:
             break
         else:
-            subprocess.call(' '.join(psql + [db, '<', '../../redshift_norm_common/expand_date_offset_table.sql']), shell=True)
-    subprocess.call(' '.join(psql + ['-v', 'min_valid_date="\'' + min_date + '\'"'] +
-        [db, '<', '../../redshift_norm_common/create_date_formatting_table.sql']), shell=True)
+            run_psql_script('../../redshift_norm_common/expand_date_offset_table.sql')
+    run_psql_script('../../redshift_norm_common/create_date_formatting_table.sql', [
+        ['min_valid_date', min_date]
+    ])
 
-subprocess.call(' '.join(psql + ['-v', 'filename="\'' + args.setid + '\'"'] + 
-    ['-v', 'today="\'' + TODAY + '\'"'] +
-    ['-v', 'feedname="\'webmd medical claims\'"'] +
-    ['-v', 'vendor="\'webmd\'"'] +
-    [db, '<', '../../redshift_norm_common/medicalclaims_common_model.sql']), shell=True)
-subprocess.call(' '.join(psql + ['-v', 'input_path="\'' + args.input_path + '\'"'] +
-    ['-v', 'credentials="\'' + args.s3_credentials + '\'"'] +
-    [db, '<', 'load_transactions.sql']), shell=True)
-subprocess.call(' '.join(psql + ['-v', 'matching_path="\'' + args.matching_path + '\'"'] +
-    ['-v', 'credentials="\'' + args.s3_credentials + '\'"'] +
-    [db, '<', 'load_matching_payload.sql']), shell=True)
-subprocess.call(' '.join(psql + [db, '<', 'split_raw_transactions.sql']), shell=True)
-subprocess.call(' '.join(psql + [db, '<', 'normalize_professional_claims.sql']), shell=True)
-subprocess.call(' '.join(psql + [db, '<', 'normalize_institutional_claims.sql']), shell=True)
+enqueue_psql_script('../../redshift_norm_common/medicalclaims_common_model.sql', [
+    ['filename', args.setid],
+    ['today', TODAY],
+    ['feedname', 'webmd medical claims'],
+    ['vendor', 'webmd']
+])
+enqueue_psql_script('load_transactions.sql', [
+    ['input_path', S3_EMDEON_IN + args.date + '/'],
+    ['credentials', args.s3_credentials]
+])
+enqueue_psql_script('load_matching_payload.sql', [
+    ['matching_path', S3_EMDEON_MATCHING + args.date + '/'],
+    ['credentials', args.s3_credentials]
+])
+enqueue_psql_script('split_raw_transactions.sql')
+enqueue_psql_script('normalize_professional_claims.sql')
+enqueue_psql_script('normalize_institutional_claims.sql')
 
 # Privacy filtering
-subprocess.call(' '.join(psql + ['-v', 'table_name=medicalclaims_common_model'] +
-    ['-v', 'column_name=diagnosis_code'] +
-    ['-v', 'qual_column_name=diagnosis_code_qual'] +
-    [db, '<', '../../redshift_norm_common/nullify_icd9_blacklist.sql']), shell=True)
-subprocess.call(' '.join(psql + ['-v', 'table_name=medicalclaims_common_model'] +
-    ['-v', 'column_name=diagnosis_code'] +
-    ['-v', 'qual_column_name=diagnosis_code_qual'] +
-    [db, '<', '../../redshift_norm_common/nullify_icd10_blacklist.sql']), shell=True)
-subprocess.call(' '.join(psql + ['-v', 'table_name=medicalclaims_common_model'] +
-    ['-v', 'column_name=diagnosis_code'] +
-    ['-v', 'qual_column_name=diagnosis_code_qual'] +
-    [db, '<', '../../redshift_norm_common/genericize_icd9.sql']), shell=True)
-subprocess.call(' '.join(psql + ['-v', 'table_name=medicalclaims_common_model'] +
-    ['-v', 'column_name=diagnosis_code'] +
-    ['-v', 'qual_column_name=diagnosis_code_qual'] +
-    [db, '<', '../../redshift_norm_common/genericize_icd10.sql']), shell=True)
-subprocess.call(' '.join(psql + [db, '<', '../../redshift_norm_common/scrub_place_of_service.sql']), shell=True)
-subprocess.call(' '.join(psql + [db, '<', '../../redshift_norm_common/scrub_discharge_status.sql']), shell=True)
-subprocess.call(' '.join(psql + [db, '<', '../../redshift_norm_common/nullify_drg_blacklist.sql']), shell=True)
-subprocess.call(' '.join(psql + ['-v', 'table_name=medicalclaims_common_model'] +
-    ['-v', 'column_name=patient_age'] +
-    [db, '<', '../../redshift_norm_common/cap_age.sql']), shell=True)
-    
+enqueue_psql_script('../../redshift_norm_common/nullify_icd9_blacklist.sql', [
+    ['table_name', 'medicalclaims_common_model'],
+    ['column_name', 'diagnosis_code'],
+    ['qual_column_name', 'diagnosis_code_qual']
+])
+enqueue_psql_script('../../redshift_norm_common/nullify_icd10_blacklist.sql', [
+    ['table_name', 'medicalclaims_common_model'],
+    ['column_name', 'diagnosis_code'],
+    ['qual_column_name', 'diagnosis_code_qual']
+])
+enqueue_psql_script('../../redshift_norm_common/genericize_icd9_blacklist.sql', [
+    ['table_name', 'medicalclaims_common_model'],
+    ['column_name', 'diagnosis_code'],
+    ['qual_column_name', 'diagnosis_code_qual']
+])
+enqueue_psql_script('../../redshift_norm_common/genericize_icd10_blacklist.sql', [
+    ['table_name', 'medicalclaims_common_model'],
+    ['column_name', 'diagnosis_code'],
+    ['qual_column_name', 'diagnosis_code_qual']
+])
+enqueue_psql_script('../../redshift_norm_common/scrub_place_of_service.sql')
+enqueue_psql_script('../../redshift_norm_common/scrub_discharge_status.sql')
+enqueue_psql_script('../../redshift_norm_common/nullify_drg_blacklist.sql')
+enqueue_psql_script('../../redshift_norm_common/cap_age.sql', [
+    ['table_name', 'medicalclaims_common_model'],
+    ['column_name', 'patient_age']
+])
 
-subprocess.call(' '.join(psql + ['-v', 'output_path="\'' + args.output_path + '\'"'] +
-    ['-v', 'credentials="\'' + args.s3_credentials + '\'"'] +
-    ['-v', 'select_from_common_model_table="\'SELECT * FROM medicalclaims_common_model\'"'] +
-    [db, '<', '../../redshift_norm_common/unload_common_model.sql']), shell=True)
+enqueue_psql_script('../../redshift_norm_common/unload_common_model.sql', [
+    ['output_path', args.output_path],
+    ['credentials', args.s3_credentials],
+    ['select_from_common_model_table', 'SELECT * FROM medicalclaims_common_model']
+])
+
+execute_queue(args.debug)
