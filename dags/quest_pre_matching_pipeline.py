d0c4c4749118f3c7331cc632c939f01130d36c5f
Matt Usifer <musifer@healthverity.com>
2017-01-09 16:58:50 -0500

    Re use get_formatted_date

--- dags/quest_pre_matching_pipeline.py
+++ dags/quest_pre_matching_pipeline.py
@@ -1,9 +1,10 @@
 from airflow import DAG
 from airflow.models import Variable
-from airflow.operators import BashOperator, PythonOperator
+from airflow.operators import PythonOperator
 from datetime import datetime, timedelta
 import sys
 import re
+import subprocess
 
 if sys.modules.get('util.file_utils'):
     del sys.modules['util.file_utils']
@@ -37,7 +38,6 @@ MINIMUM_TRANSACTION_TRUNK_FILE_SIZE = 15
 
 # Deid file
 DEID_FILE_DESCRIPTION = 'Quest deid file'
-S3_DEID_RAW_PATH = 's3://healthverity/incoming/labtests/quest/'
 DEID_FILE_NAME_TEMPLATE = 'HealthVerity_{}_1_DeID.txt.zip'
 DEID_DAG_NAME = 'validate_fetch_deid_file'
 MINIMUM_DEID_FILE_SIZE = 500
@@ -45,9 +45,9 @@ MINIMUM_DEID_FILE_SIZE = 500
 default_args = {
     'owner': 'airflow',
     'start_date': datetime(2016, 12, 1, 12),
-    'depends_on_past': False,
-    'retries': 3,
-    'retry_delay': timedelta(minutes=2)
+    'depends_on_past': False
+    # 'retries': 3,
+    # 'retry_delay': timedelta(minutes=2)
 }
 
 mdag = DAG(
@@ -56,74 +56,121 @@ mdag = DAG(
     default_args=default_args
 )
 
-yesterday = datetime.today() - timedelta(1)
-two_days_ago = yesterday - timedelta(1)
-formatted_date = two_days_ago.strftime('%Y%m%d') + yesterday.strftime('%m%d')
+
+def get_formatted_date(kwargs):
+    return (
+        datetime.strptime(
+            kwargs['yesterday_ds_nodash'], '%Y%m%d'
+        ) - timedelta(days=1)
+    ).strftime('%Y%m%d') + kwargs['yesterday_ds_nodash'][4:8]
 
 
-def fetch_step(task_id, s3_path, local_path):
+def fetch_step(task_id, s3_path_template, local_path_template):
+    def execute(ds, **kwargs):
+        file_utils.fetch_file_from_s3(
+            Variable.get("AWS_ACCESS_KEY_ID"),
+            Variable.get("AWS_SECRET_ACCESS_KEY"),
+            s3_path_template.format(get_formatted_date(kwargs)),
+            local_path_template.format(get_formatted_date(kwargs))
+        )
     return PythonOperator(
         task_id='fetch_' + task_id,
         provide_context=True,
-        python_callable=file_utils.fetch_file_from_s3(
-            Variable.get("AWS_ACCESS_KEY_ID"),
-            Variable.get("AWS_SECRET_ACCESS_KEY"),
-            s3_path,
-            local_path
-        ),
+        python_callable=execute,
         dag=mdag
     )
 fetch_addon = fetch_step(
     "addon",
-    "{}{}".format(S3_TRANSACTION_RAW_PATH, TRANSACTION_ADDON_FILE_NAME_TEMPLATE.format(formatted_date)),
-    TRANSACTION_ADDON_TMP_PATH_TEMPLATE.format(formatted_date)
+    "{}{}".format(
+        S3_TRANSACTION_RAW_PATH,
+        TRANSACTION_ADDON_FILE_NAME_TEMPLATE
+    ),
+    TRANSACTION_ADDON_TMP_PATH_TEMPLATE
 )
 fetch_trunk = fetch_step(
     "trunk",
-    "{}{}".format(S3_TRANSACTION_RAW_PATH, TRANSACTION_TRUNK_FILE_NAME_TEMPLATE.format(formatted_date)), 
-    TRANSACTION_TRUNK_TMP_PATH_TEMPLATE.format(formatted_date)
+    '{}{}'.format(
+        S3_TRANSACTION_RAW_PATH,
+        TRANSACTION_TRUNK_FILE_NAME_TEMPLATE
+    ),
+    TRANSACTION_TRUNK_TMP_PATH_TEMPLATE
 )
 
 
-def unzip_step(task_id, tmp_path):
+def unzip_step(task_id, tmp_path_template):
+    def execute(ds, **kwargs):
+        file_utils.unzip(
+            tmp_path_template.format(
+                get_formatted_date(kwargs)
+            )
+        )
     return PythonOperator(
         task_id='unzip_file_' + task_id,
         provide_context=True,
-        python_callable=file_utils.unzip(formatted_date, tmp_path),
+        python_callable=execute,
         dag=mdag
     )
-unzip_addon = unzip_step("addon", TRANSACTION_ADDON_TMP_PATH_TEMPLATE)
-unzip_trunk = unzip_step("trunk", TRANSACTION_TRUNK_TMP_PATH_TEMPLATE)
-
-decrypt_addon = PythonOperator(
-    task_id='decrypt_file',
-    provide_context=True,
-    python_callable=file_utils.decrypt(
-        Variable.get('AWS_ACCESS_KEY_ID'),
-        Variable.get('AWS_SECRET_ACCESS_KEY'),
-        formatted_date,
-        TRANSACTION_ADDON_TMP_PATH_TEMPLATE
-    ),
-    dag=mdag
+unzip_addon = unzip_step(
+    "addon", TRANSACTION_ADDON_TMP_PATH_TEMPLATE
+)
+unzip_trunk = unzip_step(
+    "trunk", TRANSACTION_TRUNK_TMP_PATH_TEMPLATE
 )
 
 
-def gunzip_step(task_id, tmp_path):
+def decrypt_step(task_id, tmp_path_template):
+    def execute(ds, **kwargs):
+        file_utils.decrypt(
+            Variable.get('AWS_ACCESS_KEY_ID'),
+            Variable.get('AWS_SECRET_ACCESS_KEY'),
+            TRANSACTION_ADDON_TMP_PATH_TEMPLATE.format(
+                get_formatted_date(kwargs)
+            )
+        )
+    return PythonOperator(
+        task_id='decrypt_file_' + task_id,
+        provide_context=True,
+        python_callable=execute,
+        dag=mdag
+    )
+decrypt_addon = decrypt_step("addon", TRANSACTION_ADDON_TMP_PATH_TEMPLATE)
+
+
+def gunzip_step(task_id, tmp_path_template):
+    def execute(ds, **kwargs):
+        file_utils.gunzip(
+            tmp_path_template.format(
+                get_formatted_date(kwargs)
+            )
+        )
     return PythonOperator(
         task_id='gunzip_file_' + task_id,
         provide_context=True,
-        python_callable=file_utils.gunzip(formatted_date, tmp_path),
+        python_callable=execute,
         dag=mdag
     )
-gunzip_addon = gunzip_step("addon", TRANSACTION_ADDON_TMP_PATH_TEMPLATE)
-gunzip_trunk = gunzip_step("trunk", TRANSACTION_TRUNK_TMP_PATH_TEMPLATE)
+gunzip_addon = gunzip_step(
+    "addon", TRANSACTION_ADDON_TMP_PATH_TEMPLATE
+)
+gunzip_trunk = gunzip_step(
+    "trunk", TRANSACTION_TRUNK_TMP_PATH_TEMPLATE
+)
 
 
-def split_step(task_id, tmp_path, tmp_parts_path):
+def split_step(task_id, tmp_path_template, tmp_parts_path_template):
+    def execute(ds, **kwargs):
+        file_utils.split(
+            tmp_path_template.format(
+                get_formatted_date(kwargs)
+            ),
+            tmp_parts_path_template.format(
+                get_formatted_date(kwargs)
+            )
+        )
     return PythonOperator(
         task_id='split_file_' + task_id,
         provide_context=True,
-        python_callable=file_utils.split(formatted_date, tmp_path, tmp_parts_path),
+        python_callable=execute,
         dag=mdag
     )
 split_addon = split_step(
@@ -138,72 +185,95 @@ split_trunk = split_step(
 )
 
 
-def bzip_parts_step(task_id, tmp_parts_path):
+def bzip_parts_step(task_id, tmp_parts_path_template):
+    def execute(ds, **kwargs):
+        file_utils.bzip_part_files(
+            tmp_parts_path_template.format(
+                get_formatted_date(kwargs)
+            )
+        )
     return PythonOperator(
         task_id='bzip_part_files_' + task_id,
         provide_context=True,
-        python_callable=file_utils.bzip_part_files(formatted_date, tmp_parts_path),
+        python_callable=execute,
         dag=mdag
     )
-bzip_parts_addon = bzip_parts_step("addon", TRANSACTION_ADDON_TMP_PATH_PARTS_TEMPLATE)
-bzip_parts_trunk = bzip_parts_step("trunk", TRANSACTION_TRUNK_TMP_PATH_PARTS_TEMPLATE)
+bzip_parts_addon = bzip_parts_step(
+    "addon", TRANSACTION_ADDON_TMP_PATH_PARTS_TEMPLATE
+)
+bzip_parts_trunk = bzip_parts_step(
+    "trunk", TRANSACTION_TRUNK_TMP_PATH_PARTS_TEMPLATE
+)
 
 
-def push_splits_to_s3_step(task_id, tmp_path, tmp_parts_path, s3_path):
+def push_splits_to_s3_step(task_id, tmp_parts_path, s3_path):
+    def execute(ds, **kwargs):
+        formatted_date = get_formatted_date(kwargs)
+        file_utils.push_splits_to_s3(
+            tmp_parts_path.format(formatted_date),
+            s3_path.format(
+                re.sub('[^0-9]', '', formatted_date)[0:4],
+                re.sub('[^0-9]', '', formatted_date)[4:6],
+                re.sub('[^0-9]', '', formatted_date)[6:8]
+            ),
+            Variable.get('AWS_ACCESS_KEY_ID'),
+            Variable.get('AWS_SECRET_ACCESS_KEY')
+        )
     return PythonOperator(
         task_id='push_splits_to_s3_' + task_id,
         provide_context=True,
-        python_callable=file_utils.push_splits_to_s3(
-            tmp_parts_path, s3_path,
-            Variable.get('AWS_ACCESS_KEY_ID'),
-            Variable.get('AWS_SECRET_ACCESS_KEY')
-        ),
+        python_callable=execute,
         dag=mdag
     )
 push_splits_to_s3_addon = push_splits_to_s3_step(
     "addon",
-    TRANSACTION_ADDON_TMP_PATH_TEMPLATE,
     TRANSACTION_ADDON_TMP_PATH_PARTS_TEMPLATE,
-    TRANSACTION_ADDON_S3_SPLIT_PATH.format(
-        re.sub('[^0-9]', '', formatted_date)[0:4],
-        re.sub('[^0-9]', '', formatted_date)[4:6],
-        re.sub('[^0-9]', '', formatted_date)[6:8]
-    )
+    TRANSACTION_ADDON_S3_SPLIT_PATH
 )
 push_splits_to_s3_trunk = push_splits_to_s3_step(
     "trunk",
-    TRANSACTION_TRUNK_TMP_PATH_TEMPLATE,
     TRANSACTION_TRUNK_TMP_PATH_PARTS_TEMPLATE,
-    TRANSACTION_TRUNK_S3_SPLIT_PATH.format(
-        re.sub('[^0-9]', '', formatted_date)[0:4],
-        re.sub('[^0-9]', '', formatted_date)[4:6],
-        re.sub('[^0-9]', '', formatted_date)[6:8]
+    TRANSACTION_TRUNK_S3_SPLIT_PATH
+)
+
+
+def queue_up_for_matching_step(seq_num, engine_env, priority):
+    def execute(ds, **kwargs):
+        deid_file = '{}{}'.format(
+            S3_TRANSACTION_RAW_PATH, DEID_FILE_NAME_TEMPLATE.format(
+                get_formatted_date(kwargs)
+            ))
+        subprocess.call([
+            '/home/airflow/airflow/dags/resources/push_file_to_s3.sh',
+            deid_file, seq_num, engine_env, priority
+        ], env=file_utils.get_s3_env(
+            Variable.get('AWS_ACCESS_KEY_ID_MATCH_PUSHER'),
+            Variable.get('AWS_SECRET_ACCESS_KEY_MATCH_PUSHER')
+        ))
+    return PythonOperator(
+        task_id='queue_up_for_matching',
+        provide_context=True,
+        python_callable=execute,
+        dag=mdag
     )
+queue_up_for_matching = queue_up_for_matching_step(
+    '0', 'prod-matching-engine', 'priority3'
 )
 
-queue_up_for_matching = BashOperator(
-    task_id='queue_up_for_matching',
-    bash_command='resources/push_file_to_s3.sh {}{}'.format(
-        S3_DEID_RAW_PATH, DEID_FILE_NAME_TEMPLATE.format(formatted_date)) +
-    ' {{ params.sequence_num }} {{ params.matching_engine_env }} {{ params.priority }}',
-    params={'sequence_num': 0,
-            'matching_engine_env': 'prod-matching-engine',
-            'priority': 'priority3'},
-    env=file_utils._get_s3_env(
-        Variable.get('AWS_ACCESS_KEY_ID'),
-        Variable.get('AWS_SECRET_ACCESS_KEY')
-    ),
-    dag=mdag
-)
 
-clean_up_workspace = BashOperator(
-    task_id='clean_up_workspace',
-    bash_command='rm -rf {};'.format(
-        TMP_PATH_TEMPLATE.format(formatted_date)
-    ),
-    trigger_rule='all_done',
-    dag=mdag
-)
+def clean_up_workspace_step():
+    def execute(ds, **kwargs):
+        subprocess.call([
+            'rm', '-rf', TMP_PATH_TEMPLATE.format(get_formatted_date(kwargs))
+        ])
+    return PythonOperator(
+        task_id='clean_up_workspace',
+        provide_context=True,
+        python_callable=execute,
+        trigger_rule='all_done',
+        dag=mdag
+    )
+clean_up_workspace = clean_up_workspace_step()
 
 # addon
 unzip_addon.set_upstream(fetch_addon)
@@ -222,4 +292,5 @@ bzip_parts_trunk.set_upstream(split_trunk)
 push_splits_to_s3_trunk.set_upstream(bzip_parts_trunk)
 
 # all
-clean_up_workspace.set_upstream(push_splits_to_s3_trunk)
+queue_up_for_matching.set_upstream(push_splits_to_s3_trunk)
+clean_up_workspace.set_upstream(queue_up_for_matching)
--- dags/util/file_utils.py
+++ dags/util/file_utils.py
@@ -7,6 +7,12 @@ from subprocess import check_call
 import config
 
 
+def _get_files(path):
+    """Get all files in a path"""
+    file_list = os.listdir(path)
+    return map(lambda x: path + x, file_list)
+
+
 def get_s3_env(aws_id, aws_key):
     env = os.environ
     env["AWS_ACCESS_KEY_ID"] = aws_id
@@ -14,124 +20,80 @@ def get_s3_env(aws_id, aws_key):
     return env
 
 
-def _get_files(path):
-    """Get all files in a path"""
-    file_list = os.listdir(path)
-    return map(lambda x: path + x, file_list)
-
-
-def unzip(formatted_date, tmp_path_template):
-    """Unzip correct file in tmp path dir"""
-    def execute(ds, **kwargs):
-        zip_file = filter(
-            lambda x: x.endswith(".zip"),
-            _get_files(tmp_path_template.format(formatted_date))
-        )[0]
-
-        check_call([
-            'unzip',
-            zip_file,
-            '-d',
-            tmp_path_template.format(formatted_date)
-        ])
-        check_call([
-            'rm',
-            zip_file
-        ])
-    return execute
-
-
 def fetch_file_from_s3(aws_id, aws_key, s3_path, local_path):
     """Download a file from S3"""
-    def execute(ds, **kwargs):
-        env = get_s3_env(aws_id, aws_key)
-        check_call(['aws', 's3', 'cp', s3_path, local_path], env=env)
-    return execute
+    env = get_s3_env(aws_id, aws_key)
+    check_call(['aws', 's3', 'cp', s3_path, local_path], env=env)
 
 
-def decrypt(aws_id, aws_key, formatted_date, tmp_path_template):
-    """Decrypt correct file in tmp path dir"""
-    TMP_DECRYPTOR_LOCATION = "/tmp/decrypt/"
-    DECRYPTOR_JAR = TMP_DECRYPTOR_LOCATION + "decryptor.jar"
-    DECRYPTOR_KEY = TMP_DECRYPTOR_LOCATION + "private.reformat"
-
-    def execute(ds, **kwargs):
-        env = get_s3_env(aws_id, aws_key)
-        check_call([
-            'aws', 's3', 'cp',
-            config.DECRYPTOR_JAR_LOCATION, DECRYPTOR_JAR
-        ], env=env)
-        check_call([
-            'aws', 's3', 'cp',
-            config.DECRYPTOR_KEY_LOCATION, DECRYPTOR_KEY
-        ], env=env)
-
-        file_name = _get_files(
-            tmp_path_template.format(formatted_date)
-        )[0]
-        check_call([
-            "java", "-jar", DECRYPTOR_JAR,
-            "-i", file_name, "-o", file_name + ".gz",
-            "-k", DECRYPTOR_KEY
-        ], env=env)
-
-        check_call([
-            'rm', '-r', TMP_DECRYPTOR_LOCATION
-        ], env=env)
-    return execute
-
-
-def gunzip(formatted_date, tmp_path_template):
-    """Unzip correct gzipped file in tmp path dir"""
-    return lambda ds, **kwargs: check_call([
-        'gzip', '-d', '-k', '-f',
-        filter(
-            lambda x: x.endswith('.gz'),
-            _get_files(tmp_path_template.format(formatted_date))
-        )[0]
-
+def unzip(path):
+    """Unzip correct file in tmp path dir"""
+    zip_file = filter(
+        lambda x: x.endswith(".zip"),
+        _get_files(path)
+    )[0]
+    check_call([
+        'unzip', zip_file, '-d', path
+    ])
+    check_call([
+        'rm', zip_file
     ])
 
 
-def split(formatted_date, tmp_path_template, tmp_path_parts_template, count='20'):
-    """Split a file into a specified number of even parts"""
-    def execute(ds, **kwargs):
-        file_path = filter(
-            lambda x: not re.search('.gz$', x),
-            _get_files(
-                tmp_path_template.format(formatted_date)
-            )
+def decrypt(aws_id, aws_key, path):
+    """Decrypt correct file in tmp path dir"""
+    DECRYPTOR_LOCATION = "/home/airflow/decrypt/"
+    DECRYPTOR_JAR = DECRYPTOR_LOCATION + "decryptor.jar"
+    DECRYPTOR_KEY = DECRYPTOR_LOCATION + "private.reformat"
+
+    env = get_s3_env(aws_id, aws_key)
+    file_name = _get_files(path)[0]
+    check_call([
+        "java", "-jar", DECRYPTOR_JAR,
+        "-i", file_name, "-o", file_name + ".gz",
+        "-k", DECRYPTOR_KEY
+    ], env=env)
+
+
+def gunzip(path):
+    """Unzip correct gzipped file in tmp path dir"""
+    check_call([
+        'gzip', '-d', '-k', '-f',
+        filter(
+            lambda x: x.endswith('.gz'),
+            _get_files(path)
         )[0]
-        parts_file_path = tmp_path_parts_template.format(formatted_date)
-        check_call(['mkdir', '-p', parts_file_path])
-        check_call(['split', '-n', 'l/' + count, file_path, '{}{}.'
-                    .format(parts_file_path, file_path.split('/')[-1])])
-    return execute
+    ])
 
 
-def bzip_part_files(formatted_date, tmp_path_parts_template):
+def split(path, parts_path, count='20'):
+    """Split a file into a specified number of even parts"""
+    file_path = filter(
+        lambda x: not re.search('.gz$', x),
+        _get_files(path)
+    )[0]
+    check_call(['mkdir', '-p', parts_path])
+    check_call(['split', '-n', 'l/' + count, file_path, '{}{}.'
+                .format(parts_path, file_path.split('/')[-1])])
+
+
+def bzip_part_files(parts_path):
     """Bzip each file in a directory"""
-    def execute(ds, **kwargs):
-        tmp_path_parts = tmp_path_parts_template.format(formatted_date)
-        file_list = _get_files(
-            tmp_path_parts_template.format(formatted_date)
-        )
-        for file_name in file_list:
-            check_call([
-                'lbzip2', '{}{}'.format(
-                    tmp_path_parts, file_name.split('/')[-1]
-                )
-            ])
-    return execute
+    file_list = _get_files(
+        parts_path
+    )
+    for file_name in file_list:
+        check_call([
+            'lbzip2', '{}{}'.format(
+                parts_path, file_name.split('/')[-1]
+            )
+        ])
 
 
 def push_splits_to_s3(
-        tmp_path_parts_template, s3_transaction_split_path, aws_id, aws_key
+        parts_path, s3_transaction_split_path, aws_id, aws_key
 ):
     """Push each file in a directory up to a specified s3 location"""
-    def execute(ds, **kwargs):
-        tmp_path_parts = tmp_path_parts_template.format(formatted_date)
-        env = get_s3_env(aws_id, aws_key)
-        check_call(['aws', 's3', 'cp', '--recursive', tmp_path_parts, "{}"
-                    .format(s3_transaction_split_path)], env=env)
-    return execute
+    env = get_s3_env(aws_id, aws_key)
+    check_call(['aws', 's3', 'cp', '--recursive', parts_path, "{}"
+                .format(s3_transaction_split_path)], env=env)
