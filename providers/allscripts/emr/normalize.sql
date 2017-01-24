793151d2677d57b6521b95bb172efbc02f822886
Matt Usifer <musifer@healthverity.com>
2017-01-24 14:41:10 -0500

    Correctly joining between tables

--- providers/allscripts/emr/normalize.sql
+++ providers/allscripts/emr/normalize.sql
@@ -1,6 +1,7 @@
 INSERT INTO emr_common_model (
         hvid,
         source_version,
+        date_start,
         patient_year_of_birth,
         patient_zip,
         patient_gender,
@@ -10,8 +11,13 @@ INSERT INTO emr_common_model (
         ndc_code
         )
 SELECT DISTINCT
-    COALESCE(payload.parentid, payload.hvid),
+    CASE
+    WHEN payload.parentid IS NOT NULL 
+    THEN 'AS13842-' || payload.parentid
+    ELSE payload.hvid
+    END,
     '1',
+    encounters.encounterDTTM,
     as_patients.dobyear,
     payload.threeDigitZip,
     CASE as_patients.gender 
@@ -28,7 +34,6 @@ FROM transactional_encounters encounters
     -- diagnoses
     SELECT
         encounterid,
-        genpatientID,
         gen2patientID,
         billingicd10code AS diagnosis_code,
         null AS procedure_code,
@@ -38,7 +43,6 @@ FROM transactional_encounters encounters
     UNION ALL
     SELECT DISTINCT
         encounterid,
-        genpatientID,
         gen2patientID,
         icd10 AS diagnosis_code,
         null AS procedure_code,
@@ -51,7 +55,6 @@ FROM transactional_encounters encounters
     UNION ALL
     SELECT
         encounterid,
-        genpatientID,
         gen2patientID,
         null AS diagnosis_code,
         cpt4 AS procedure_code,
@@ -61,19 +64,17 @@ FROM transactional_encounters encounters
     UNION ALL
     SELECT
         encounterid,
-        genpatientID,
         gen2patientID,
         null AS diagnosis_code,
         hcpcs AS procedure_code,
-        null as ndc_code,
-        null as loinc_code
+        null AS ndc_code,
+        null AS loinc_code
     FROM transactional_orders
     UNION ALL
     SELECT
         encounterid,
-        genpatientID,
         gen2patientID,
-        null AS diagnosis_code
+        null AS diagnosis_code,
         cptcode AS procedure_code,
         null AS ndc_code,
         null AS loinc_code
@@ -84,7 +85,6 @@ FROM transactional_encounters encounters
     UNION ALL
     SELECT
         encounterid,
-        genpatientID,
         gen2patientID,
         null AS diagnosis_code,
         null AS procedure_code,
@@ -95,7 +95,6 @@ FROM transactional_encounters encounters
     UNION ALL
     SELECT
         encounterid,
-        genpatientID,
         gen2patientID,
         null AS diagnosis_code,
         null AS procedure_code,
@@ -107,20 +106,17 @@ FROM transactional_encounters encounters
     UNION ALL
     SELECT
         encounterid,
-        genpatientID,
         gen2patientID,
         null AS diagnosis_code,
         null AS procedure_code,
         null AS ndc_code,
-        loinc AS code
+        loinc AS loinc_code
     FROM transactional_results 
         ) codes
-    ON encounters.encounterid = codes.encounterid
-    AND encounters.genpatientID = codes.genpatientID
-    AND encounters.gen2patientID = codes.gen2patientID
-    INNER JOIN transactional_patients as_patients
-    ON encounters.genpatientID = as_patients.genpatientID
-    AND encounters.gen2patientID = as_patients.gen2patientID
-    INNER JOIN matching_payload payload
-    ON encounters.genpatientID = payload.claimid
-    AND encounters.gen2patientID = payload.personid
+    ON encounters.encounterid = codes.encounterid AND encounters.gen2patientID = codes.gen2patientID
+    INNER JOIN transactional_patients as_patients ON encounters.gen2patientID = as_patients.gen2patientID
+    INNER JOIN matching_payload payload ON encounters.gen2patientID = upper(payload.personid)
+WHERE codes.diagnosis_code IS NOT NULL 
+    OR codes.procedure_code IS NOT NULL
+    OR codes.ndc_code IS NOT NULL
+    OR codes.loinc_code IS NOT NULL
