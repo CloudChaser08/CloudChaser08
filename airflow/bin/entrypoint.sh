dfbd07139a743cf81da047aff413283b77baa029
John Cappiello <jcappiello@healthverity.com>
2017-01-27 21:20:43 -0500

    initial docker env

---
+++ airflow/bin/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+AIRFLOW_HOME="/usr/local/airflow"
+CMD="airflow"
+
+# Generate Fernet key
+: ${FERNET_KEY:=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print FERNET_KEY")}
+sed -i "s|\$FERNET_KEY|$FERNET_KEY|" "$AIRFLOW_HOME"/airflow.cfg
+
+echo "Reset database..."
+
+
+$CMD initdb
+$CMD resetdb
+
+sqlite3 $AIRFLOW_HOME/airflow.db \
+  "INSERT INTO connection (conn_id, conn_type, host, schema, login, password, port, extra, is_encrypted, is_extra_encrypted)
+    VALUES ('hive_analytics','hiveserver2','analytics.aws.healthverity.com','default','airflow','','10000','',0,0)"
+
+sqlite3 $AIRFLOW_HOME/airflow.db \
+  "INSERT INTO variable (\"key\", val, is_encrypted)
+   VALUES ('DECRYPTOR_JAR_REMOTE_LOCATION','gAAAAABYi84qao5VTHGJ6-u-7xmtPnDJdoNxtpF5Y5Qrr3EPWP0nBLaKWJsfw61XRzKy558zYXfJ_m0VdNpCEZqfdkGijBrKHm0c5lr0epE8oUBn07DkN03OxZ9DgLyW3demkb6R0Tu1gV0ze_tSTIX_J8HcMrCtrQ==',1)"
+
+sqlite3 $AIRFLOW_HOME/airflow.db \
+  "INSERT INTO variable (\"key\", val, is_encrypted)
+   VALUES ('DECRYPTION_KEY_REMOTE_LOCATION','gAAAAABYi8416gMCIkCxvtPDz7GrXJWOoPKvWGFu42B6LJc0kHAcrMv_o0ZhsHDIcCPFXujI06zCQbg4epduYKJKVU6LW2lkFVwCUBiuf0n1EYmCC7ydLC7xSqPK9Tc_A13hFpB2p4H-qHdh2-VnAkJ6SIN87C11RQ==',1)"
+
+$CMD variables -s 'Slack Token' 'testing'
+
+echo "Starting Webserver..."
+$CMD webserver -D --stderr /dev/stdout
+
+exec $CMD scheduler
