6616d4387335d1f4b57a03034748d1bd8616efeb
Matt Usifer <musifer@healthverity.com>
2017-01-30 10:43:18 -0500

    Use ids for vendor and feed name

--- providers/ability/rsNormalizeAbility.py
+++ providers/ability/rsNormalizeAbility.py
@@ -2,7 +2,6 @@
 import subprocess
 import argparse
 import time
-import hashlib
 import sys
 sys.path.append(os.path.abspath("../redshift_norm_common/"))
 import create_date_validation_table as date_validator
@@ -38,14 +37,12 @@ if args.rs_user:
 date_validator.generate(psql, db, args.s3_credentials)
 
 # create table for medical claims common model
-prov_hash = hashlib.md5()
-prov_hash.update("ability")
 subprocess.call(' '.join(
     psql
     + ['-v', 'filename="\'' + args.setid + '\'"']
     + ['-v', 'today="\'' + TODAY + '\'"']
     + ['-v', 'feedname="\'15\'"']
-    + ['-v', 'vendor="\'' + prov_hash.hexdigest() + '\'"']
+    + ['-v', 'vendor="\'14\'"']
     + [db, '<', '../redshift_norm_common/medicalclaims_common_model.sql']
 ), shell=True)
 
