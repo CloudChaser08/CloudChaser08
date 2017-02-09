918db9b7813d6692c36c6031688fe7d50f95b490
Matt Usifer <musifer@healthverity.com>
2017-02-09 10:39:51 -0500

    Deleted udfs

--- spark/spark.py
+++ spark/spark.py
@@ -1,9 +1,7 @@
 from pyspark.sql import HiveContext, SparkSession
-from spark.helpers.udf.user_defined_functions \
-    import get_diagnosis_with_priority, string_set_diff, uniquify
-from spark.helpers.udf.post_normalization_cleanup \
-    import clean_up_diagnosis_code, obscure_place_of_service, \
-    filter_due_to_place_of_service
+from helpers.udf.post_normalization_cleanup \
+  import clean_up_diagnosis_code, obscure_place_of_service, \
+  filter_due_to_place_of_service
 
 
 def init(provider, local=False):
@@ -14,17 +12,6 @@ def init(provider, local=False):
                     .getOrCreate()
 
     sqlContext = HiveContext(spark.sparkContext)
-
-    # register udfs
-    sqlContext.registerFunction(
-        'get_diagnosis_with_priority', get_diagnosis_with_priority
-    )
-    sqlContext.registerFunction(
-        'string_set_diff', string_set_diff
-    )
-    sqlContext.registerFunction(
-        'uniquify', uniquify
-    )
 
     # register privacy filters
     sqlContext.registerFunction(
