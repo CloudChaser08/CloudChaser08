870b48f5e7bd143e2700607b8ab1e4648922c865
Ilia Fishbein <ifishbein@healthverity.com>
2017-03-07 13:36:31 -0500

    Don't make any assumptions about which files need to be pushed. Instead call a function that retuns all the absolute file paths

--- airflow/dags/subdags/s3_push_files.py
+++ airflow/dags/subdags/s3_push_files.py
@@ -7,23 +7,18 @@ import logging
 import os
 
 def do_push_files(ds, **kwargs):
-    file_name      = kwargs['file_name_func'](ds, kwargs) if kwargs.get('file_name_func') else None
+    file_paths     = kwargs['file_paths_func'](ds, kwargs)
     s3_prefix      = kwargs['s3_prefix_func'](ds, kwargs)
 
-    tmp_path = kwargs['tmp_path_template'].format(kwargs['ds_nodash'])
     if kwargs.get('aws_access_key_id'):
-        logging.info('Configuring AWS keys')
         s3 = boto3.resource('s3', aws_access_key_id=kwargs['aws_access_key_id'], aws_secret_access_key=kwargs['aws_secret_access_key'])
     else:
         s3 = boto3.resource('s3')
-
-    files = [file_name]
-    if kwargs.get('all_files_in_dir'):
-        files = os.listdir(tmp_path)
     
-    for f in files:
-        obj = s3.Object(kwargs['s3_bucket'], s3_prefix + f)
-        obj.upload_file(tmp_path + f, ExtraArgs={'ServerSideEncryption':'AES256'})
+    for f in file_paths:
+        fn = f.split('/')[-1]
+        obj = s3.Object(kwargs['s3_bucket'], s3_prefix + fn)
+        obj.upload_file(f, ExtraArgs={'ServerSideEncryption':'AES256'})
 
 def s3_push_files(parent_dag_name, child_dag_name, start_date, schedule_interval, dag_config):
     default_args = {
