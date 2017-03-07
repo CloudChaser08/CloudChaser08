69806ea48f556e49363c316a83dd1f28dff18e55
Matt Usifer <musifer@healthverity.com>
2017-03-07 10:03:02 -0500

    Avoid 'create table as'

--- spark/providers/practice_insight/create_exploded_diagnosis_map.sql
+++ spark/providers/practice_insight/create_exploded_diagnosis_map.sql
@@ -1,88 +1,76 @@
-DROP TABLE IF EXISTS exploded_ecodes;
-CREATE TABLE exploded_ecodes AS
-SELECT CONCAT(
-        COALESCE(transactional.src_claim_id,''),
-        COALESCE(transactional.src_svc_id,'')
-        ), -- pk
-    TRIM(REGEXP_REPLACE(SUBSTRING(SPLIT(ecodes, ',')[ecodes_exploder.n], 0, 
-                locate('=>', SPLIT(ecodes, ',')[ecodes_exploder.n])), '"', '')) as ecodes
-FROM transactional_raw transactional
-    CROSS JOIN diagnosis_exploder ecodes_exploder
-WHERE SPLIT(TRIM(transactional.ecodes), ',')[ecodes_exploder.n] IS NULL
-    OR SPLIT(TRIM(transactional.ecodes), ',')[ecodes_exploder.n] != ''
-    ;
-
-DROP TABLE IF EXISTS exploded_dx;
-CREATE TABLE exploded_dx AS
-SELECT CONCAT(
-        COALESCE(transactional.src_claim_id,''),
-        COALESCE(transactional.src_svc_id,'')
-        ), -- pk
-    TRIM(REGEXP_REPLACE(SUBSTRING(SPLIT(dx, ',')[dx_exploder.n], 0, 
-                locate('=>', SPLIT(dx, ',')[dx_exploder.n])), '"', '')) as dx
-FROM transactional_raw transactional
-    CROSS JOIN diagnosis_exploder dx_exploder
-WHERE SPLIT(TRIM(transactional.dx), ',')[dx_exploder.n] IS NULL
-    OR SPLIT(TRIM(transactional.dx), ',')[dx_exploder.n] != ''
-    ;
-
-DROP TABLE IF EXISTS exploded_other_diag_codes;
-CREATE TABLE exploded_other_diag_codes AS
-SELECT CONCAT(
-        COALESCE(transactional.src_claim_id,''),
-        COALESCE(transactional.src_svc_id,'')
-        ), -- pk
-    TRIM(REGEXP_REPLACE(SUBSTRING(SPLIT(other_diag_codes, ',')[other_diag_codes_exploder.n], 0, 
-                locate('=>', SPLIT(other_diag_codes, ',')[other_diag_codes_exploder.n])), '"', '')) as other_diag_codes
-FROM transactional_raw transactional
-    CROSS JOIN diagnosis_exploder other_diag_codes_exploder
-WHERE SPLIT(TRIM(transactional.other_diag_codes), ',')[other_diag_codes_exploder.n] IS NULL
-    OR SPLIT(TRIM(transactional.other_diag_codes), ',')[other_diag_codes_exploder.n] != ''
-    ;
-
-DROP TABLE IF EXISTS exploded_rest_of_codes;
-CREATE TABLE exploded_rest_of_codes AS
-SELECT CONCAT(
-        COALESCE(all_codes.src_claim_id,''),
-        COALESCE(all_codes.src_svc_id,'')
-        ), 
-    TRIM(SPLIT(all_codes.rest_of_codes, ',')[rest_of_codes_exploder.n])
-FROM (
-    SELECT DISTINCT
-        transactional.src_claim_id, transactional.src_svc_id, -- pk
-        CONCAT(
-            COALESCE(transactional.prinpl_diag_cd, ''), ',',
-            COALESCE(transactional.admtg_diag_cd, ''), ',',
-            COALESCE(transactional.patnt_rsn_for_visit_01, ''), ',', 
-            COALESCE(transactional.patnt_rsn_for_visit_02, ''), ',', 
-            COALESCE(transactional.patnt_rsn_for_visit_03, '')
-            ) as rest_of_codes
-    FROM transactional_raw transactional
-        ) all_codes
-    CROSS JOIN diagnosis_exploder rest_of_codes_exploder
-WHERE SPLIT(TRIM(all_codes.rest_of_codes), ',')[rest_of_codes_exploder.n] IS NULL
-    OR SPLIT(TRIM(all_codes.rest_of_codes), ',')[rest_of_codes_exploder.n] != ''
-    ;
-
 DROP TABLE IF EXISTS exploded_diag_codes_nulls;
 CREATE TABLE exploded_diag_codes_nulls (
         claim_svc_num string,
         diag_code string
         );
 
--- union will remove duplicates
+-- insert ecodes
 INSERT INTO exploded_diag_codes_nulls (
-    SELECT * FROM exploded_ecodes
-    UNION
-    SELECT * FROM exploded_dx
-    UNION
-    SELECT * FROM exploded_other_diag_codes
-    UNION
-    SELECT * FROM exploded_rest_of_codes
-        )
-    ;
+    SELECT CONCAT(
+            COALESCE(transactional.src_claim_id,''),
+            COALESCE(transactional.src_svc_id,'')
+            ), -- pk
+        TRIM(REGEXP_REPLACE(SUBSTRING(SPLIT(ecodes, ',')[ecodes_exploder.n], 0, 
+                    locate('=>', SPLIT(ecodes, ',')[ecodes_exploder.n])-1), '"', '')) as ecodes
+    FROM transactional_raw transactional
+        CROSS JOIN diagnosis_exploder ecodes_exploder
+    WHERE SPLIT(TRIM(transactional.ecodes), ',')[ecodes_exploder.n] IS NULL
+        OR SPLIT(TRIM(transactional.ecodes), ',')[ecodes_exploder.n] != ''
+        );
 
--- strip out nulls for claim_svc_nums with populated codes
+-- insert dx
+INSERT INTO exploded_diag_codes_nulls (
+    SELECT CONCAT(
+            COALESCE(transactional.src_claim_id,''),
+            COALESCE(transactional.src_svc_id,'')
+            ), -- pk
+        TRIM(REGEXP_REPLACE(SUBSTRING(SPLIT(dx, ',')[dx_exploder.n], 0, 
+                    locate('=>', SPLIT(dx, ',')[dx_exploder.n])-1), '"', '')) as dx
+    FROM transactional_raw transactional
+        CROSS JOIN diagnosis_exploder dx_exploder
+    WHERE SPLIT(TRIM(transactional.dx), ',')[dx_exploder.n] IS NULL
+        OR SPLIT(TRIM(transactional.dx), ',')[dx_exploder.n] != ''
+        );
+
+-- insert other
+INSERT INTO exploded_diag_codes_nulls (
+    SELECT CONCAT(
+            COALESCE(transactional.src_claim_id,''),
+            COALESCE(transactional.src_svc_id,'')
+            ), -- pk
+        TRIM(REGEXP_REPLACE(SUBSTRING(SPLIT(other_diag_codes, ',')[other_diag_codes_exploder.n], 0, 
+                    locate('=>', SPLIT(other_diag_codes, ',')[other_diag_codes_exploder.n])-1), '"', '')) as other_diag_codes
+    FROM transactional_raw transactional
+        CROSS JOIN diagnosis_exploder other_diag_codes_exploder
+    WHERE SPLIT(TRIM(transactional.other_diag_codes), ',')[other_diag_codes_exploder.n] IS NULL
+        OR SPLIT(TRIM(transactional.other_diag_codes), ',')[other_diag_codes_exploder.n] != ''
+        );
+
+-- insert rest_of
+INSERT INTO exploded_diag_codes_nulls (
+    SELECT CONCAT(
+            COALESCE(all_codes.src_claim_id,''),
+            COALESCE(all_codes.src_svc_id,'')
+            ), 
+        TRIM(SPLIT(all_codes.rest_of_codes, ',')[rest_of_codes_exploder.n])
+    FROM (
+        SELECT DISTINCT
+            transactional.src_claim_id, transactional.src_svc_id, -- pk
+            CONCAT(
+                COALESCE(transactional.prinpl_diag_cd, ''), ',',
+                COALESCE(transactional.admtg_diag_cd, ''), ',',
+                COALESCE(transactional.patnt_rsn_for_visit_01, ''), ',', 
+                COALESCE(transactional.patnt_rsn_for_visit_02, ''), ',', 
+                COALESCE(transactional.patnt_rsn_for_visit_03, '')
+                ) as rest_of_codes
+        FROM transactional_raw transactional
+            ) all_codes
+        CROSS JOIN diagnosis_exploder rest_of_codes_exploder
+    WHERE SPLIT(TRIM(all_codes.rest_of_codes), ',')[rest_of_codes_exploder.n] IS NULL
+        OR SPLIT(TRIM(all_codes.rest_of_codes), ',')[rest_of_codes_exploder.n] != ''
+        );
+
+-- strip out nulls for claim_svc_nums with populated codes, remove dupes
 DROP TABLE IF EXISTS exploded_diag_codes;
 CREATE TABLE exploded_diag_codes (
         claim_svc_num string,
--- spark/providers/practice_insight/create_exploded_procedure_map.sql
+++ spark/providers/practice_insight/create_exploded_procedure_map.sql
@@ -1,38 +1,44 @@
-DROP TABLE IF EXISTS exploded_other_proc_codes;
-CREATE TABLE exploded_other_proc_codes AS
-SELECT CONCAT(
-        COALESCE(transactional.src_claim_id,''),
-        COALESCE(transactional.src_svc_id,'')
-        ), -- pk
-    TRIM(REPLACE(SUBSTRING(SPLIT(other_proc_codes, ',')[other_proc_codes_exploder.n], 
-                charindex('=>', SPLIT(other_proc_codes, ',')[other_proc_codes_exploder.n]) + 2, 
-                LENGTH(SPLIT(other_proc_codes, ',')[other_proc_codes_exploder.n])), '"', '')) as other_proc_codes
-FROM transactional_raw transactional
-    CROSS JOIN diagnosis_exploder other_proc_codes_exploder
-WHERE SPLIT(TRIM(transactional.other_proc_codes), ',')[other_proc_codes_exploder.n] IS NULL
-    OR SPLIT(TRIM(transactional.other_proc_codes), ',')[other_proc_codes_exploder.n] != ''
-    ;
+DROP TABLE IF EXISTS exploded_proc_codes_nulls;
+CREATE TABLE exploded_proc_codes_nulls (
+        claim_svc_num string,
+        proc_code string
+        );
 
-DROP TABLE IF EXISTS exploded_rest_of_codes;
-CREATE TABLE exploded_rest_of_codes AS
-SELECT CONCAT(
-        COALESCE(all_codes.src_claim_id,''),
-        COALESCE(all_codes.src_svc_id,'')
-        ), 
-    TRIM(SPLIT(all_codes.rest_of_codes, ',')[rest_of_codes_exploder.n])
-FROM (
-    SELECT DISTINCT
-        transactional.src_claim_id, transactional.src_svc_id, -- pk
-        CONCAT(
-            COALESCE(transactional.proc_cd,''), ',', 
-            COALESCE(transactional.prinpl_proc_cd,'')
-            ) as rest_of_codes
+-- insert other proc codes
+INSERT INTO exploded_proc_codes_nulls (
+    SELECT CONCAT(
+            COALESCE(transactional.src_claim_id,''),
+            COALESCE(transactional.src_svc_id,'')
+            ), -- pk
+        TRIM(REGEXP_REPLACE(SUBSTRING(SPLIT(other_proc_codes, ',')[other_proc_codes_exploder.n], 
+                    LOCATE('=>', SPLIT(other_proc_codes, ',')[other_proc_codes_exploder.n]) + 1, 
+                    LENGTH(SPLIT(other_proc_codes, ',')[other_proc_codes_exploder.n])), '"', '')) as other_proc_codes
     FROM transactional_raw transactional
-        ) all_codes
-    CROSS JOIN diagnosis_exploder rest_of_codes_exploder
-WHERE SPLIT(TRIM(all_codes.rest_of_codes), ',')[rest_of_codes_exploder.n] IS NULL
-    OR SPLIT(TRIM(all_codes.rest_of_codes), ',')[rest_of_codes_exploder.n] != ''
-    ;
+        CROSS JOIN diagnosis_exploder other_proc_codes_exploder
+    WHERE SPLIT(TRIM(transactional.other_proc_codes), ',')[other_proc_codes_exploder.n] IS NULL
+        OR SPLIT(TRIM(transactional.other_proc_codes), ',')[other_proc_codes_exploder.n] != ''
+        );
+
+-- insert rest of codes
+INSERT INTO exploded_proc_codes_nulls (
+    SELECT CONCAT(
+            COALESCE(all_codes.src_claim_id,''),
+            COALESCE(all_codes.src_svc_id,'')
+            ), 
+        TRIM(SPLIT(all_codes.rest_of_codes, ',')[rest_of_codes_exploder.n])
+    FROM (
+        SELECT DISTINCT
+            transactional.src_claim_id, transactional.src_svc_id, -- pk
+            CONCAT(
+                COALESCE(transactional.proc_cd,''), ',', 
+                COALESCE(transactional.prinpl_proc_cd,'')
+                ) as rest_of_codes
+        FROM transactional_raw transactional
+            ) all_codes
+        CROSS JOIN diagnosis_exploder rest_of_codes_exploder
+    WHERE SPLIT(TRIM(all_codes.rest_of_codes), ',')[rest_of_codes_exploder.n] IS NULL
+        OR SPLIT(TRIM(all_codes.rest_of_codes), ',')[rest_of_codes_exploder.n] != ''
+        );
 
 DROP TABLE IF EXISTS exploded_proc_codes_nulls;
 CREATE TABLE exploded_proc_codes_nulls (
@@ -40,15 +46,7 @@ CREATE TABLE exploded_proc_codes_nulls (
         proc_code string
         );
 
--- union will remove duplicates
-INSERT INTO exploded_proc_codes_nulls (
-    SELECT * FROM exploded_other_proc_codes
-    UNION
-    SELECT * FROM exploded_rest_of_codes
-        )
-    ;
-
--- strip out nulls for claim_svc_nums with populated codes
+-- strip out nulls for claim_svc_nums with populated codes, remove dupes
 DROP TABLE IF EXISTS exploded_proc_codes;
 CREATE TABLE exploded_proc_codes (
         claim_svc_num string,
