30fcd161dfaf248046dee9a528e55eafa9ca6129
Matt Usifer <musifer@healthverity.com>
2017-03-17 12:20:52 -0400

    Make import plural and add tmp_dir_func

--- airflow/dags/test/subdags/test_decrypt_file.py
+++ airflow/dags/test/subdags/test_decrypt_file.py
@@ -3,7 +3,9 @@ import unittest
 import mock
 import airflow.models
 import util.s3_utils as s3_utils
-import subdags.decrypt_file as decrypt_file
+import subdags.decrypt_files as decrypt_files
+
+reload(decrypt_files)
 
 
 class TestDecryptFileSubDag(unittest.TestCase):
@@ -14,7 +16,10 @@ class TestDecryptFileSubDag(unittest.TestCase):
         self.kwargs = {
             # test input args
             'tmp_path_template': 'base/{}/',
-            'ds_nodash': '20160101'
+            'ds_nodash': '20160101',
+            'tmp_dir_func': lambda ds, k: k['tmp_path_template'].format(
+                k['ds_nodash']
+            )
         }
         # save the state of s3_utils before mocking to prevent mocks
         # bleeding into other tests
@@ -34,18 +39,18 @@ class TestDecryptFileSubDag(unittest.TestCase):
 
         s3_utils.fetch_file_from_s3 = mock.MagicMock()
 
-        decrypt_file.do_fetch_decryption_files(self.ds, **self.kwargs)
+        decrypt_files.do_fetch_decryption_files(self.ds, **self.kwargs)
 
         s3_utils.fetch_file_from_s3.assert_has_calls([
             mock.call(
                 'location',
                 'base/' + self.kwargs['ds_nodash']
-                + '/' + decrypt_file.DECRYPTION_KEY
+                + '/' + decrypt_files.DECRYPTION_KEY
             ),
             mock.call(
                 'location',
                 'base/' + self.kwargs['ds_nodash']
-                + '/' + decrypt_file.DECRYPTOR_JAR
+                + '/' + decrypt_files.DECRYPTOR_JAR
             )
         ], any_order=True)
 
