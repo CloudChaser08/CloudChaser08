f3407cf0cd4f728df84326aad40f51adb28f4648
Matt Usifer <musifer@healthverity.com>
2017-04-21 17:01:21 -0400

    Add sample payload and fixture provider

---
+++ spark/test/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+
+from spark.spark_setup import init
+from spark.runner import Runner
+
+
+@pytest.fixture(scope="session")
+def spark(request):
+    spark, sqlContext = init("Tests", True)
+    runner = Runner(sqlContext)
+
+    yield {
+        "runner": runner,
+        "spark": spark,
+        "sqlContext": sqlContext
+    }
+
+    spark.stop()
