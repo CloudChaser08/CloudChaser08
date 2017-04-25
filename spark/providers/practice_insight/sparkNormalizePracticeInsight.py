e871084be6c23fd65dfdc58abfd25aedc6b17dad
Matt Usifer <musifer@healthverity.com>
2017-04-25 11:13:23 -0400

    Modifications to the norm script to enable testing

--- spark/providers/practice_insight/sparkNormalizePracticeInsight.py
+++ spark/providers/practice_insight/sparkNormalizePracticeInsight.py
@@ -8,81 +8,78 @@ import calendar
 from spark.runner import Runner
 from spark.spark_setup import init
 import spark.helpers.payload_loader as payload_loader
+import spark.helpers.file_utils as file_utils
 import spark.helpers.normalized_records_unloader as normalized_records_unloader
 import spark.helpers.explode as explode
 import spark.providers.practice_insight.udf as pi_udf
 
-
-def get_rel_path(relative_filename):
-    return os.path.abspath(
-        os.path.join(
-            os.path.dirname(__file__),
-            relative_filename
-        )
-    )
-
-
-# init
-spark, sqlContext = init("Practice Insight")
-
-# register practice insight udfs:
-sqlContext.registerFunction(
-    'generate_place_of_service_std_id', pi_udf.generate_place_of_service_std_id
-)
-sqlContext.registerFunction(
-    'generate_inst_type_of_bill_std_id',
-    pi_udf.generate_inst_type_of_bill_std_id
-)
-
-# initialize runner
-runner = Runner(sqlContext)
-
 TODAY = time.strftime('%Y-%m-%d', time.localtime())
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--date', type=str)
-parser.add_argument('--output_path', type=str)
-parser.add_argument('--shuffle_partitions', type=str, default="1200")
-parser.add_argument('--debug', default=False, action='store_true')
-args = parser.parse_args()
-
-date_obj = datetime.strptime(args.date, '%Y-%m-%d')
-
-input_path = 's3a://salusv/incoming/medicalclaims/practice_insight/{}/{}/'.format(
-    str(date_obj.year),
-    str(date_obj.month).zfill(2)
-)
-output_path = 's3a://salusv/warehouse/text/medicalclaims/2017-02-24/'
-
-if date_obj.year <= 2016:
-    max_date = str(date_obj.year) + '-12-31'
-    matching_path = 's3a://salusv/matching/payload/medicalclaims/practice_insight/{}/'.format(
-        str(date_obj.year)
-    )
-    setid = 'HV.data.837.' + str(date_obj.year) + '.csv.gz_' \
-            + str(date_obj.month)
-else:
-    max_date = date_obj.strftime('%Y-%m-') \
-               + str(calendar.monthrange(date_obj.year, date_obj.month)[1])
-    matching_path = 's3a://salusv/matching/payload/medicalclaims/practice_insight/{}/{}/'.format(
-        str(date_obj.year),
-        str(date_obj.month).zfill(2)
-    )
-    setid = 'HV.data.837.' + str(date_obj.year) + '.' \
-            + date_obj.strftime('%b').lower() + '.csv.gz'
-
-min_date = '2010-01-01'
-
-# create helper tables
-runner.run_spark_script(get_rel_path('create_helper_tables.sql'))
-payload_loader.load(runner, matching_path, ['claimId'])
+date_obj, input_path, min_date, max_date, matching_path, setid = None
 
 
-def run(part):
-    # Set shuffle partitions to stabilize job
-    sqlContext.setConf("spark.sql.shuffle.partitions", args.shuffle_partitions)
+def run_part(
+        spark, runner, part, date_input,
+        output_path, shuffle_partitions, test=False
+):
+    """
+    Normalize one of 4 parts of practice insight
+    """
 
-    runner.run_spark_script(get_rel_path(
+    # initialize globals on first part
+    if part == '1':
+        global date_obj, input_path, min_date, max_date, matching_path, setid
+
+        # register practice insight udfs:
+        runner.sqlContext.registerFunction(
+            'generate_place_of_service_std_id',
+            pi_udf.generate_place_of_service_std_id
+        )
+        runner.sqlContext.registerFunction(
+            'generate_inst_type_of_bill_std_id',
+            pi_udf.generate_inst_type_of_bill_std_id
+        )
+
+        # Set shuffle partitions to stabilize job
+        runner.sqlContext.setConf(
+            "spark.sql.shuffle.partitions", shuffle_partitions
+        )
+
+        date_obj = datetime.strptime(args.date, '%Y-%m-%d')
+
+        input_path = 's3a://salusv/incoming/medicalclaims/practice_insight/{}/{}/'.format(
+            str(date_obj.year),
+            str(date_obj.month).zfill(2)
+        )
+
+        if date_obj.year <= 2016:
+            max_date = str(date_obj.year) + '-12-31'
+            matching_path = 's3a://salusv/matching/payload/medicalclaims/practice_insight/{}/'.format(
+                str(date_obj.year)
+            )
+            setid = 'HV.data.837.' + str(date_obj.year) + '.csv.gz_' \
+                    + str(date_obj.month)
+        else:
+            max_date = date_obj.strftime('%Y-%m-') \
+                       + str(calendar.monthrange(date_obj.year, date_obj.month)[1])
+            matching_path = 's3a://salusv/matching/payload/medicalclaims/practice_insight/{}/{}/'.format(
+                str(date_obj.year),
+                str(date_obj.month).zfill(2)
+            )
+            setid = 'HV.data.837.' + str(date_obj.year) + '.' \
+                    + date_obj.strftime('%b').lower() + '.csv.gz'
+
+        min_date = '2010-01-01'
+
+        # create helper tables
+        runner.run_spark_script(
+            file_utils.get_rel_path('create_helper_tables.sql')
+        )
+        payload_loader.load(runner, matching_path, ['claimId'])
+
+    # end init #
+
+    runner.run_spark_script(file_utils.get_rel_path(
         '../../common/medicalclaims_common_model.sql'
     ), [
         ['table_name', 'medicalclaims_common_model', False],
@@ -90,16 +87,20 @@ def run(part):
     ])
 
     # load transactions and payload
-    runner.run_spark_script(get_rel_path('load_transactions.sql'), [
+    runner.run_spark_script(file_utils.get_rel_path('load_transactions.sql'), [
         ['input_path', input_path + part + '/']
     ])
 
     # create explosion maps
-    runner.run_spark_script(get_rel_path('create_exploded_diagnosis_map.sql'))
-    runner.run_spark_script(get_rel_path('create_exploded_procedure_map.sql'))
+    runner.run_spark_script(
+        file_utils.get_rel_path('create_exploded_diagnosis_map.sql')
+    )
+    runner.run_spark_script(
+        file_utils.get_rel_path('create_exploded_procedure_map.sql')
+    )
 
     # normalize
-    runner.run_spark_script(get_rel_path('normalize.sql'), [
+    runner.run_spark_script(file_utils.get_rel_path('normalize.sql'), [
         [
             'date_service_sl',
             """
@@ -176,18 +177,38 @@ def run(part):
     # explode date ranges
     explode.explode_medicalclaims_dates(runner)
 
-    normalized_records_unloader.partition_and_rename(
-        spark, runner, 'medicalclaims', 'medicalclaims_common_model.sql',
-        'practice_insight', 'medicalclaims_common_model', 'date_service',
-        args.date
-    )
+    if not test:
+        normalized_records_unloader.partition_and_rename(
+            spark, runner, 'medicalclaims', 'medicalclaims_common_model.sql',
+            'practice_insight', 'medicalclaims_common_model', 'date_service',
+            args.date
+        )
 
-    spark.catalog.dropTempView('medicalclaims_common_model')
+        spark.catalog.dropTempView('medicalclaims_common_model')
 
 
-for part in ['1', '2', '3', '4']:
-    run(part)
+def main(args):
+    # init
+    spark, sqlContext = init("Practice Insight")
 
-spark.stop()
+    # initialize runner
+    runner = Runner(sqlContext)
 
-normalized_records_unloader.distcp(output_path)
+    for part in ['1', '2', '3', '4']:
+        run_part(
+            spark, runner, part, args.date,
+            args.output_path, args.shuffle_partitions
+        )
+
+    spark.stop()
+
+    normalized_records_unloader.distcp(args.output_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--date', type=str)
+    parser.add_argument('--output_path', type=str)
+    parser.add_argument('--shuffle_partitions', type=str, default="1200")
+    args = parser.parse_args()
+    main(args)
