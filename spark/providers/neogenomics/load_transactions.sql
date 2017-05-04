42c842da73f34c0182d45647358d22b715ed493d
Matt Usifer <musifer@healthverity.com>
2017-05-04 16:57:57 -0400

    First pass at normalization routine

---
+++ spark/providers/neogenomics/load_transactions.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS transactional_tests;
+CREATE EXTERNAL TABLE transactional_tests 
+    hdr_rec_typ_cde          string,
+    patient_id               string,
+    test_order_id            string,
+    status                   string,
+    level_of_service         string,
+    technology               string,
+    specimen_type            string,
+    body_site                string,
+    icd_code                 string,
+    specimen_collected_date  string,
+    test_ordered_date        string,
+    test_reported_date       string,
+    test_canceled_date       string,
+    panel_name               string,
+    panel_code               string,
+    test_name                string,
+    test_code                string,
+    client_zip               string
+        ) 
+    ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+    WITH SERDEPROPERTIES (
+        'separatorChar' = '|'
+        )
+    STORED AS TEXTFILE
+    LOCATION {input_path}
+    ;
---
+++ spark/providers/neogenomics/normalize.sql
@@ -0,0 +1,191 @@
+-- insert all rows with diagnoses
+INSERT INTO lab_common_model
+SELECT
+    NULL,                                    -- record_id
+    t.test_order_id,                         -- claim_id
+    mp.hvid,                                 -- hvid
+    {today},                                 -- created
+    '3',                                     -- model_version
+    {filename},                              -- data_set
+    {feedname},                              -- data_feed
+    {vendor},                                -- data_vendor
+    '1',                                     -- source_version
+    mp.gender,                               -- patient_gender
+    cap_age(mp.age),                         -- patient_age
+    cap_year_of_birth(
+        mp.age,
+        CAST(extract_date(
+                t.test_ordered_date, '%m/%d/%Y', CAST({min_date} AS DATE), CAST({max_date} AS DATE)
+                ) AS DATE),
+        mp.yearOfBirth
+        ),                                   -- patient_year_of_birth
+    mp.threeDigitZip,                        -- patient_zip3
+    UPPER(mp.state),                         -- patient_state
+    extract_date(
+        t.test_ordered_date, '%m/%d/%Y', CAST({min_date} AS DATE), CAST({max_date} AS DATE)
+        ),                                   -- date_service
+    extract_date(
+        t.specimen_collected_date, '%m/%d/%Y', CAST({min_date} AS DATE), CAST({max_date} AS DATE)
+        ),                                   -- date_specimen
+    COALESCE(
+        extract_date(
+            t.test_canceled_date, '%m/%d/%Y', CAST({min_date} AS DATE), CAST({max_date} AS DATE)
+            ),
+        extract_date(
+            t.test_reported_date, '%m/%d/%Y', CAST({min_date} AS DATE), CAST({max_date} AS DATE)
+            )
+        ),                                   -- date_report
+    NULL,                                    -- time_report
+    NULL,                                    -- loinc_code
+    NULL,                                    -- lab_id
+    t.test_order_id,                         -- test_id
+    NULL,                                    -- test_number
+    t.panel_code,                            -- test_battery_local_id
+    NULL,                                    -- test_battery_std_id
+    t.panel_name,                            -- test_battery_name
+    NULL,                                    -- test_ordered_local_id
+    NULL,                                    -- test_ordered_std_id
+    CONCAT(t.test_name, ' ', t.technology),  -- test_ordered_name
+    NULL,                                    -- result_id
+    NULL,                                    -- result
+    NULL,                                    -- result_name
+    NULL,                                    -- result_unit_of_measure
+    NULL,                                    -- result_desc
+    t.level_of_service,                      -- result_comments
+    NULL,                                    -- ref_range_low
+    NULL,                                    -- ref_range_high
+    NULL,                                    -- ref_range_alpha
+    NULL,                                    -- abnormal_flag
+    NULL,                                    -- fasting_status
+    clean_up_diagnosis_code(
+        SPLIT(t.icd_code, ',')[n.n],
+        NULL,
+        CAST(extract_date(
+                t.test_ordered_date, '%m/%d/%Y', CAST({min_date} AS DATE), CAST({max_date} AS DATE)
+                ) as DATE)
+        ),                                   -- diagnosis_code
+    NULL,                                    -- diagnosis_code_qual
+    n.n,                                     -- diagnosis_code_priority
+    NULL,                                    -- procedure_code
+    NULL,                                    -- procedure_code_qual
+    NULL,                                    -- lab_npi
+    NULL,                                    -- ordering_npi
+    NULL,                                    -- payer_id
+    NULL,                                    -- payer_id_qual
+    NULL,                                    -- payer_name
+    NULL,                                    -- payer_parent_name
+    NULL,                                    -- payer_org_name
+    NULL,                                    -- payer_plan_id
+    NULL,                                    -- payer_plan_name
+    NULL,                                    -- payer_type
+    NULL,                                    -- lab_other_id
+    NULL,                                    -- lab_other_qual
+    NULL,                                    -- ordering_other_id
+    NULL,                                    -- ordering_other_qual
+    NULL,                                    -- ordering_market_type
+    NULL,                                    -- ordering_specialty
+    t.client_zip,                            -- ordering_zip
+    CASE WHEN UPPER(t.status) = 'CANCELLED'
+    THEN 'CANCELED'
+    ELSE NULL
+    END                                      -- logical_delete_reason
+FROM transactional_tests t
+    LEFT JOIN matching_payload mp ON t.test_order_id = mp.personid
+    CROSS JOIN diagnosis_exploder n
+
+-- implicit here is that q.diagnosis_code itself is not null or blank
+WHERE SPLIT(TRIM(t.icd_code),',')[n.n] IS NOT NULL
+    AND SPLIT(TRIM(q.icd_code),',')[n.n] != ''
+    ;
+
+-- insert all rows with diagnoses
+INSERT INTO lab_common_model
+SELECT
+    NULL,                                    -- record_id
+    t.test_order_id,                         -- claim_id
+    mp.hvid,                                 -- hvid
+    {today},                                 -- created
+    '3',                                     -- model_version
+    {filename},                              -- data_set
+    {feedname},                              -- data_feed
+    {vendor},                                -- data_vendor
+    '1',                                     -- source_version
+    mp.gender,                               -- patient_gender
+    cap_age(mp.age),                         -- patient_age
+    cap_year_of_birth(
+        mp.age,
+        CAST(extract_date(
+                t.test_ordered_date, '%m/%d/%Y', CAST({min_date} AS DATE), CAST({max_date} AS DATE)
+                ) AS DATE),
+        mp.yearOfBirth
+        ),                                   -- patient_year_of_birth
+    mp.threeDigitZip,                        -- patient_zip3
+    UPPER(mp.state),                         -- patient_state
+    extract_date(
+        t.test_ordered_date, '%m/%d/%Y', CAST({min_date} AS DATE), CAST({max_date} AS DATE)
+        ),                                   -- date_service
+    extract_date(
+        t.specimen_collected_date, '%m/%d/%Y', CAST({min_date} AS DATE), CAST({max_date} AS DATE)
+        ),                                   -- date_specimen
+    COALESCE(
+        extract_date(
+            t.test_canceled_date, '%m/%d/%Y', CAST({min_date} AS DATE), CAST({max_date} AS DATE)
+            ),
+        extract_date(
+            t.test_reported_date, '%m/%d/%Y', CAST({min_date} AS DATE), CAST({max_date} AS DATE)
+            )
+        ),                                   -- date_report
+    NULL,                                    -- time_report
+    NULL,                                    -- loinc_code
+    NULL,                                    -- lab_id
+    t.test_order_id,                         -- test_id
+    NULL,                                    -- test_number
+    t.panel_code,                            -- test_battery_local_id
+    NULL,                                    -- test_battery_std_id
+    t.panel_name,                            -- test_battery_name
+    NULL,                                    -- test_ordered_local_id
+    NULL,                                    -- test_ordered_std_id
+    CONCAT(t.test_name, ' ', t.technology),  -- test_ordered_name
+    NULL,                                    -- result_id
+    NULL,                                    -- result
+    NULL,                                    -- result_name
+    NULL,                                    -- result_unit_of_measure
+    NULL,                                    -- result_desc
+    t.level_of_service,                      -- result_comments
+    NULL,                                    -- ref_range_low
+    NULL,                                    -- ref_range_high
+    NULL,                                    -- ref_range_alpha
+    NULL,                                    -- abnormal_flag
+    NULL,                                    -- fasting_status
+    NULL,                                    -- diagnosis_code
+    NULL,                                    -- diagnosis_code_qual
+    NULL,                                    -- diagnosis_code_priority
+    NULL,                                    -- procedure_code
+    NULL,                                    -- procedure_code_qual
+    NULL,                                    -- lab_npi
+    NULL,                                    -- ordering_npi
+    NULL,                                    -- payer_id
+    NULL,                                    -- payer_id_qual
+    NULL,                                    -- payer_name
+    NULL,                                    -- payer_parent_name
+    NULL,                                    -- payer_org_name
+    NULL,                                    -- payer_plan_id
+    NULL,                                    -- payer_plan_name
+    NULL,                                    -- payer_type
+    NULL,                                    -- lab_other_id
+    NULL,                                    -- lab_other_qual
+    NULL,                                    -- ordering_other_id
+    NULL,                                    -- ordering_other_qual
+    NULL,                                    -- ordering_market_type
+    NULL,                                    -- ordering_specialty
+    t.client_zip,                            -- ordering_zip
+    CASE WHEN UPPER(t.status) = 'CANCELLED'
+    THEN 'CANCELED'
+    ELSE NULL
+    END                                      -- logical_delete_reason
+FROM transactional_tests t
+    LEFT JOIN matching_payload mp ON t.test_order_id = mp.personid
+
+WHERE q.diagnosis_code IS NULL
+    OR q.diagnosis_code = ''
+    ;
