4a00da8319b69ea907cf5c823492929bb6958190
Matt Usifer <musifer@healthverity.com>
2017-05-05 10:43:08 -0400

    Add matching payload link test

--- spark/test/providers/neogenomics/sparkNormalizeNeogenomics_test.py
+++ spark/test/providers/neogenomics/sparkNormalizeNeogenomics_test.py
@@ -15,13 +15,13 @@ def test_init(spark):
                                  .collect()
 
 
-def test_date_parsing(spark):
+def test_date_parsing():
     "Ensure that dates are correctly parsed"
     assert filter(lambda r: r.claim_id == 'test1', results)[0] \
         .date_service == datetime.date(2017, 4, 14)
 
 
-def test_diag_explosion(spark):
+def test_diag_explosion():
     "Ensure that diagnosis codes were exploded on '^'"
     diags = map(
         lambda r: str(r.diagnosis_code),
@@ -32,11 +32,17 @@ def test_diag_explosion(spark):
     assert diags == ['DIAG1', 'DIAG2', 'DIAG3']
 
 
-def test_nodiag_inclusion(spark):
+def test_nodiag_inclusion():
     "Ensure that claims with no diagnosis codes were included"
     claim = filter(lambda r: r.claim_id == 'test3', results)
 
     assert len(claim) == 1
+
+
+def test_matching_payload_link():
+    assert filter(lambda r: r.claim_id == 'test1', results)[0].hvid == '0001'
+    assert filter(lambda r: r.claim_id == 'test2', results)[0].hvid == '0002'
+    assert filter(lambda r: r.claim_id == 'test3', results)[0].hvid == '0003'
 
 
 def test_cleanup(spark):
