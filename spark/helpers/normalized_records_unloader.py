7afae61a0c71d27a8801474f24ad02e9a003eb70
Ilia Fishbein <ifishbein@healthverity.com>
2017-05-08 11:13:31 -0400

    Allow unloading to a specific partition value

--- spark/helpers/normalized_records_unloader.py
+++ spark/helpers/normalized_records_unloader.py
@@ -18,7 +18,7 @@ def mk_move_file(file_date, test=False):
     return move_file
 
 
-def partition_and_rename(spark, runner, data_type, common_model_script, provider, table_name, date_column, file_date, test_dir=None):
+def partition_and_rename(spark, runner, data_type, common_model_script, provider, table_name, date_column, file_date, partition_value=None, test_dir=None):
     """
     Unload normalized data into partitions based on
     a date column
@@ -35,18 +35,25 @@ def partition_and_rename(spark, runner, data_type, common_model_script, provider
         common_dirpath = '../../../../common/'
 
 
-    runner.run_spark_script(file_utils.get_rel_path(__file__, common_dirpath + '{}'.format(common_model_script)), [
+    runner.run_spark_script(file_utils.get_rel_path(__file__, common_dirpath + common_model_script), [
         ['table_name', 'final_unload', False],
         ['properties', constants.unload_properties_template.format(staging_dir), False]
     ])
-    runner.run_spark_script(file_utils.get_rel_path(__file__, common_dirpath + 'unload_common_model.sql'), [
-        ['select_statement', "SELECT *, '{}' as part_provider, 'NULL' as part_best_date FROM {} WHERE {} is NULL".format(provider, table_name, date_column), False],
-        ['partitions', '20', False]
-    ])
-    runner.run_spark_script(file_utils.get_rel_path(__file__, common_dirpath + 'unload_common_model.sql'), [
-        ['select_statement', "SELECT *, '{0}' as part_provider, regexp_replace({2}, '-..$', '') as part_best_date FROM {1} WHERE {2} IS NOT NULL".format(provider, table_name, date_column), False],
-        ['partitions', '20', False]
-    ])
+
+    if partition_value is None:
+        runner.run_spark_script(file_utils.get_rel_path(__file__, common_dirpath + 'unload_common_model.sql'), [
+            ['select_statement', "SELECT *, '{}' as part_provider, 'NULL' as part_best_date FROM {} WHERE {} is NULL".format(provider, table_name, date_column), False],
+            ['partitions', '20', False]
+        ])
+        runner.run_spark_script(file_utils.get_rel_path(__file__, common_dirpath + 'unload_common_model.sql'), [
+            ['select_statement', "SELECT *, '{0}' as part_provider, regexp_replace({2}, '-..$', '') as part_best_date FROM {1} WHERE {2} IS NOT NULL".format(provider, table_name, date_column), False],
+            ['partitions', '20', False]
+        ])
+    else:
+        runner.run_spark_script(file_utils.get_rel_path(__file__, common_dirpath + 'unload_common_model.sql'), [
+            ['select_statement', "SELECT *, '{}' as part_provider, '{}' as part_best_date FROM {}".format(provider, partition_value, table_name), False],
+            ['partitions', '20', False]
+        ])
 
     part_files = subprocess.check_output(part_files_cmd).strip().split("\n")
 
