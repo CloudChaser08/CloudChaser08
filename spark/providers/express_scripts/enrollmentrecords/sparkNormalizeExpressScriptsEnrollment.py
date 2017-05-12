6088c36da9ad743676bc87193df86d8829cc95b4
Ilia Fishbein <ifishbein@healthverity.com>
2017-05-12 16:02:05 -0400

    WIP Adding testing to ESI enrollment normalization

--- spark/providers/express_scripts/enrollmentrecords/sparkNormalizeExpressScriptsEnrollment.py
+++ spark/providers/express_scripts/enrollmentrecords/sparkNormalizeExpressScriptsEnrollment.py
@@ -10,11 +10,6 @@ import spark.helpers.file_utils as file_utils
 import spark.helpers.payload_loader as payload_loader
 import spark.helpers.normalized_records_unloader as normalized_records_unloader
 
-# init
-spark, sqlContext = init("Express Scripts")
-
-# initialize runner
-runner = Runner(sqlContext)
 
 TODAY = time.strftime('%Y-%m-%d', time.localtime())
 S3_EXPRESS_SCRIPTS_IN = 's3a://salusv/incoming/enrollmentrecords/express_scripts/'
@@ -26,75 +21,113 @@ S3A_REF_PHI = 's3a://salusv/reference/express_scripts_phi/'
 S3_REF_PHI = 's3://salusv/reference/express_scripts_phi/'
 LOCAL_REF_PHI = 'hdfs:///local_phi/'
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--date', type=str)
-args = parser.parse_args()
 
-setid = '10130X001_HV_RX_ENROLLMENT_D{}.txt'.format(args.date.replace('-',''))
+def run (spark, runnner, date_input, test=False):
+    setid = '10130X001_HV_RX_ENROLLMENT_D{}.txt'.format(args.date.replace('-',''))
 
-min_date = '2008-01-01'
-max_date = args.date
+    min_date = '2008-01-01'
+    max_date = args.date
 
-# create helper tables
-runner.run_spark_script(file_utils.get_rel_path(
-    __file__,
-    '../../../common/zip3_to_state.sql'
-))
-
-runner.run_spark_script(file_utils.get_rel_path(
+    # create helper tables
+    runner.run_spark_script(file_utils.get_rel_path(
         __file__,
-        '../../../common/enrollment_common_model.sql'
+        '../../../common/zip3_to_state.sql'
+    ))
+
+    runner.run_spark_script(file_utils.get_rel_path(
+            __file__,
+            '../../../common/enrollment_common_model.sql'
+        ), [
+            ['table_name', 'enrollment_common_model', False],
+            ['properties', '', False]
+    ])
+
+    date_path = args.date.replace('-', '/')
+
+    if test:
+        input_path = file_utils.get_rel_path(
+            __file__,
+            '../../test/providers/express_scripts/enrollment/resources/input/'
+        )
+        matching_path = file_utils.get_rel_path(
+            __file__,
+            '../../test/providers/express_scripts/enrollment/resources/matching/'
+        )
+        new_phi_path = file_utils.get_rel_path(
+            __file__,
+            '../../test/providers/express_scripts/enrollment/resources/new_phi/'
+        )
+        ref_phi_path = file_utils.get_rel_path(
+            __file__,
+            '../../test/providers/express_scripts/enrollment/resources/ref_phi/'
+        )
+    else:
+        input_path    = S3_EXPRESS_SCRIPTS_IN + date_path + '/'
+        matching_path = S3_EXPRESS_SCRIPTS_ENROLLMENT_MATCHING + date_path + '/'
+        new_phi_path  = S3_EXPRESS_SCRIPTS_RX_MATCHING + date_path + '/'
+        ref_phi_path  = S3A_REF_PHI
+
+
+    runner.run_spark_script(file_utils.get_rel_path(
+            __file__,
+            'load_enrollment_records.sql'
+        ), [
+            ['input_path', input_path]
+    ])
+
+    payload_loader.load(runner, matching_path, ['hvJoinKey', 'patientId'])
+
+    runner.run_spark_query('ALTER TABLE matching_payload RENAME TO new_phi')
+
+    runner.run_spark_script(file_utils.get_rel_path(
+            __file__,
+            'load_matching_payload.sql'
+        ), [
+            ['matching_path', matching_path] 
+    ])
+
+    subprocess.check_call(['hadoop', 'fs', '-rm', '-r', '-f', LOCAL_REF_PHI])
+    subprocess.check_call(['hadoop', 'fs', '-mkdir', LOCAL_REF_PHI])
+
+    runner.run_spark_script(file_utils.get_rel_path(
+            __file__,
+            'load_and_combine_phi.sql'
+        ), [
+            ['local_phi_path', LOCAL_REF_PHI],
+            ['s3_phi_path', ref_phi_path]
+    ])
+
+    runner.run_spark_script(file_utils.get_rel_path(
+        __file__, 'normalize.sql'
     ), [
-        ['table_name', 'enrollment_common_model', False],
-        ['properties', '', False]
-])
+        ['filename', setid],
+        ['today', TODAY],
+        ['feedname', '16'],
+        ['vendor', '17']
+    ])
 
-date_path = args.date.replace('-', '/')
+    normalized_records_unloader.partition_and_rename(spark, runner, 'enrollmentrecords', 'enrollment_common_model.sql',
+        'express_scripts', 'enrollment_common_model', 'date_service', args.date, args.date[:-3])
 
-runner.run_spark_script(file_utils.get_rel_path(
-        __file__,
-        'load_enrollment_records.sql'
-    ), [
-        ['input_path', S3_EXPRESS_SCRIPTS_IN + date_path + '/']
-])
+def main(args):
+    # init
+    spark, sqlContext = init("Express Scripts")
 
-payload_loader.load(runner, S3_EXPRESS_SCRIPTS_RX_MATCHING + date_path + '/', ['hvJoinKey', 'claimId', 'patientId'])
+    # initialize runner
+    runner = Runner(sqlContext)
 
-runner.run_spark_query('ALTER TABLE matching_payload RENAME TO new_phi')
+    run(spark, runner, args.date)
 
-runner.run_spark_script(file_utils.get_rel_path(
-        __file__,
-        'load_matching_payload.sql'
-    ), [
-        ['matching_path', S3_EXPRESS_SCRIPTS_ENROLLMENT_MATCHING + date_path + '/']
-])
+    spark.stop()
 
-subprocess.check_call(['hadoop', 'fs', '-rm', '-r', '-f', LOCAL_REF_PHI])
-subprocess.check_call(['hadoop', 'fs', '-mkdir', LOCAL_REF_PHI])
+    normalized_records_unloader.distcp(S3_EXPRESS_SCRIPTS_OUT)
 
-runner.run_spark_script(file_utils.get_rel_path(
-        __file__,
-        'load_and_combine_phi.sql'
-    ), [
-        ['local_phi_path', LOCAL_REF_PHI],
-        ['s3_phi_path', S3A_REF_PHI]
-])
+    # offload reference data
+    subprocess.check_call(['aws', 's3', 'rm', '--recursive', S3_REF_PHI])
+    subprocess.check_call(['s3-dist-cp', '--s3ServerSideEncryption', '--src', LOCAL_REF_PHI, '--dest', S3A_REF_PHI])
 
-runner.run_spark_script(file_utils.get_rel_path(
-    __file__, 'normalize.sql'
-), [
-    ['filename', setid],
-    ['today', TODAY],
-    ['feedname', '16'],
-    ['vendor', '17']
-])
-
-normalized_records_unloader.partition_and_rename(spark, runner, 'enrollmentrecords', 'enrollment_common_model.sql',
-    'express_scripts', 'enrollment_common_model', 'date_service', args.date, args.date[:-3])
-
-spark.sparkContext.stop()
-
-normalized_records_unloader.distcp(S3_EXPRESS_SCRIPTS_OUT)
-
-subprocess.check_call(['aws', 's3', 'rm', '--recursive', S3_REF_PHI])
-subprocess.check_call(['s3-dist-cp', '--s3ServerSideEncryption', '--src', LOCAL_REF_PHI, '--dest', S3A_REF_PHI])
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--date', type=str)
+    args = parser.parse_args()
+    main(args)
---
+++ spark/test/providers/express_scripts/enrollmentrecords/sparkNormalizeExpressScripts_test.py
@@ -0,0 +1,35 @@
+import pytest
+import datetime
+import spark.providers.express_scripts.enrollment.sparkNormalizeExpressScriptsEnrollment as esi
+
+results  = []
+ref_data = []
+@pytest.mark.usefixtures("spark")
+def test_init(spark):
+    esi.run(spark['spark'], spark['runner'], '2017-05-01', True)
+    global results
+    results  = spark['sqlContext'].sql('select * from enrollment_common_model') \
+                                 .collect()
+    ref_data = spark['sqlContext'].sql('select * from local_phi') \
+                                 .collect()
+
+def test_phi_merge():
+    "Check that old reference data is merged with new reference data" 
+    assert len(ref_data) == 8
+
+def test_no_orphaned_records():
+    """Check that enrollment records where the patient_id doesn't match
+    any patients in the reference data aren't in the final table"""
+    assert len(filter(lambda r: r.start_date == datetime.date(2017, 4, 22))) == 0
+
+def test_start_date_parsing():
+    "Check that start_date is parsed and not capped even when it's absurd"
+    assert len(filter(lambda r: r.start_date == datetime.date(1856, 1, 1))) != 0
+
+def test_end_date_parsing():
+    "Check that start_date is parsed and not capped even when it's absurd"
+    assert len(filter(lambda r: r.start_date == datetime.date(3000, 1, 1))) != 0
+
+def test_cleanup(spark):
+    spark['sqlContext'].dropTempTable('lab_common_model')
+
