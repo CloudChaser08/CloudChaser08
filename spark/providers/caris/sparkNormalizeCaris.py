57e96e89df165dd6d908e579403f2d5d1495de56
Matt Usifer <musifer@healthverity.com>
2017-05-19 16:04:01 -0400

    Add test

--- spark/providers/caris/sparkNormalizeCaris.py
+++ spark/providers/caris/sparkNormalizeCaris.py
@@ -11,103 +11,127 @@ import spark.helpers.normalized_records_unloader as normalized_records_unloader
 from spark.spark_setup import init
 from spark.runner import Runner
 
-# init
-spark, sqlContext = init("Caris")
-
-# initialize runner
-runner = Runner(sqlContext)
-
 TODAY = time.strftime('%Y-%m-%d', time.localtime())
+output_path = 's3://salusv/warehouse/parquet/labtests/2017-02-16/'
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--date', type=str)
-args = parser.parse_args()
 
-date_obj = datetime.strptime(args.date, '%Y-%m-%d')
+def run(spark, runner, date_input, test=False):
 
-input_path = 's3a://salusv/incoming/labtests/caris/{year}/{month}/'.format(
-    year=str(date_obj.year),
-    month=str(date_obj.month).zfill(2)
-)
-output_path = 's3a://salusv/warehouse/text/labtests/2017-02-16/'
+    date_obj = datetime.strptime(date_input, '%Y-%m-%d')
 
-matching_path = 's3a://salusv/matching/payload/labtests/caris/{year}/{month}/'.format(
-    year=str(date_obj.year),
-    month=str(date_obj.month).zfill(2)
-)
-addon_path = 's3a://salusv/incoming/labtests/caris/hist_additional_columns/'
+    script_path = __file__
 
-script_path = __file__
+    if test:
+        if date_input <= '2017-04-01':
+            input_path = file_utils.get_rel_path(
+                script_path, '../../test/providers/caris/resources/input-legacy/'
+            ) + '/'
+            addon_path = file_utils.get_rel_path(
+                script_path, '../../test/providers/caris/resources/addon/'
+            ) + '/'
+        else:
+            input_path = file_utils.get_rel_path(
+                script_path, '../../test/providers/caris/resources/input/'
+            ) + '/'
 
-setid = 'DATA_' + str(date_obj.year) \
-        + str(date_obj.month).zfill(2) + '01' \
-        if args.date != '2016-08-01' else 'Data_7_29'
+        matching_path = file_utils.get_rel_path(
+            script_path, '../../test/providers/caris/resources/matching/'
+        ) + '/'
 
-min_date = '2005-01-01'
-max_date = date_obj.strftime('%Y-%m-') \
-           + str(calendar.monthrange(date_obj.year, date_obj.month)[1])
+    else:
+        input_path = 's3a://salusv/incoming/labtests/caris/{year}/{month}/'.format(
+            year=str(date_obj.year),
+            month=str(date_obj.month).zfill(2)
+        )
+        matching_path = 's3a://salusv/matching/payload/labtests/caris/{year}/{month}/'.format(
+            year=str(date_obj.year),
+            month=str(date_obj.month).zfill(2)
+        )
+        addon_path = 's3a://salusv/incoming/labtests/caris/hist_additional_columns/'
 
-staging_dir = 'hdfs:///text-out/'
+    setid = 'DATA_' + str(date_obj.year) \
+            + str(date_obj.month).zfill(2) + '01' \
+            if date_input != '2016-08-01' else 'Data_7_29'
 
-runner.run_spark_script('../../common/zip3_to_state.sql')
+    min_date = '2005-01-01'
+    max_date = date_obj.strftime('%Y-%m-') \
+        + str(calendar.monthrange(date_obj.year, date_obj.month)[1])
 
-runner.run_spark_script('../../common/lab_common_model.sql', [
-    ['table_name', 'lab_common_model', False],
-    ['properties', '', False]
-])
+    runner.run_spark_script('../../common/zip3_to_state.sql')
 
-payload_loader.load(runner, matching_path, ['hvJoinKey'])
-
-# append additional columns
-if args.date <= '2017-04-01':
-    runner.run_spark_script('load_transactions_legacy.sql', [
-        ['input_path', input_path]
+    runner.run_spark_script('../../common/lab_common_model.sql', [
+        ['table_name', 'lab_common_model', False],
+        ['properties', '', False]
     ])
 
-    runner.run_spark_script('load_additional_columns.sql', [
-        ['addon_path', addon_path]
-    ])
+    payload_loader.load(runner, matching_path, ['hvJoinKey'])
 
-    # merge tables
-    sqlContext.sql("""
-    SELECT t.*, a.ods_id as ods_id,
-    a.accession_date AS accession_date,
-    a.sign_out_date AS sign_out_date
-    FROM raw_transactional t
-    LEFT JOIN additional_columns a
-    ON t.customer__patient_id = a.patient_id
-    """).createTempView('raw_transactional')
-else:
-    runner.run_spark_script(
-        file_utils.get_rel_path(script_path, 'load_transactions.sql'), [
+    # append additional columns
+    if args.date <= '2017-04-01':
+        runner.run_spark_script('load_transactions_legacy.sql', [
             ['input_path', input_path]
-        ]
-    )
+        ])
 
-    sqlContext.sql('select * from raw_transactional')   \
-              .withColumn('ods_id', col('deid'))        \
-              .withColumn('sign_out_date', lit(None))   \
-              .createTempView('raw_transactional')
+        runner.run_spark_script('load_additional_columns.sql', [
+            ['addon_path', addon_path]
+        ])
 
-runner.run_spark_script('normalize.sql', [
-    ['filename', setid],
-    ['today', TODAY],
-    ['feedname', '14'],
-    ['vendor', '13'],
-    ['date_received', args.date],
-    ['min_date', min_date],
-    ['max_date', max_date]
-])
+        # merge tables
+        runner.sqlContext.sql("""
+        SELECT t.*, a.ods_id as ods_id,
+        a.accession_date AS accession_date,
+        a.sign_out_date AS sign_out_date
+        FROM raw_transactional t
+        LEFT JOIN additional_columns a
+        ON t.customer__patient_id = a.patient_id
+        """).createTempView('raw_transactional')
+    else:
+        runner.run_spark_script('load_transactions.sql', [
+            ['input_path', input_path]
+        ])
 
-sqlContext.sql('select * from lab_common_model').withColumn(
-    'record_id', monotonically_increasing_id()
-).createTempView('lab_common_model')
+        runner.sqlContext.sql('select * from raw_transactional')   \
+                         .withColumn('ods_id', col('deid'))        \
+                         .withColumn('sign_out_date', lit(None))   \
+                         .createTempView('raw_transactional')
 
-normalized_records_unloader.partition_and_rename(
-    spark, runner, 'lab', 'lab_common_model.sql', 'caris', 'lab_common_model',
-    'date_service', args.date
-)
+    runner.run_spark_script('normalize.sql', [
+        ['filename', setid],
+        ['today', TODAY],
+        ['feedname', '14'],
+        ['vendor', '13'],
+        ['date_received', date_input],
+        ['min_date', min_date],
+        ['max_date', max_date]
+    ])
 
-spark.stop()
+    runner.sqlContext.sql('select * from lab_common_model').withColumn(
+        'record_id', monotonically_increasing_id()
+    ).createTempView('lab_common_model')
 
-normalized_records_unloader.distcp(output_path)
+    if not test:
+        normalized_records_unloader.partition_and_rename(
+            spark, runner, 'lab', 'lab_common_model.sql', 'caris', 'lab_common_model',
+            'date_service', date_input
+        )
+
+
+def main(args):
+    # init
+    spark, sqlContext = init("Caris")
+
+    # initialize runner
+    runner = Runner(sqlContext)
+
+    run(spark, runner, args.date)
+
+    spark.stop()
+
+    normalized_records_unloader.distcp(output_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--date', type=str)
+    args = parser.parse_args()
+    main(args)
---
+++ spark/test/providers/caris/sparkNormalizeCaris_test.py
@@ -0,0 +1,59 @@
+import pytest
+
+import datetime
+
+import spark.providers.caris.sparkNormalizeCaris as caris
+
+results = []
+
+
+@pytest.mark.usefixtures("spark")
+def test_init(spark):
+    # new model run
+    caris.run(spark['spark'], spark['runner'], '2017-05-01', True)
+    global results
+    results = spark['sqlContext'].sql('select * from lab_common_model') \
+                                 .collect()
+
+
+def test_year_of_birth_cap():
+
+    assert filter(lambda r: r.claim_id == '2073344007_17897', results)[0] \
+        .patient_year_of_birth == '1927'
+
+
+def test_date_parsing():
+    "Ensure that dates are correctly parsed"
+    assert filter(lambda r: r.claim_id == '2073344007_17897', results)[0] \
+        .date_service == datetime.date(2016, 12, 1)
+
+
+def test_diag_explosion():
+    "Ensure that diagnosis codes were exploded on '^'"
+    diags = map(
+        lambda r: str(r.diagnosis_code),
+        filter(lambda r: r.claim_id == '2073344012_17897', results)
+    )
+    diags.sort()
+
+    assert diags == ['DIAG1', 'DIAG4', 'DIAG6']
+
+
+def test_nodiag_inclusion():
+    "Ensure that claims with no diagnosis codes were included"
+    claim = filter(lambda r: r.claim_id == '2073344008_17897', results)
+
+    assert len(claim) == 1
+
+
+def test_diagnosis_qual_translation():
+    "Ensure that diagnosis code qualifiers were correctly translated to HV"
+    assert filter(lambda r: r.claim_id == '2073344009_17897', results)[0] \
+        .diagnosis_code_qual == '02'
+
+    assert filter(lambda r: r.claim_id == '2073344012_17897', results)[0] \
+        .diagnosis_code_qual == '01'
+
+
+def test_cleanup(spark):
+    spark['sqlContext'].dropTempTable('lab_common_model')
