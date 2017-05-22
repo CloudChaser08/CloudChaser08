5c440fc0f65bf74062655efba22a91df7546d94d
Matt Usifer <musifer@healthverity.com>
2017-05-22 09:16:53 -0400

    Add working tests

--- spark/test/providers/caris/sparkNormalizeCaris_test.py
+++ spark/test/providers/caris/sparkNormalizeCaris_test.py
@@ -16,43 +16,16 @@ def test_init(spark):
                                  .collect()
 
 
-def test_year_of_birth_cap():
-
-    assert filter(lambda r: r.claim_id == '2073344007_17897', results)[0] \
-        .patient_year_of_birth == '1927'
-
-
 def test_date_parsing():
-    "Ensure that dates are correctly parsed"
-    assert filter(lambda r: r.claim_id == '2073344007_17897', results)[0] \
-        .date_service == datetime.date(2016, 12, 1)
+    assert filter(lambda r: r.claim_id == 'patient-0_deid-0', results)[0] \
+        .date_service == datetime.date(2017, 3, 25)
 
 
-def test_diag_explosion():
-    "Ensure that diagnosis codes were exploded on '^'"
-    diags = map(
-        lambda r: str(r.diagnosis_code),
-        filter(lambda r: r.claim_id == '2073344012_17897', results)
-    )
-    diags.sort()
-
-    assert diags == ['DIAG1', 'DIAG4', 'DIAG6']
-
-
-def test_nodiag_inclusion():
-    "Ensure that claims with no diagnosis codes were included"
-    claim = filter(lambda r: r.claim_id == '2073344008_17897', results)
-
-    assert len(claim) == 1
-
-
-def test_diagnosis_qual_translation():
-    "Ensure that diagnosis code qualifiers were correctly translated to HV"
-    assert filter(lambda r: r.claim_id == '2073344009_17897', results)[0] \
-        .diagnosis_code_qual == '02'
-
-    assert filter(lambda r: r.claim_id == '2073344012_17897', results)[0] \
-        .diagnosis_code_qual == '01'
+def test_labtests_translated():
+    assert len(filter(
+        lambda r: r.claim_id == 'patient-1_deid-1' and r.test_ordered_name == 'NGS_OFFERING',
+        results
+    ))
 
 
 def test_cleanup(spark):
