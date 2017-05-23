da3a7ea5e3d560be6b7e8bbbb0dfa7953eb88e18
Matt Usifer <musifer@healthverity.com>
2017-05-23 14:18:57 -0400

    Add legacy test

--- spark/test/providers/caris/sparkNormalizeCaris_test.py
+++ spark/test/providers/caris/sparkNormalizeCaris_test.py
@@ -4,16 +4,36 @@ import datetime
 
 import spark.providers.caris.sparkNormalizeCaris as caris
 
+legacy_results = []
 results = []
+
+
+def cleanup(spark):
+    spark['sqlContext'].dropTempTable('lab_common_model')
+    spark['sqlContext'].dropTempTable('raw_transactional')
 
 
 @pytest.mark.usefixtures("spark")
 def test_init(spark):
     # new model run
+    caris.run(spark['spark'], spark['runner'], '2017-04-01', True)
+    global legacy_results
+    legacy_results = spark['sqlContext'].sql('select * from lab_common_model') \
+                                        .collect()
+
+    cleanup(spark)
+
     caris.run(spark['spark'], spark['runner'], '2017-05-01', True)
     global results
     results = spark['sqlContext'].sql('select * from lab_common_model') \
                                  .collect()
+
+
+def test_legacy_date_parsing():
+    assert filter(lambda r: r.claim_id == 'patient-6_ods-6', legacy_results)[0] \
+        .date_service == datetime.date(2005, 4, 20)
+    assert filter(lambda r: r.claim_id == 'patient-7_ods-7', legacy_results)[0] \
+        .date_service == datetime.date(2017, 3, 17)
 
 
 def test_date_parsing():
@@ -29,4 +49,4 @@ def test_labtests_translated():
 
 
 def test_cleanup(spark):
-    spark['sqlContext'].dropTempTable('lab_common_model')
+    cleanup(spark)
