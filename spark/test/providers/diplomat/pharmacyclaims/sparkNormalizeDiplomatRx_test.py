9d40e81a1044bf317f351a9928a776fb560eff46
Matt Usifer <musifer@healthverity.com>
2017-06-20 09:54:42 -0400

    Add min cap tests

--- spark/test/providers/diplomat/pharmacyclaims/sparkNormalizeDiplomatRx_test.py
+++ spark/test/providers/diplomat/pharmacyclaims/sparkNormalizeDiplomatRx_test.py
@@ -1,5 +1,6 @@
 import pytest
 
+import datetime
 import spark.providers.diplomat.pharmacyclaims.sparkNormalizeDiplomatRx as diplomat
 
 results = []
@@ -22,6 +23,17 @@ def test_embedded_commas():
     "Ensure that rows with embedded columns were filtered out"
     assert not filter(lambda r: r.claim_id == 'claim-4', results)
     assert len(results) == 9
+
+
+def test_date_written_mincap():
+    "Ensure that date written has a lower min cap"
+    assert filter(lambda r: r.claim_id == 'claim-3', results)[0].date_written == datetime.date(2004, 3, 5)
+
+
+def test_all_date_mincap():
+    "Ensure that dates are min capped"
+    assert not filter(lambda r: r.claim_id == 'claim-9', results)[0].date_service
+    assert filter(lambda r: r.claim_id == 'claim-8', results)[0].date_service == datetime.date(2015, 3, 2)
 
 
 def test_cleanup(spark):
