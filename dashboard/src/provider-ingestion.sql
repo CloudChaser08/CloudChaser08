43b4b5fe6ad8800b639e18126fcebc9e8f1f2728
Matt Usifer <musifer@healthverity.com>
2017-06-26 16:08:56 +0000

    Deduplicate task instances

--- dashboard/src/provider-ingestion.sql
+++ dashboard/src/provider-ingestion.sql
@@ -10,8 +10,21 @@ FROM (
     SELECT split_part(dag_id, '.', 1) as true_dag_id,
         execution_date::date as success_date,
         SUM(CASE WHEN state IN ('success', 'skipped') THEN 1 ELSE 0 END) as total_success_count,
-        SUM(CASE WHEN state NOT IN ('success', 'skipped') THEN 1 ELSE 0 END) as total_nonsuccess_count
-    FROM task_instance
+        SUM(CASE WHEN state NOT IN ('success', 'skipped') AND NOT EXISTS (
+
+            -- ignore this failed task instance if the same task
+            -- exists in the DB with a 'success' state for this
+            -- execution date
+            SELECT ti2.task_id
+            FROM task_instance ti2
+            WHERE ti.task_id = ti2.task_id
+                AND ti.dag_id = ti2.dag_id
+                AND ti.execution_date::date = ti2.execution_date::date
+                AND ti2.state = 'success'
+
+                ) THEN 1 ELSE 0
+            END) as total_nonsuccess_count
+    FROM task_instance ti
     GROUP BY split_part(dag_id, '.', 1),
         execution_date::date
     ORDER BY execution_date::date
