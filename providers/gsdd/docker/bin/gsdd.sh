81a84efe04178d974428e32a38d48c6c85ccc659
Kyle Halpin <khalpin@healthverity.com>
2017-07-06 23:19:48 +0000

    copy up now puts files in named subdirectories, the names are files minus extensions

--- providers/gsdd/docker/bin/gsdd.sh
+++ providers/gsdd/docker/bin/gsdd.sh
@@ -21,9 +21,18 @@ GSDDUpdate
 GSDDServerStart
 GSDDExport
 
-for x in $(ls /opt/goldstandard/gsdd5/Export/Table/) ; 
-	do aws s3 cp /opt/goldstandard/gsdd5/Export/Table/$x s3://salusv/reference/gsdd/
+
+for x in $(ls /opt/goldstandard/gsdd5/Export/Table/); do
+	# get short name
+	base_name=$(basename $x);
+
+	# lop off the file extension
+	dir_name=${base_name::-4};
+
+	# copy with dir name
+	aws s3 cp /opt/goldstandard/gsdd5/Export/Table/$x s3://salusv/reference/gsdd/${dir_name}/$x;
 done
+
 
 # now, we source the env var info, and run this into redshift
 git clone git@github.com:healthverity/dewey.git
