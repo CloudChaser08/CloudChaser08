495d81df8a21f8af77e09a45c87abe3919a4d64f
Joe Illuminati <illumijo@gmail.com>
2017-07-19 14:47:23 -0400

    Load Transactions Script

---
+++ spark/providers/mindbody/load_transactions.sql
@@ -0,0 +1,30 @@
+DROP TABLE IF EXISTS transactional_mindbody;
+
+CREATE EXTERNAL TABLE transactional_mindbody(
+    claim_id                string,
+    first_name              string,
+    last_name               string,
+    address                 string,
+    city                    string,
+    state                   string,
+    zip                     string,
+    studioVertical          string,
+    location_zip            string,
+    gender                  string,
+    birthdate               string,
+    visitClassDate          string,
+    sessionStart            string,
+    sessionStop             string,
+    visitTypeGroupName      string,
+    visitTypeName           string,
+    visitClassName          string,
+    visitRevenue            string,
+    hv_linking_id           string
+)
+ROW FORMAT SERDE "org.apache.hadoop.hive.serde2.OpenCSVSerde"
+SERDEPROPERTIES (
+    "separatorChar" = "|"
+)
+STORED AS TEXTFILE
+LOCATION {input_path}
+;
