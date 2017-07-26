03ca92a39ac5d80f612fcad3f017ea0d0c7b62c8
Joe Illuminati <illumijo@gmail.com>
2017-07-26 13:45:14 -0400

    Took stuff out that should be done in post processing

--- spark/providers/mindbody/normalize.sql
+++ spark/providers/mindbody/normalize.sql
@@ -2,17 +2,14 @@ INSERT INTO event_common_model
 SELECT
     NULL,                              -- record_id
     p.hvid,                            -- hvid
-    current_date(),                    -- created
-    '2',                               -- model_version
+    NULL,                              -- created
+    '3',                               -- model_version
     NULL,                              -- data_set
     NULL,                              -- data_feed
     NULL,                              -- data_vendor 
     NULL,                              -- source_version
-    cap_age(p.age),                    -- patient_age 
-    cap_year_of_birth(
-        p.age,
-        current_date(),
-        p.yearOfBirth),                -- patient_year_of_birth
+    p.age,                             -- patient_age 
+    p.yearOfBirth,                     -- patient_year_of_birth
     p.threeDigitZip,                   -- patient_zip3
     p.state,                           -- patient_state
     p.gender,                          -- patient_gender
@@ -22,9 +19,7 @@ SELECT
     t.studioVertical,                  -- event
     t.visitClassName,                  -- event_val
     t.visitTypeGroupName,              -- event_val_uom
-    t.visitClassDate,                  -- event_date
-    'Private Source 133',              -- part_provider
-    current_date()                     -- part_best_date
+    t.visitClassDate                   -- event_date
     
 FROM transactional_mindbody t
 LEFT JOIN matching_payload p
