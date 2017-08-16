1b646fb60b29702470a86ea604d817471e3f3d85
Matt Usifer <musifer@healthverity.com>
2017-08-16 17:01:17 -0400

    Fix existing tests

--- spark/test/providers/emdeon/medicalclaims/sparkNormalizeEmdeonDx_test.py
+++ spark/test/providers/emdeon/medicalclaims/sparkNormalizeEmdeonDx_test.py
@@ -32,12 +32,13 @@ def test_unrelated_professional_explosions():
     professional claim for unrelated diagnoses
 
     These diagnoses should be exploded by date based on the full claim
-    (2017-01-01 - 2017-01-03)
+     (2017-01-01 - 2017-01-03)
     """
     for unrelated_diag in ['DIAG5', 'DIAGADMIT']:
-        assert sorted([(res.date_service, res.date_service_end)
-                       for res in results if res.claim_id == 'claim-0' and res.diagnosis_code == 'DIAG5']) \
-                           == [('2017-01-01', '2017-01-01'), ('2017-01-02', '2017-01-02'), ('2017-01-03', '2017-01-03')]
+        assert sorted([(res.date_service.strftime('%Y-%m-%d'), res.date_service_end.strftime('%Y-%m-%d'))
+                       for res in results if res.claim_id == 'claim-0' and res.diagnosis_code == unrelated_diag]) \
+                           == [('2017-01-01', '2017-01-01'), ('2017-01-02', '2017-01-02'),
+                               ('2017-01-03', '2017-01-03')]
 
 
 def test_related_professional_explosions():
@@ -46,14 +47,15 @@ def test_related_professional_explosions():
 
     These diagnoses are related to service line 1 and so they should
      be exploded by date based on the dates on that service line
-     (2017-01-01 - 2017-01-04)
+     (2017-01-01 - 2017-01-05)
     """
 
     for related_diag in ['DIAGPRIMARY', 'DIAG2', 'DIAG3']:
-        assert sorted([(res.date_service, res.date_service_end)
-                       for res in results if res.claim_id == 'claim-0' and res.diagnosis_code == 'DIAG5']) \
+        assert sorted([(res.date_service.strftime('%Y-%m-%d'), res.date_service_end.strftime('%Y-%m-%d'))
+                       for res in results if res.claim_id == 'claim-0' and res.diagnosis_code == related_diag]) \
                            == [('2017-01-01', '2017-01-01'), ('2017-01-02', '2017-01-02'),
-                               ('2017-01-03', '2017-01-03'), ('2017-01-04', '2017-01-04')]
+                               ('2017-01-03', '2017-01-03'), ('2017-01-04', '2017-01-04'),
+                               ('2017-01-05', '2017-01-05')]
 
 
 def test_cleanup(spark):
