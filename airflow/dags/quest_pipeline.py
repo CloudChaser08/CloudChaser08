be73b5e6e4896de0dad9bee990ac000e4c7eb913
Matt Usifer <musifer@healthverity.com>
2017-08-23 10:01:34 -0400

    Fix quest typo

--- airflow/dags/quest_pipeline.py
+++ airflow/dags/quest_pipeline.py
@@ -404,7 +404,7 @@ detect_move_normalize_dag = SubDagOperator(
 
 sql_new_template = """
     ALTER TABLE labtests_20170216 ADD PARTITION (part_provider='quest', part_best_date='{0}-{1}')
-    LOCATION 's3a://salusv/warehouse/parquet/labtests/2017-02-16/part_provider=quest/part_best_data={0}-{1}/'
+    LOCATION 's3a://salusv/warehouse/parquet/labtests/2017-02-16/part_provider=quest/part_best_date={0}-{1}/'
 """
 
 if HVDAG.HVDAG.airflow_env != 'test':
