864a63f3a244ae2b6dc545f7705218ca5641e57b
Joe Illuminati <illumijo@gmail.com>
2017-08-31 17:40:09 -0400

    Splitting stuff out based on needing to do two mappings.  Service Line mapping done.

--- spark/providers/cardinal_pms/medical_claims/sparkNormalizeCardinalPMS.py
+++ spark/providers/cardinal_pms/medical_claims/sparkNormalizeCardinalPMS.py
@@ -1,26 +1,28 @@
 #! /usr/bin/python
 import argparse
-import spark.helpers.file_utils as file_utils
+
 from spark.runner import Runner
 from spark.spark_setup import init
 
+import spark.helpers.file_utils as file_utils
 import spark.helpers.normalized_records_unloader as normalized_records_unloader
+import spark.helpers.explode as explode
+import spark.helpers.postprocessor as postprocessor
+import spark.helpers.privacy.medicalclaims as medical_priv
 
 def run(spark, runner, date_input, test=False, airflow_test=False):
     script_path = __file__
 
-    set_id = 'TBD' #TODO: fill in with file format when found out
-
     if test:
         input_path = file_utils.get_abs_path(
-            script_path = '../../test/providers/cardinal_pms/resources/input/'
+            script_path = '../../test/providers/cardinal_pms/medicalclaims/resources/input/'
         )
     elif airflow_test:
         input_path = 's3://salusv/testing/dewey/airflow/e2e/cardinal_pms/out/{}'\
-                        .format(date_path)
+                        .format(date_input.replace('-', '/')
     else:
-        input_path = 's3://salusv/incoming/consumer/mindbody/{}/'\
-                        .format(date_path)
+        input_path = 's3://salusv/incoming/medicalclaims/cardinal_pms/{}/'\
+                        .format(date_input.replace('-', '/')
 
     # Note: This routine does not contain any matching payloads
 
@@ -36,22 +38,41 @@ def run(spark, runner, date_input, test=False, airflow_test=False):
     ])
 
     # Remove leading and trailing whitespace from any strings
-    postprocessor.trimmify(runner.sqlContext.sql('select * from claim'))\
-                    .createTempView('claim')
-    postprocessor.trmmify(runner.sqlContext.sql('seelct * from claim_lines'))\
-                    .createTempView('claim_lines')
-    posprocessor.trimmify(runner.sqlContext.sql('select * from claim_files'))\
-                    .createTempView('claim_files')
+    postprocessor.compose(postprocessor.trimmify, postprocessor.nullify)(
+        runner.sqlContext.sql('select * from transactional_cardinal_pms')
+    ).createTempView('transactional_cardinal_pms')
 
-    # Normalize the transactional data into the
-    # medical claim common model
-    runner.run_spark_script('normalize.sql', [])
+    # Create exploder table for service-line
+    explode.generate_exploder_table(spark, 5, 'service_line_exploder')
+
+    # Normalize service-line
+    runner.run_spark_script('normalize_service_line.sql', [])
+
+    # Create exploder table for claim
+    explode.generate_exploder_table(spark, 9, 'claim_exploder')
+
+    # Normalize claim
+    runner.run_spark_script('normalize_claim.sql', [])
 
     # Postprocessing
     postprocessor.compose(
         postprocessor.nullify,
-        postprocessor.add_universal_column(feed_id='41', vendor_id='188', filename=set_id)
-    )
+        postprocessor.add_universal_column(
+            feed_id='41',
+            vendor_id='188',
+            # TODO: this is not right, fix when we know what the name format is.
+            filename='PMS_Claims_{}.psv'.format(date_obj.strftime('%Y%m%d'))
+        ),
+        medical_priv.filter
+    )(
+        runner.sqlContext.sql('select * from medicalclaims_common_model')
+    ).createTempView('medicalclaims_common_model')
+    
+    if not test:
+        normalized_records_unloader.partition_and_rename(
+            spark, runner, 'medicalclaims', 'medicalclaims_common_model.sql', 'cardinal_pms',
+            'medicalclaims_common_model', 'date_service', date_input
+        )
 
 
 def main(args):
