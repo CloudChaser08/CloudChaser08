39f6d95464d54d10ae7644a1b016e39d1b12f211
Joe Illuminati <illumijo@gmail.com>
2017-09-19 12:56:07 -0400

    fill rate calculation

---
+++ spark/stats/calc/fill_rate.py
@@ -0,0 +1,35 @@
+from pyspark.sql.functions import col, sum, isnan, count
+
+def _col_fill_rate(c):
+    '''
+    Calculate the fill rate for a given column.
+    "null" values can be the following:
+        - NULL
+        - ''
+        - ' '
+        - NaN
+    Input:
+        - col: dataframe column of type pyspark.sql.Column
+    Output:
+        - fr: the fill rate for that column of type pyspark.sql.Column
+    '''
+
+    row_count = count(col(c))
+    is_null = col(c).isNotNull() & ~isnan(c) & ~col(c).isin(['', ' '])
+    fr = (sum(is_null.cast("integer")) / row_count).alias(c)
+    return fr
+
+
+def calculate_fill_rate(df):
+    '''
+    Calculates the fill rate for a dataframe.
+    Input:
+        - df: a pyspark.sql.DataFrame
+    Output:
+        fr_df: the fill rates for each column as a pyspark.sql.DataFrame
+    '''
+
+    fr_df = df.agg(*[_col_fill_rate(c) for c in df.columns])
+    return fr_df
+
+
---
+++ spark/test/stats/calc/fill_rate_test.py
@@ -0,0 +1,43 @@
+import pytest
+
+from pyspark.sql import Row
+
+import spark.stats.calc.fill_rate as fill_rate
+
+df = None
+results = None
+expected_df = None
+
+def cleanup(spark):
+    pass
+
+@pytest.mark.usefixtures("spark")
+def test_init(spark):
+    global df, results, expected_df
+    data_row = Row('a', 'b', 'c', 'd', 'e')
+    df = spark['spark'].sparkContext.parallelize([
+        data_row('1', ' ', '', 'null', ' '),
+        data_row('2', '', '2', 'NaN', 'hey'),
+        data_row(None, None, 'a', None, 'hi'),
+        data_row('', float('Nan'), 'b', float('Nan'), 'hello')
+    ]).toDF()
+    results = fill_rate.calculate_fill_rate(df)
+    expected_df = spark['spark'].sparkContext.parallelize([
+        data_row(0.6666666666666666, 0.0, 0.75, 0.3333333333333333, 0.75)
+    ]).toDF()
+
+
+def test_num_columns_equal():
+    num_df_cols = len(df.columns)
+    num_results_cols = len(results.columns)
+    assert num_df_cols == num_results_cols
+    
+
+def test_column_names_equal():
+    df_cols = df.columns
+    results_cols = results.columns
+    assert df_cols == results_cols
+
+
+def test_expected_values():
+    assert expected_df.subtract(results).count() == 0
