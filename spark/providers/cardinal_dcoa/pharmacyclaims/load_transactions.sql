0680325224abd97464786db190a914689f6b6acf
Joe Illuminati <illumijo@gmail.com>
2017-09-25 16:50:03 -0400

    setup and load transactions sql script.

---
+++ spark/providers/cardinal_dcoa/pharmacyclaims/load_transactions.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS cardinal_dcoa_transactions;
+CREATE TABLE cardinal_dcoa_transactions (
+    SKUtilizationKey                string,
+    SKClientTypeKey                 string,
+    AcqCost                         string,
+    QTY                             string,
+    ExtendedFee                     string,
+    Revenue                         string,
+    ExtendedCost                    string,
+    UOMQTY                          string,
+    DispenseDttm                    string,
+    HDC                             string,
+    EffectiveStartDate              string,
+    DischargeDttm                   string,
+    PatientType                     string,
+    ClientPatientId                 string,
+    Outlier                         string,
+    PhysicianCode                   string,
+    PrescribingPhysicianCode        string,
+    NDC                             string,
+    MonthlyPatientDays              string,
+    Discharge                       string,
+    DischargePatientDays            string,
+    TotalPatientDays                string,
+    ClientName                      string,
+    Address1                        string,
+    DrgCode                         string,
+    ServiceAreaDescription          string,
+    MasterServiceAreaDescription    string
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+    "separatorChar" = "|"
+)
+STORED AS TEXTFILE
+LOCATION {input_path}
+;
