47d6f51f7768881e41a7bec70405521310f107c1
Ilia Fishbein <ifishbein@healthverity.com>
2017-09-25 21:46:06 -0400

    LEFT JOIN is faster than WHERE IN <subquery>

--- spark/providers/express_scripts/pharmacyclaims/select_not_reversed_claims.sql
+++ spark/providers/express_scripts/pharmacyclaims/select_not_reversed_claims.sql
@@ -1,6 +1,5 @@
 -- whA4dkmHELckamkOJvaI/De+Ru9qgWK6Cc72fHZPvL0= is a blank reference id
-SELECT *
+SELECT {table_name}.*
 FROM {table_name}
-WHERE claim_id NOT IN (
-    SELECT pharmacy_claim_ref_id from transactions WHERE pharmacy_claim_ref_id != 'whA4dkmHELckamkOJvaI/De+Ru9qgWK6Cc72fHZPvL0='
-)
+LEFT JOIN transactions ON claim_id = pharmacy_claim_ref_id
+WHERE pharmacy_claim_ref_id IS NULL
