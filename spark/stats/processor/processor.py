ab015fed4f1c11b0bb267cb34236ff7b2bf0a8d1
Joe Illuminati <illumijo@gmail.com>
2017-09-28 14:12:06 -0400

    done iterating on feedback

--- spark/stats/processor/processor.py
+++ spark/stats/processor/processor.py
@@ -34,31 +34,6 @@ def _get_all_data(sqlContext, datatype, provider_name):
     return all_data_df
 
 
-def _get_provider_conf(provider_name):
-    '''
-    Input:
-        provider_name: a string that is the name of the provider
-    Output:
-        datafeed_id: the datafeed id for the provider on HealthVerity Marketplace
-        datatype: a string describing the datatype for the provider (i.e. medicalclaims, events, etc...)
-        date_field: the column in the data to do date comparisons on
-        record_field: the column in the data to determine what to consider as a distinct "row" count.
-                      if None, then just use number of rows in data
-    '''
-    #TODO: Implement after module complete
-    datafeed_id = None
-    datatype = None
-    date_field = None
-    record_field = None
-
-    return {
-        'datafeed_id': datafeed_id,
-        'datatype': datatype,
-        'date_field': date_field,
-        'record_field': record_field
-    }
-
-
 def run_marketplace_stats(spark, sqlContext, provider_name, quarter, \
                           start_date, end_date, earliest_date, \
                           get_data_func, get_provider_conf_func):
@@ -93,44 +68,51 @@ def run_marketplace_stats(spark, sqlContext, provider_name, quarter, \
     # provider, start_date, end_date df cache
     # used for fill rate, top values, and key stats
     if distinct_column_name:
-        reduced_df_1 = postprocessor.compose(
+        gen_stats_df = postprocessor.compose(
             limit_date_range.limit_date_range(start_date, end_date, date_column_field),
             select_distinct_column.select_distinct_column(distinct_column_name)
         )(all_data_df).cache()
     else:
-        reduced_df_1 = limit_date_range.limit_date_range(start_date, \
+        gen_stats_df = limit_date_range.limit_date_range(start_date, \
                         end_date, date_column_field)(all_data_df).cache()
+
+
+    # Generate fill rates
+    fill_rates = _run_fill_rates(gen_stats_df)
+
+    # Generate key stats
+    key_stats = None
+
+    # Generate top values
+    top_values = None
+
+    gen_stats_df.unpersist()
 
     # datatype, provider, earliest_date, end_date df cache
     # used for longitudinality and year over year
-    reduced_df_2 = None
+    date_stats_df = None
+
+    # Generate Longitudinality
+    longitudinality = None
+
+    # Generate year over year
+    year_over_year = None
 
     # datafeed_id, provider, datatype, quarter df cache
     # used for epidemiological
-    reduced_df_3 = None
-
-    # Generate fill rates
-    fill_rates_df = _run_fill_rates(reduced_df_1)
-
-    # Generate key stats
-    key_stats_df = None
-
-    # Generate top values
-    top_values_df = None
-
-    # Generate Longitudinality
-    longitudinality_df = None
+    epi_stats_df = None
 
     # Generate Epidemiological calculations
-    epi_calcs_df = None
+    epi_calcs = None
 
     # Return all the dfs
     all_dfs = {
-        'fill_rates': fill_rates_df,
-        'key_stats': key_stats_df,
-        'top_values': top_values_df,
-        'longitudinality': longitudinality_df,
-        'epi_calcs': epi_calcs_df
+        'fill_rates': fill_rates,
+        'key_stats': key_stats,
+        'top_values': top_values,
+        'longitudinality': longitudinality,
+        'year_over_year': year_over_year,
+        'epi_calcs': epi_calcs
     }
     return all_dfs
 
