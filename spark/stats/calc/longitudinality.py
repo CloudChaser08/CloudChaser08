c4765771cb49d1e0edbed075b7d835a30ba7e27b
Joe Illuminati <illumijo@gmail.com>
2017-10-02 16:53:25 -0400

    Longitudinality skeleton

---
+++ spark/stats/calc/longitudinality.py
@@ -0,0 +1,15 @@
+from pyspark.sql.functions import mean, stddev, count, months_between
+from pyspark.sql.types import IntegerType
+
+def calculate_longitudinality(df):
+    '''
+    Calculate the longitudinality for a given set of data
+    Input:
+        -df: a pyspark.sql.DataFrame
+    Output:
+        - long_stats: the longitudinal stats for the data
+    '''
+
+    pass
+
+
