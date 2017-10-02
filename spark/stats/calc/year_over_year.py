27cab7b352dc5d2c1949faf1d4c5f684a35fe772
Joe Illuminati <illumijo@gmail.com>
2017-10-02 17:07:38 -0400

    year over year skeleton

---
+++ spark/stats/calc/year_over_year.py
@@ -0,0 +1,14 @@
+from pyspark.sql.functions import collect_set, year, array_contains, col
+
+def calculate_year_over_year(df):
+    '''
+    Calculate patient year over year on the data set
+    Input:
+        -df: a pyspark.sql.DataFrame
+    Output:
+        yoy_stats: a Dictionary of the stats
+    '''
+
+    pass
+
+
