70d727523045b1c302f6581cbb4ad90635c2a0fa
Ilia Fishbein <ifishbein@healthverity.com>
2017-10-06 18:48:13 -0400

    Don't de-dupe demographic info

--- spark/providers/nextgen/emr/deduplicate_transactions.sql
+++ spark/providers/nextgen/emr/deduplicate_transactions.sql
@@ -1,34 +1,3 @@
-DROP TABLE IF EXISTS demographics_dedup;
-CREATE TABLE demographics_dedup
-AS SELECT 
-    first(preambleformatcode) as preambleformatcode,
-    x.nextgengroupid,
-    first(referencedatetime) as referencedatetime,
-    first(postamblecategoryformat) as postamblecategoryformat,
-    first(datacapturedate) as datacapturedate,
-    first(birthyear) as birthyear,
-    first(birthmonth) as birthmonth,
-    first(gender) as gender,
-    first(race) as race,
-    first(zip3) as zip3,
-    first(coveredbymedicarepartbflag) as coveredbymedicarepartbflag,
-    first(patientpseudonym) as patientpseudonym,
-    x.reportingenterpriseid,
-    x.recorddate,
-    first(dataset) as dataset
-FROM demographics
-LEFT JOIN
-    (
-        SELECT reportingenterpriseid, nextgengroupid, MAX(recorddate) as recorddate
-        FROM demographics
-        GROUP BY reportingenterpriseid, nextgengroupid
-    ) x
-    ON x.reportingenterpriseid = demographics.reportingenterpriseid
-        AND x.nextgengroupid = demographics.nextgengroupid
-        AND x.recorddate = demographics.recorddate
-WHERE x.recorddate IS NOT NULL
-GROUP BY x.nextgengroupid, x.reportingenterpriseid, x.recorddate;
-
 DROP TABLE IF EXISTS encounter_dedup;
 CREATE TABLE encounter_dedup
 AS SELECT
