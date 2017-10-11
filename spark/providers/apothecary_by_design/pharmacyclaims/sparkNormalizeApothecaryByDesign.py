17ff09475c01ee3d1e57e24f4a94c055875b465e
Joe Illuminati <illumijo@gmail.com>
2017-10-11 16:41:16 -0400

    iterating on feedback

--- spark/providers/apothecary_by_design/pharmacyclaims/sparkNormalizeApothecaryByDesign.py
+++ spark/providers/apothecary_by_design/pharmacyclaims/sparkNormalizeApothecaryByDesign.py
@@ -12,7 +12,7 @@ import spark.helpers.privacy.pharmacyclaims as pharm_priv
 def run(spark, runner, date_input, test = False, airflow_test = False):
     date_obj = datetime.strptime(date_input, '%Y-%m-%d')
 
-    setid = 'hv_export_data'
+    setid = 'hv_export_data_{}'.format(date_input)      #TODO: rename when they send us filename formats
 
     script_path = __file__
 
