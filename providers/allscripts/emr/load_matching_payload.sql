47ab00d629e414f628b05b7233e636e94cfba16f
Matt Usifer <musifer@healthverity.com>
2017-10-13 11:26:03 -0400

    Remove stray comma

--- providers/allscripts/emr/load_matching_payload.sql
+++ providers/allscripts/emr/load_matching_payload.sql
@@ -4,7 +4,7 @@ CREATE TABLE matching_payload (
         hvid text ENCODE lzo,
         threeDigitZip char(3) ENCODE lzo,
         yearOfBirth text ENCODE lzo
-        ),
+        )
     DISTKEY(personId) SORTKEY(hvid);
 
 COPY matching_payload FROM :matching_path CREDENTIALS :credentials BZIP2 FORMAT AS JSON 's3://healthveritydev/musifer/scratch/payloadpaths.json';
