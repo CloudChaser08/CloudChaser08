ad1c9c7dfbdd4da6660a2d63d369f936ee99b5e7
Ilia Fishbein <ifishbein@healthverity.com>
2017-10-19 16:04:31 -0400

    Updated de-duplication logic for Nextgen encounter and merging logic for Nextgen demographics

--- spark/providers/nextgen/emr/deduplicate_transactions.sql
+++ spark/providers/nextgen/emr/deduplicate_transactions.sql
@@ -1,51 +1,46 @@
 DROP TABLE IF EXISTS encounter_dedup
 CREATE TABLE encounter_dedup
+    STORED AS ORC
 AS SELECT
-    first(preambleformatcode) as preambleformatcode,
-    first(nextgengroupid) as nextgengroupid,
-    first(referencedatetime) as referencedatetime,
-    first(postamblecategoryformat) as postamblecategoryformat,
-    x.encounterid,
-    first(encounterdatetime) as encounterdatetime,
-    first(encountertype) as encountertype,
-    first(encounterdescription) as encounterdescription,
-    first(hcpzipcode) as hcpzipcode,
-    first(hcpprimarytaxonomy) as hcpprimarytaxonomy,
-    x.reportingenterpriseid,
-    x.recorddate,
-    first(dataset) as dataset
-FROM encounter
-LEFT JOIN
-    (
-        SELECT reportingenterpriseid, encounterid, MAX(recorddate) as recorddate
-        FROM encounter
-        GROUP BY reportingenterpriseid, encounterid
-    ) x
-    ON x.reportingenterpriseid = encounter.reportingenterpriseid
-        AND x.encounterid = encounter.encounterid
-        AND x.recorddate = encounter.recorddate
-WHERE x.recorddate IS NOT NULL
-GROUP BY x.encounterid, x.reportingenterpriseid, x.recorddate
+    preambleformatcode,
+    nextgengroupid,
+    referencedatetime,
+    postamblecategoryformat,
+    encounterid,
+    encounterdatetime,
+    encountertype,
+    encounterdescription,
+    hcpzipcode,
+    hcpprimarytaxonomy,
+    reportingenterpriseid,
+    recorddate,
+    dataset
+FROM (
+    SELECT *,
+        lead(recorddate, 1) OVER (PARTITION BY encounterid, reportingenterpriseid SORT BY recorddate) as nextrecorddate
+    FROM (
+        SELECT * FROM new_encounter
+        UNION ALL
+        SELECT preambleformatcode, nextgengroupid, referencedatetime,
+            postamblecategoryformat, encounterid, encounterdatetime,
+            encountertype, encounterdescription, hcpzipcode, hcpprimarytaxonomy,
+            reportingenterpriseid, recorddate, dataset
+        FROM old_encounter
+    ) encounter_union
+) encounter_distinct
+WHERE encounter_w_nextrecorddate IS NULL;
 
 DROP TABLE IF EXISTS demographics_local;
 CREATE TABLE demographics_local
-    STORED AS PARQUET
-AS SELECT
-    column1  as preambleformatcode,
-    column2  as nextgengroupid,
-    column3  as referencedatetime,
-    column4  as postamblecategoryformat,
-    column5  as datacapturedate,
-    column6  as birthyear,
-    column7  as birthmonth,
-    column8  as gender,
-    column9  as race,
-    column10 as zip3,
-    column11 as coveredbymedicarepartbflag,
-    column12 as patientpseudonym,
-    regexp_extract(input_file_name(), 'NG_LSSA_([^_]*)_[^\.]*.txt') as reportingenterpriseid,
-    regexp_extract(input_file_name(), 'NG_LSSA_[^_]*_([^\.]*).txt') as recorddate,
-    regexp_extract(input_file_name(), '(NG_LSSA_[^_]*_[^\.]*.txt)') as dataset,
+    STORED AS ORC
+AS SELECT *,
     lead(recorddate, 1) OVER (PARTITION BY nextgengroupid, reportingenterpriseid SORT BY recorddate) as nextrecorddate
-FROM all_raw_data
-WHERE column4 = '0005.001';
+FROM (
+    SELECT * FROM new_demographics
+    UNION ALL
+    SELECT preambleformatcode, nextgengroupid, referencedatetime,
+        postamblecategoryformat, datacapturedate, birthyear, birthmonth,
+        gender, race, zip3, coveredbymedicarepartbflag,
+        patientpseudonym, reportingenterpriseid, recorddate, dataset
+    FROM old_demographics
+) demogpraphics_union;
