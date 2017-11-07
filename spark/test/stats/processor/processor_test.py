e9f38d288861abddba98e5eb6a5aa64db7283983
Joe Illuminati <illumijo@gmail.com>
2017-11-07 15:45:07 -0500

    fixed processor test to use mock and new function def

--- spark/test/stats/processor/processor_test.py
+++ spark/test/stats/processor/processor_test.py
@@ -1,8 +1,12 @@
 import pytest
+from mock import Mock
 
 from pyspark.sql import Row
 
 import spark.stats.processor as processor
+
+import spark.stats.config.reader.config_reader as config_reader
+import spark.helpers.stats.utils as stats_utils
 
 results_distinct_column = None
 results_no_distinct_column = None
@@ -15,6 +19,9 @@ data_row = None
 
 fill_rate_conf = None
 
+old_get_data_func = None
+old_generate_get_provider_config_function_func = None
+
 def cleanup(spark):
     pass
 
@@ -23,7 +30,8 @@ def cleanup(spark):
 def test_init(spark):
     global df, provider_name, results_distinct_column, \
             results_no_distinct_column, results_no_fill_rate, \
-            columns, data_row, fill_rate_conf
+            columns, data_row, fill_rate_conf, old_get_data_func, \
+            old_generate_get_provider_config_function_func
 
     provider_name = 'test'
 
@@ -38,8 +46,11 @@ def test_init(spark):
     columns = ['claim_id', 'service_date', 'col_1', 'col_2', 'col_3']
     data_row = Row(*columns)
 
-    def _inject_get_data(*params):
-        df = spark['spark'].sparkContext.parallelize([
+    old_get_data_func = stats_utils.get_provider_data
+    old_generate_get_provider_config_function_func = config_reader.generate_get_provider_config_function
+
+    inject_data_mock = Mock(
+        return_value = spark['spark'].sparkContext.parallelize([
             data_row('0', '1995-10-11', None, 'a', 'b'),
             data_row('0', '2016-01-12', 'a', 'b', 'c'),
             data_row('1', '2015-11-08', 'a', 'b', '  '),
@@ -50,14 +61,15 @@ def test_init(spark):
             data_row('2', '1850-01-01', 'a', 'b', 'c'),
             data_row('2', '1900-01-01', 'a', 'b', 'c')
         ]).toDF()
-        return df
+    )
 
+    stats_utils.get_provider_data = inject_data_mock
 
     fill_rate_conf = { 'blacklist_columns': ['claim_id', \
-                                            'service_date', 'col_3'] }
+                               'service_date', 'col_3'] }
 
-    def _inject_get_provider_conf(*params):
-        return {
+    get_prov_conf = Mock(
+        return_value = lambda *x: {
             'name'              : 'test',
             'datafeed_id'       : '27',
             'datatype'          : 'medicalclaims',
@@ -70,10 +82,11 @@ def test_init(spark):
             'year_over_year'    : None,
             'epi_calcs'         : None
         }
+    )
 
-
-    def _inject_get_provider_conf_no_unique_column(*params):
-        return {
+    get_prov_conf_no_unique_column = Mock(
+        return_value = lambda *x: 
+        {
             'name'              : 'test',
             'datafeed_id'       : '27',
             'datatype'          : 'medicalclaims',
@@ -86,10 +99,10 @@ def test_init(spark):
             'year_over_year'    : None,
             'epi_calcs'         : None
         }
+    )
 
-
-    def _inject_get_provider_conf_no_fill_rate_calc(*params):
-        return {
+    get_prov_conf_no_fill_rate_calc = Mock(
+        return_value = lambda *x: {
             'name'              : 'test',
             'datafeed_id'       : '27',
             'datatype'          : 'medicalclaims',
@@ -102,25 +115,25 @@ def test_init(spark):
             'year_over_year'    : None,
             'epi_calcs'         : None
         }
+    )
 
-
+    config_reader.generate_get_provider_config_function = get_prov_conf
     results_distinct_column = processor.run_marketplace_stats( \
                     spark_obj, sqlContext, \
                     provider_name, quarter, start_date, end_date, \
-                    earliest_date, _inject_get_data, \
-                    _inject_get_provider_conf)
+                    earliest_date)
 
+    config_reader.generate_get_provider_config_function = get_prov_conf_no_unique_column
     results_no_distinct_column = processor.run_marketplace_stats( \
                     spark_obj, sqlContext, \
                     provider_name, quarter, start_date, end_date, \
-                    earliest_date, _inject_get_data, \
-                    _inject_get_provider_conf_no_unique_column)
+                    earliest_date)
 
+    config_reader.generate_get_provider_config_function = get_prov_conf_no_fill_rate_calc
     results_no_fill_rate = processor.run_marketplace_stats( \
                     spark_obj, sqlContext, \
                     provider_name, quarter, start_date, end_date, \
-                    earliest_date, _inject_get_data, \
-                    _inject_get_provider_conf_no_fill_rate_calc)
+                    earliest_date)
 
 def test_fill_rate_calculated():
     assert results_distinct_column['fill_rates'] is not None
@@ -141,5 +154,10 @@ def test_fill_rate_column_are_blacklisted():
 
 def test_no_df_if_fill_rates_is_none_in_provider_conf():
     assert results_no_fill_rate['fill_rates'] == None
+
+
+def test_cleanup():
+    stats_utils.get_provider_data = old_get_data_func
+    config_reader.generate_get_provider_config_function = old_generate_get_provider_config_function_func
 
 
