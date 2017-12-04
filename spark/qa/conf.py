81f646455095e0ed3546375ec26ecc1512751e26
Matt Usifer <musifer@healthverity.com>
2017-12-04 17:00:05 -0500

    New checks, basic framework changes and medicalclaims example

--- spark/qa/conf.py
+++ spark/qa/conf.py
@@ -16,5 +16,60 @@ datatype_config = {
         'test_all_unique_vals_in_src_and_target',
         'test_validations',
         'test_full_fill_rates'
+    ],
+    types.EVENTS: [
+        'test_all_unique_vals_in_src_and_target',
+        'test_validations',
+        'test_full_fill_rates'
+    ],
+    types.ENROLLMENT: [
+        'test_all_unique_vals_in_src_and_target',
+        'test_validations',
+        'test_full_fill_rates'
+    ],
+    types.EMR_ENCOUNTER: [
+        'test_all_unique_vals_in_src_and_target',
+        'test_validations',
+        'test_full_fill_rates'
+    ],
+    types.EMR_PROCEDURE: [
+        'test_all_unique_vals_in_src_and_target',
+        'test_validations',
+        'test_full_fill_rates'
+    ],
+    types.EMR_DIAGNOSIS: [
+        'test_all_unique_vals_in_src_and_target',
+        'test_validations',
+        'test_full_fill_rates'
+    ],
+    types.EMR_PROVIDER_ORDER: [
+        'test_all_unique_vals_in_src_and_target',
+        'test_validations',
+        'test_full_fill_rates'
+    ],
+    types.EMR_LAB_ORDER: [
+        'test_all_unique_vals_in_src_and_target',
+        'test_validations',
+        'test_full_fill_rates'
+    ],
+    types.EMR_LAB_RESULT: [
+        'test_all_unique_vals_in_src_and_target',
+        'test_validations',
+        'test_full_fill_rates'
+    ],
+    types.EMR_MEDICATION: [
+        'test_all_unique_vals_in_src_and_target',
+        'test_validations',
+        'test_full_fill_rates'
+    ],
+    types.EMR_CLINICAL_OBSERVATION: [
+        'test_all_unique_vals_in_src_and_target',
+        'test_validations',
+        'test_full_fill_rates'
+    ],
+    types.EMR_VITAL_SIGN: [
+        'test_all_unique_vals_in_src_and_target',
+        'test_validations',
+        'test_full_fill_rates'
     ]
 }
