48672c4290eb31c7151985bb476ebcf0168117ee
Joe Illuminati <illumijo@gmail.com>
2017-12-05 10:47:06 -0500

    test changes

--- spark/test/stats/calc/longitudinality_test.py
+++ spark/test/stats/calc/longitudinality_test.py
@@ -23,16 +23,14 @@ def test_init(spark):
         data_row('2017-11-07', 'a', 's', 'u', 'x'),
         data_row('2017-11-01', 'c', 's', 'a', 'b'),
         data_row('2017-12-21', 'j', 'o', 'e', 'y'),
-        data_row('2016-11-07', 'z', 'y', 'x', 'w'),
-        data_row('2015-01-01', 'z', 'a', 'a', 'a'),
-        data_row('2015-01-01', 'j', 'a', 'a', 'a'),
-        data_row('2015-01-01', 'c', 'a', 'a', 'a'),
-        data_row('2015-01-01', 'a', 'a', 'a', 'a')
+        data_row('2015-12-21', 'j', 'a', 'a', 'a'),
+        data_row('2016-11-07', 'z', 'y', 'x', 'w')
     ]).toDF()
     results = longitudinality.calculate_longitudinality(df, conf)
 
 
 def test_num_of_months_rows_are_correct():
+    print results
     months_rows = filter(lambda x: x['value'].endswith('months'), results)
     assert len(months_rows) == 1
 
