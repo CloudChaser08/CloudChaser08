cd732952ffb35800f47202031032a327aaa28d13
Joe Illuminati <illumijo@gmail.com>
2017-12-14 15:17:32 -0500

    comments

--- spark/stats/calc/year_over_year.py
+++ spark/stats/calc/year_over_year.py
@@ -24,9 +24,19 @@ def calculate_year_over_year(df, earliest_date, end_date, provider_conf):
 
     end_year = _parse_year(end_date)
 
+    # --- YoY Logic ---
+    # (1) Select and limit by inputted date range
+    # (2) Only care about unique paitent visits per year
+    # (3) Window on patient id and sort by visit year,
+    #     then add index within each window
+    # (4) If a rows rank + 1 subtracted from the end year 
+    #     does not equal the year for that row,
+    #     then it is not sequential and we exclude it
+    # (5) Get unique patient visits for each year
     window = Window.partitionBy(col(patient_identifier)).orderBy(col('year').desc())
     yoy_stats = df.select(col(patient_identifier), year(col(date_field)).alias('year'))         \
                   .where((col(date_field) >= earliest_date) & (col(date_field) <= end_date))    \
+                  .drop_duplicates()                                                            \
                   .withColumn('rank', rank().over(window))                                      \
                   .where(end_year - col('rank') + 1 == col('year'))                             \
                   .groupby('year').count()                                                      \
