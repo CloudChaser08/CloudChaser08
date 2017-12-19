02c54321c63a7fedde7407f9198b6fe326198775
Joe Illuminati <illumijo@gmail.com>
2017-12-19 15:18:44 -0500

    iterating on feedback

--- airflow/dags/mckessonrx_pipeline.py
+++ airflow/dags/mckessonrx_pipeline.py
@@ -296,7 +296,6 @@ if HVDAG.HVDAG.airflow_env != 'test':
             mdag.schedule_interval,
             {
                 'sql_command_func' : lambda ds, k: insert_current_date(sql_template, k)
-                if insert_current_date('{}-{}-{}', k).find('-01') == 7 else ''
             }
         ),
         task_id='update_analytics_db',
