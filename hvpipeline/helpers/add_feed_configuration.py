ecf5165365d83fb37c8f1be9e5df2931f997dfeb
Ilia Fishbein <ifishbein@healthverity.com>
2017-12-20 18:06:58 -0500

    Removed bad code. Updated model fields

--- hvpipeline/helpers/add_feed_configuration.py
+++ hvpipeline/helpers/add_feed_configuration.py
@@ -1,19 +1,18 @@
 import hvpipeline.framework.db as pipeline_db
 import hvpipeline.framework.models as pipeline_models
 import sys
+from datetime import datetime
 
 session = pipeline_db.get_db_session()
 feed_config = pipeline_models.DataFeedConfiguration(
         hvm_feed_id=sys.argv[1],
         feed_name=sys.argv[2],
         dag_name=sys.argv[3],
-        grace_period=int(sys.argv[4]),
-        cron_schedule=sys.argv[5]
+        dag_date_offset=int(sys.argv[4]),
+        dag_date_offset_qualifier=sys.argv[5],
+        grace_period=int(sys.argv[6]),
+        cron_schedule=sys.argv[7],
+        start_dt=datetime.strptime('%Y-%m-%dT%H%M%S')
 )
 session.add(feed_config)
 session.commit()
-
-file_config = pipeline_models.DataFeedFileConfiguration(
-        data_feed_configuration_id=int(sys.argv[1]),
-        s3_key_pattern=sys.argv[2]
-)
