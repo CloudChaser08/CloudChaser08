b250f79157bb45e4d6a286e718202bce5827988f
Ilia Fishbein <ifishbein@healthverity.com>
2017-12-20 18:02:18 -0500

    Read provider and file configuration from a database. Handle a scenario where a new file arrival impacts multiple feeds

--- hvpipeline/lambdas/ingestor/ingestor.py
+++ hvpipeline/lambdas/ingestor/ingestor.py
@@ -1,6 +1,6 @@
 import boto3
 import paramiko
-import re
+import json
 from botocore.exceptions import ClientError
 from datetime import datetime, timedelta
 import hvpipeline.models as pipeline_models
@@ -11,56 +11,82 @@ def lambda_handler(event, context):
         msg = rec['Sns']['Message']
         key = json.loads(msg)['Records'][0]['s3']['object']['key']
 
-    db = pipeline_db.
-    with open('incoming_files_config.json') as infile:
-        incoming_files = json.loads(infile.read())
+    session = pipeline_db.get_db_session()
+    configured_files = session.query(pipeline_models.DataFeedFileConfiguration).all()
 
-    all_provider_config = {}
-    with open('provider_config.json') as infile:
-        for provider in json.loads(infile.read()):
-            all_provider_config[provider['provider_name']] = provider['configuration']
+    matching_configurations = []
+    for f in configured_files:
+        try:
+            file_date = datetime.strptime(key, f.s3_key_pattern)
+            matching_configurations.append(f)
+        except ValueError:
+            continue
 
-    matched = False
-
-    for f in incoming_files:
-        if re.search(f['s3_key_regex'], key) is not None:
-            matched = True
-            provider_name = f['provider_name']
-            file_date = datetime.strptime(re.search(f['s3_key_regex'], key).group(1), f['date_pattern'])
-            break
-
-    if not matched:
+    if not matching_configurations:
         #TODO send to error queue
         return
 
-    s3_client = boto3.client('s3')
+    feeds_all_files_arrived = []
 
-    provider_config = all_provider_config[provider_name]
-    expected_files = provider_config['expected_files']
-    all_here = True
+    session.begin()
+    # Lock table to prevent a race condition
+    pipeline_db.lock(session, pipeline_models.FileArrivalLog)
+    try:
+        for conf in matching_configurations:
+            new_file = pipeline_models.FileArrivalLog(
+                    s3_key=key,
+                    data_feed_file_configuration_id=conf.id
+                    received_dt=datetime.utcnow(),
+                    batch_dt=file_date,
+                    status=pipeline_models.FILE_ARRIVAL_STATUS.ARRIVED_ON_TIME
+            )
 
-    for expected_file in expected_files:
-        s3_key_to_find = datetime.strftime(file_date, expected_file['s3_key_pattern'])
-        try:
-            s3_client.get_object(Bucket='healthverity', Key=s3_key_to_find)
-        except ClientError as e:
-            if e.response['Error']['Code'] == 'NoSuchKey':
-                all_here = False
-                break
-            else:
-                raise e
+            session.add(new_file)
 
-    date_offset = provider_config.get('date_offset', {'days': 0})
-    if 'days' in date_offset:
-        exec_date = file_date + timedelta(days=date_offset['days'])
-    else:
-        # Hacky way of adding/substracting months
-        day = file_date.day
-        file_date.replace(day=15)
-        exec_date = file_date + timedelta(days=date_offset['months'] * 30)
-        exec_date.replace(day=day)
+            ingested_files = session.query(pipeline_models.FileArrivalLog).\
+                    join(pipeline_models.DataFeedFileConfiguration).\
+                    select(pipeline_models.FileArrivalLog.s3_key).\
+                    filter_by(batch_dt=file_date, data_feed_configuration_id=conf.feed_config_id).all()
 
-    _trigger_dag(provider_config['dag_name'], datetime.strftime(exec_date, '%Y-%m-%d'))
+            keys_logged = set([f.s3_key for f in ingested_files] + [key])
+
+            expected_file_configurations = session.query(
+                            pipeline_models.DataFeedFileConfiguration
+                        ).\
+                    select(pipeline_models.DataFeedFileConfiguration.s3_key_pattern).\
+                    filter_by(data_feed_configuration_id=conf.feed_config_id).all()
+
+            expected_keys = [file_date.strftime(f.s3_key_pattern) for f in
+                    expected_file_configurations]
+
+            if not expected_keys.difference(keys_logged):
+                feeds_all_files_arrived.append(conf.feed_config_id)
+
+        session.commit()
+    except:
+        #TODO send to error queue
+        session.rollback()
+
+    for feed_config_id in feeds_all_files_arrived:
+        feed_config = session.query(pipeline_models.DataFeedConfiguration).\
+                filter_by(id=feed_config_id)
+
+        date_offset_qualifier = 'days'
+        date_offset = 0
+        if feed_config.dag_date_offset_qualifier:
+            date_offset_qualifier = feed_config.dag_date_offset_qualifier
+            date_offset = feed_config.dag_date_offset
+
+        if date_offset_qualifier in ('days', 'hours'):
+            exec_date = file_date + timedelta(**{date_offset_qualifier : date_offset})
+        else:
+            # Hacky way of adding/substracting months
+            day = file_date.day
+            file_date.replace(day=15)
+            exec_date = file_date + timedelta(days=date_offset * 30)
+            exec_date.replace(day=day)
+
+        _trigger_dag(provider_config.dag_name, datetime.strftime(exec_date, '%Y-%m-%dT%H%M%S'))
 
 def _trigger_dag(dag_name, exec_date):
     s3_client = boto3.client('s3')
