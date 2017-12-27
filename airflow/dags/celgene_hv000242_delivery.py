e244b64f6483af13e89f9f73baceeded343e9f65
Matt Usifer <musifer@healthverity.com>
2017-12-27 17:01:27 -0500

    Add external task sensor and offset

--- airflow/dags/celgene_hv000242_delivery.py
+++ airflow/dags/celgene_hv000242_delivery.py
@@ -1,9 +1,10 @@
-from airflow.operators import PythonOperator
+from airflow.operators import PythonOperator, ExternalTaskSensor
 from datetime import datetime, timedelta
 
 import util.emr_utils as emr_utils
+import util.date_utils as date_utils
 import common.HVDAG as HVDAG
-for m in [emr_utils, HVDAG]:
+for m in [emr_utils, HVDAG, date_utils]:
     reload(m)
 
 DAG_NAME='celgene_delivery'
@@ -12,6 +13,8 @@ EMR_CLUSTER_NAME='delivery_cluster-celgene'
 NUM_NODES=5
 NODE_TYPE='m4.2xlarge'
 EBS_VOLUME_SIZE='100'
+
+CELGENE_DAY_OFFSET = 7
 
 def do_create_cluster(ds, **kwargs):
     emr_utils.create_emr_cluster(EMR_CLUSTER_NAME, NUM_NODES, NODE_TYPE,
@@ -28,7 +31,9 @@ def do_run_pyspark_export_routine(ds, **kwargs):
     )
 
 def get_export_args(ds, kwargs):
-    return ['--date', kwargs['ds']]
+    return ['--date', date_utils.insert_date_into_template(
+        '{}-{}-{}', kwargs, day_offset=CELGENE_DAY_OFFSET
+    )]
 
 default_args = {
     'owner': 'airflow',
@@ -41,8 +46,16 @@ default_args = {
 
 mdag = HVDAG.HVDAG(
     dag_id=DAG_NAME,
-    schedule_interval='0 0 5 * *',
+    schedule_interval='0 19 * * 2', # tuesday afternoons
     default_args=default_args
+)
+
+apothecary_by_design = ExternalTaskSensor(
+    task_id='wait_for_abd',
+    external_dag_id='apothecary_by_design_pipeline',
+    external_task_id='update_analytics_db',
+    execution_delta=timedelta(days=1), # abd runs on mondays
+    dag=mdag
 )
 
 create_cluster = PythonOperator(
@@ -70,5 +83,6 @@ delete_cluster = PythonOperator(
     dag=mdag
 )
 
+create_cluster.set_upstream(apothecary_by_design)
 run_pyspark_export_routine.set_upstream(create_cluster)
 delete_cluster.set_upstream(run_pyspark_export_routine)
