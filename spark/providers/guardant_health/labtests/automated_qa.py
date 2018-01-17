caa6e5e0754f31de7d3563c255903c80f88f1ff5
Matt Usifer <musifer@healthverity.com>
2018-01-17 12:42:36 -0500

    Fix filters on transaction/matching payload source tables

--- spark/providers/guardant_health/labtests/automated_qa.py
+++ spark/providers/guardant_health/labtests/automated_qa.py
@@ -14,17 +14,21 @@ GUARDANT_HEALTH_SRC_DATA_LOCATION = 's3a://salusv/incoming/labtests/guardant_hea
 GUARDANT_HEALTH_MATCHING_PAYLOAD_LOCATION = 's3a://salusv/matching/payload/labtests/guardant_health/2017/12/18/'
 GUARDANT_HEALTH_TARGET_DATA_LOCATION = 's3a://salusv/warehouse/parquet/labtests/2018-01-16/part_provider=guardant_health/*'
 
-payload_loader.load(spark_sql_runner, GUARDANT_HEALTH_MATCHING_PAYLOAD_LOCATION, extra_cols=['claimId'])
+payload_loader.load(spark_sql_runner, GUARDANT_HEALTH_MATCHING_PAYLOAD_LOCATION, extra_cols=['hvJoinKey'])
+
+sqlContext.read.csv(
+    GUARDANT_HEALTH_SRC_DATA_LOCATION, schema=transaction_schemas.schema
+).registerTempTable('transactions')
+
+filtered_transactions_source = sqlContext.sql(
+    "SELECT t.*, mp.hvid FROM transactions t "
+    "LEFT JOIN matching_payload mp ON t.hvjoinkey = mp.hvjoinkey "
+    "WHERE REGEXP_REPLACE(coalesce(trim(upper(physician_country)), 'UNITED STATES'), 'UNITED STATES', '') = '' "
+    "AND REGEXP_REPLACE(coalesce(trim(upper(patient_country)), 'UNITED STATES'), 'UNITED STATES', '') = ''"
+)
 
 source_data = {
-    "transactions": sqlContext.read.csv(
-        GUARDANT_HEALTH_SRC_DATA_LOCATION, schema=transaction_schemas.schema
-    ).filter(
-        (upper(trim(col('physician_country'))) == 'UNITED_STATES')
-        | (trim(col('physician_country')) == '')
-        | col('physician_country').isNull()
-    ),
-    "matching_payload": sqlContext.sql('select * from matching_payload')
+    "transactions": filtered_transactions_source
 }
 
 # load target data
@@ -34,7 +38,7 @@ target_data = sqlContext.read.parquet(GUARDANT_HEALTH_TARGET_DATA_LOCATION)
 labtests_datafeed = datafeed.standard_labtests_datafeed(
     source_data=source_data,
     target_data=target_data,
-    source_hvid_full_name='matching_payload.hvid',
+    source_hvid_full_name='transactions.hvid',
     source_claim_id_full_name='transactions.row_id',
     source_test_ordered_name_full_name='transactions.gene'
 )
