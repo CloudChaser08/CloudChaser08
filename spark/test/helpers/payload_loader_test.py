24bdf9934de3bd8d86d700ecc5a6f7d078870d4a
gzangakis <gzangakis@healthverity.com>
2018-01-31 09:52:07 -0500

    define common variables outside of functions[D

--- spark/test/helpers/payload_loader_test.py
+++ spark/test/helpers/payload_loader_test.py
@@ -3,6 +3,14 @@ import pytest
 import spark.helpers.payload_loader as payload_loader
 import spark.helpers.file_utils as file_utils
 
+std_location = file_utils.get_abs_path(
+    __file__, '../resources/parentId_test_payload.json'
+)
+
+no_hvid_location = file_utils.get_abs_path(
+    __file__, '../resources/no_id_test_payload.json'
+)
+
 
 def cleanup(spark):
     spark['sqlContext'].sql('DROP TABLE IF EXISTS test')
@@ -14,9 +22,6 @@ def test_no_hvid_columns(spark):
     Test if hvid column exists with null values
     """
     cleanup(spark)
-    no_hvid_location = file_utils.get_abs_path(
-        __file__, '../resources/no_id_test_payload.json'
-    )
 
     payload_loader.load(spark['runner'], no_hvid_location)
 
@@ -26,7 +31,7 @@ def test_no_hvid_columns(spark):
 
     null_hvid_count = spark['sqlContext'].sql('SELECT hvid FROM test').collect()  # ensure hvid column exists
 
-    row_count = spark['sqlContext'].sql('SELECT COUNT(*) FROM TEST').head()[0]
+    row_count = spark['sqlContext'].sql('SELECT COUNT(*) FROM test').head()[0]
 
     assert row_count == len(null_hvid_count)  # test that there are null hvids in every row
 
@@ -37,9 +42,7 @@ def test_extra_cols(spark):
     """
     cleanup(spark)
     extra_cols = ['claimId', 'hvJoinKey']
-    std_location = file_utils.get_abs_path(
-        __file__, '../resources/parentId_test_payload.json'
-    )
+
     payload_loader.load(spark['runner'], std_location, extra_cols)
     spark['sqlContext'].sql(
         'create table test as select * from matching_payload'
@@ -50,9 +53,7 @@ def test_extra_cols(spark):
 
 def test_correct_hvid_used(spark):
     cleanup(spark)
-    std_location = file_utils.get_abs_path(
-        __file__, '../resources/parentId_test_payload.json'
-    )
+
     payload_loader.load(spark['runner'], std_location)
     spark['sqlContext'].sql(
         'create table test as select * from matching_payload'
@@ -62,5 +63,10 @@ def test_correct_hvid_used(spark):
     assert len(parentId_rows) == 4  # test that parentId is aliased as hvid where present
 
 
-def test_table_dropped(spark):
-    pass
+def test_temp_table_created(spark):
+    cleanup(spark)
+    payload_loader.load(spark['runner'], std_location)
+
+    is_Temporary = spark['sqlContext'].sql("SHOW tables").head()[2]
+
+    assert is_Temporary is True
