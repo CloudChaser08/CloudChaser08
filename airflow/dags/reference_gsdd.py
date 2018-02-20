913d2d2eddb8bbf3af78a0d381f81c04f9a502ef
gzangakis <gzangakis@healthverity.com>
2018-02-20 16:55:15 -0500

    Added step to download files into tmp directory then copy to s3 and finally clear tmp dir.

--- airflow/dags/reference_gsdd.py
+++ airflow/dags/reference_gsdd.py
@@ -1,5 +1,6 @@
 import common.HVDAG as HVDAG
 from datetime import datetime, timedelta
+from subprocess import check_call
 import json
 import pysftp
 
@@ -8,13 +9,15 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.models import Variable
 
 import util.sftp_utils as sftp_utils
+import util.s3_utils as s3_utils
 
 for m in [sftp_utils, HVDAG]:
     reload(m)
 
+TMP_PATH = '/tmp/reference/gsdd/'
 
 if HVDAG.HVDAG.airflow_env == 'test':
-    DESTINATION = 's3://salusv/testing/dewey/airflow/reference/gsdd/'
+    DESTINATION = 's3://salusv/testing/dewey/airflow/e2e/reference/gsdd/'
 else:
     DESTINATION = 's3://salusv/reference/gsdd/'
 
@@ -42,15 +45,17 @@ def sftp_fetch_files():
         cnopts = pysftp.CnOpts()
         cnopts.hostkeys = None
         sftp_utils.fetch_file(sftp_config['path'] + filename,
-                              DESTINATION + filename,
+                              TMP_PATH + filename,
                               sftp_config['host'],
                               sftp_config['user'],
                               sftp_config['password'],
                               cnopts
                               )
 
-# sftp_utils.fetch_files(**sftp_config)
 
+def copy_files_to_s3():
+    for filename in FILES_OF_INTEREST:
+        s3_utils.copy_file(TMP_PATH+filename, DESTINATION+filename)
 
 fetch_files = PythonOperator(
     task_id='sftp_fetch_files',
@@ -59,12 +64,39 @@ fetch_files = PythonOperator(
 )
 
 
+move_files_to_s3 = PythonOperator(
+    task_id='move_files_to_s3',
+    python_callable=copy_files_to_s3,
+    dag=mdag
+    )
+
+
+def clean_up_workspace_step(template):
+    def execute(ds, **kwargs):
+        check_call([
+            'rm', '-rf', TMP_PATH
+        ])
+    return PythonOperator(
+        task_id='clean_up_workspace',
+        provide_context=True,
+        python_callable=execute,
+        trigger_rule='all_done',
+        dag=mdag
+    )
+
+clean_up_workspace = clean_up_workspace_step(TMP_PATH)
+
 if HVDAG.HVDAG.airflow_env != 'test':
     update_s3_with_new_files = BashOperator(
-    task_id='update_s3_from_db_files',
-    bash_command='docker run 581191604223.dkr.ecr.us-east-1.amazonaws.com/hvgsdd',
-    retries=3,
-    dag=mdag)
-    
+        task_id='update_s3_from_db_files',
+        bash_command='docker run 581191604223.dkr.ecr.us-east-1.amazonaws.com/hvgsdd',
+        retries=3,
+        dag=mdag
+    )
+
     update_s3_with_new_files.set_upstream(fetch_files)
+    move_files_to_s3.set_upstream(update_s3_with_new_files)
+
+move_files_to_s3.set_upstream(fetch_files)
+clean_up_workspace.set_upstream(move_files_to_s3)
 
