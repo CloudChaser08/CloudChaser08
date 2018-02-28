49ee07a531b498de49206d9f1a632aa3a0379b23
Matt Usifer <musifer@healthverity.com>
2018-02-28 10:49:21 -0500

    Add null date tests

--- spark/helpers/stats/utils.py
+++ spark/helpers/stats/utils.py
@@ -50,7 +50,7 @@ def select_data_in_date_range(start_date, end_date, date_column_name, include_nu
     def out(df):
         is_in_range = ((col(date_column_name) >= start_date) & (col(date_column_name) < end_date))
         if include_nulls:
-            is_in_range = is_in_range | (col(date_column_name).isNull() | trim(col(date_column_name)) == '')
+            is_in_range = is_in_range | (col(date_column_name).isNull() | (trim(col(date_column_name)) == ''))
 
         limited_date_df = df.filter(is_in_range)
         return limited_date_df
