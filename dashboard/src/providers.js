70bc383cc1e02f3e0648a93dc66fc96294c7c36b
gzangakis <gzangakis@healthverity.com>
2018-03-12 15:32:26 -0400

    updated regex and dates

--- dashboard/src/providers.js
+++ dashboard/src/providers.js
@@ -405,15 +405,15 @@ exports.config = [
     schedule: this.schedule.MONTHLY,
     startDate: new Date('2017-12-03'),
     airflowPipelineName: 'genoa_pipeline',
-    expectedFilenameRegex: /^.*Genoa_HealthVerity_[0-9]{6}_[0-9]{6}$/,
+    expectedFilenameRegex: /^.*Genoa_HealthVerity_[0-9]{8}_[0-9]{6}.zip$/,
     filenameToExecutionDate: function(filename) {
       var isolatedDate = filename.split('_')[2];
       return isolatedDate.substring(0, 4) + '-' + isolatedDate.substring(4, 6) + '-' + isolatedDate.substring(6, 8);
     },
     executionDateToFilename: function(date) {
       return 'incoming/genoa/Genoa_HealthVerity_' + date.getUTCFullYear() + helpers.leftZPad(date.getUTCMonth() + 1, 2)
-        + helpers.leftZPad(date.getUTCDate(), 2)
-        + '_[0-9]{6}';
+        + '01'
+        + '_[0-9]{6}.zip';
     }
   },
   {
