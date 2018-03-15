cc18602deec9ee39dacd8f7361d5de27d96db56e
Joe Illuminati <illumijo@gmail.com>
2018-03-15 09:38:11 -0400

    removing unnecessary args to bash script

--- spark/stats/bin/generate_stats.sh
+++ spark/stats/bin/generate_stats.sh
@@ -4,16 +4,15 @@ set -x
 
 # Generate all provider stats
 
-if [ -z $4 ] || [ $1 = "-h" ]
+if [ -z $3 ] || [ $1 = "-h" ]
 then
-    echo "usage: ./generate_stats.sh [quarter] [start_date] [end_date] [output_dir]"
+    echo "usage: ./generate_stats.sh [quarter] [start_date] [end_date]"
     exit 0
 fi
 
 QUARTER=$1
 START_DATE=$2
 END_DATE=$3
-S3_OUTPUT_DIR=$4
 
 function generate_stats()
 {
@@ -32,8 +31,6 @@ function generate_stats()
     echo "Generating stats for feed $feed_id"
 
     spark-submit --py-files ../../target/dewey.zip --conf spark.executor.instances=15 --conf spark.executor.cores=2 --conf spark.executor.memory=10G --conf spark.driver.memory=13G --conf spark.yarn.executor.memoryOverhead=1024 --conf spark.scheduler.minRegisteredResourcesRatio=1 --conf spark.scheduler.maxRegisteredResourcesWaitingTime=60s ../stats_runner.py --feed_id $feed_id --quarter $QUARTER --start_date $START_DATE --end_date $END_DATE --output_dir output/$feed_id
-
-    aws s3 cp --recursive output/$feed_id $S3_OUTPUT_DIR$QUARTER/$feed_id
 }
 
 # Medical Claims (Old Model)
