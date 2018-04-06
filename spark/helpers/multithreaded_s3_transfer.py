c7548b011d7e6a5d42dbabb089820b6a8a74e549
Matt Usifer <musifer@healthverity.com>
2018-04-06 13:03:07 -0400

    Fix pickling error

--- spark/helpers/multithreaded_s3_transfer.py
+++ spark/helpers/multithreaded_s3_transfer.py
@@ -3,27 +3,29 @@ from multiprocessing import Pool
 
 THREADS=64
 
-def _copy_file(src_bucket, dest_bucket, src_prefix, dest_prefix, file_list):
-    def out(thread_id):
-        i = thread_id
-        s3 = boto3.resource('s3')
-        bucket = s3.Bucket(dest_bucket)
-        source = {
-            'Bucket': src_bucket
-        }
-        while i < len(file_list):
-            s3_file = file_list[i]
-            source['Key'] = s3_file
-            bucket.Object(s3_file.replace(src_prefix, dest_prefix)).copy_from(CopySource=source)
+src_bucket = dest_bucket = src_prefix = dest_prefix = file_list = None
 
-            i += THREADS
-    return out
+
+def _copy_file(thread_id):
+    i = thread_id
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(dest_bucket)
+    source = {
+        'Bucket': src_bucket
+    }
+    while i < len(file_list):
+        s3_file = file_list[i]
+        source['Key'] = s3_file
+        bucket.Object(s3_file.replace(src_prefix, dest_prefix)).copy_from(CopySource=source)
+
+        i += THREADS
 
 
 def multithreaded_copy(src, dest):
     src  = src + '/' if src[-1] != '/' else src
     dest = dest + '/' if dest[-1] != '/' else dest
 
+    global src_bucket, dest_bucket, src_prefix, dest_prefix, file_list
     src_bucket = src.split('/')[2]
     dest_bucket = dest.split('/')[2]
 
@@ -41,6 +43,4 @@ def multithreaded_copy(src, dest):
     file_list = map(lambda x: x.get('key'), sorted(files, key= lambda x: x.get('size'), reverse=True))
 
     p = Pool(THREADS)
-    p.map(_copy_file(
-        src_bucket, dest_bucket, src_prefix, dest_prefix, file_list
-    ), range(THREADS))
+    p.map(_copy_file, range(THREADS))
