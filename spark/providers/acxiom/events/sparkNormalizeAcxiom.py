278dd4ecaf64ef15bf0576979ab02a20473f8d8e
Joe Illuminati <illumijo@gmail.com>
2018-04-13 14:54:06 -0400

    acxiom norm routine

---
+++ spark/providers/acxiom/events/sparkNormalizeAcxiom.py
@@ -0,0 +1,115 @@
+from datetime import datetime, date
+import argparse
+
+from spark.runner import Runner
+from spark.spark_setup import init
+from spark.common.event_common_model_v4 import schema
+import spark.helpers.file_utils as file_utils
+import spark.helpers.payload_loader as payload_loader
+import spark.helpers.normalized_records_unloader as normalized_records_unloader
+import spark.helpers.external_table_loader as external_table_loader
+import spark.helpers.schema_enforcer as schema_enforcer
+import spark.helpers.explode as explode
+import spark.helpers.postprocessor as postprocessor
+import spark.helpers.privacy.events as event_priv
+
+FEED_ID = '50'
+VENDOR_ID = '229'
+
+def run(spark, runner, date_input, test=False, airflow_test=False):
+    setid = 'acxiom'
+
+    script_path = __file__
+
+    if test:
+        matching_path = file_utils.get_abs_path(
+            script_path, '../../../test/providers/acxiom/events/resources/matching/'
+        )
+    elif airflow_test:
+        matching_path = 's3://salusv/testing/dewey/airflow/e2e/acxiom/payload/{}/'.format(
+            date_input.replace('-', '/')
+        )
+    else:
+        matching_path = 's3://salusv/matching/payload/consumer/acxiom/{}/'.format(
+            date_input.replace('-', '/')
+        )
+
+    if not test:
+        external_table_loader.load_ref_gen_ref(runner.sqlContext)
+
+    min_date = postprocessor.coalesce_dates(
+                    runner.sqlContext,
+                    FEED_ID,
+                    None,
+                    'HVM_AVAILABLE_HISTORY_START_DATE'
+                )
+    if min_date:
+        min_date = min_date.isoformat()
+
+    max_date = date_input
+
+    payload_loader.load(runner, matching_path, ['claimId'])
+
+    normalized_df = runner.run_spark_script(
+        'normalize.sql',
+        [['date_input', date_input]],
+        return_output=True
+    )
+
+    postprocessor.compose(
+        schema_enforcer.apply_schema_func(schema),
+        postprocessor.add_universal_columns(
+            feed_id=FEED_ID,
+            vendor_id=VENDOR_ID,
+            filename=setid,
+            model_version_number='04'
+        ),
+        postprocessor.nullify,
+        event_priv.filter
+    )(
+        normalized_df
+    ).createOrReplaceTempView('event_common_model')
+
+    if not test:
+        hvm_historical = postprocessor.coalesce_dates(
+            runner.sqlContext,
+            FEED_ID,
+            date(1900, 1, 1),
+            'HVM_AVAILABLE_HISTORY_START_DATE',
+            'EARLIST_VALID_SERVICE_DATE'
+        )
+
+        normalized_records_unloader.partition_and_rename(
+            spark, runner, 'events', 'event_common_model_v4.sql',
+            'acxiom', 'event_common_model',
+            'source_record_date', date_input,
+            hvm_historical_date=datetime(hvm_historical.year,
+                                         hvm_historical.month,
+                                         hvm_historical.day)
+        )
+
+
+def main(args):
+    spark, sqlContext = init('Acxiom')
+
+    runner = Runner(sqlContext)
+
+    run(spark, runner, args.date, airflow_test=args.airflow_test)
+
+    spark.stop()
+
+    if args.airflow_test:
+        output_path = 's3://salusv/testing/dewey/airflow/e2e/acxiom/spark-output/'
+    else:
+        output_path = 's3://salusv/warehouse/parquet/consumer/2017-08-02/'
+
+    normalized_records_unloader.distcp(output_path)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--date', type=str)
+    parser.add_argument('--airflow_test', default=False, action='store_true')
+    args = parser.parse_args()
+    main(args)
+
