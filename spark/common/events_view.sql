38cd2b3242a3c51b010f0ee47d39cdfbb32bafcc
Joe Illuminati <illumijo@gmail.com>
2018-04-17 15:09:03 -0400

    fixed

--- spark/common/events_view.sql
+++ spark/common/events_view.sql
@@ -49,36 +49,7 @@ CREATE VIEW default.events (
     CASE WHEN part_best_date IN ('NULL', '0_PREDATES_HVM_HISTORY')
     THEN '0_PREDATES_HVM_HISTORY'
     ELSE part_best_date
-    END as part_best_date
-FROM events_v6
-UNION ALL
-SELECT
-    record_id,
-    hvid,
-    created,
-    model_version,
-    data_set,
-    data_feed,
-    data_vendor,
-    source_version,
-    patient_age,
-    patient_year_of_birth,
-    patient_zip3,
-    patient_state,
-    patient_gender,
-    source_record_id,
-    source_record_qual,
-    source_record_date, 
-    event,
-    event_val,
-    event_val_uom,
-    event_date,
-    logical_delete_reason,
-    part_provider,
-    CASE WHEN part_best_date IN ('NULL', '0_PREDATES_HVM_HISTORY')
-    THEN '0_PREDATES_HVM_HISTORY'
-    ELSE part_best_date
-    END as part_best_date
+    END AS part_best_date
 FROM events_v4
 UNION ALL
 SELECT 
@@ -100,12 +71,12 @@ SELECT
     source_record_date, 
     event,
     event_val,
-    NULL as event_val_uom,
+    NULL AS event_val_uom,
     event_date,
-    logical_delete_reason,
+    NULL AS logical_delete_reason,
     part_provider,
     CASE WHEN part_best_date IN ('NULL', '0_PREDATES_HVM_HISTORY')
     THEN '0_PREDATES_HVM_HISTORY'
     ELSE part_best_date
-    END as part_best_date
+    END AS part_best_date
 FROM events_old;
