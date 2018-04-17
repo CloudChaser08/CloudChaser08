b99bc8fd271db58a6cd14a1e3065924990dbec34
Joe Illuminati <illumijo@gmail.com>
2018-04-17 11:07:50 -0400

    events model v6

--- spark/common/events_view.sql
+++ spark/common/events_view.sql
@@ -20,6 +20,7 @@ CREATE VIEW default.events (
     event_val,
     event_val_uom,
     event_date,
+    logical_delete_reason,
     part_provider,
     part_best_date
 ) AS SELECT 
@@ -43,6 +44,36 @@ CREATE VIEW default.events (
     event_val,
     event_val_uom,
     event_date,
+    logical_delete_reason,
+    part_provider,
+    CASE WHEN part_best_date IN ('NULL', '0_PREDATES_HVM_HISTORY')
+    THEN '0_PREDATES_HVM_HISTORY'
+    ELSE part_best_date
+    END as part_best_date
+FROM events_v6
+UNION ALL
+SELECT
+    record_id,
+    hvid,
+    created,
+    model_version,
+    data_set,
+    data_feed,
+    data_vendor,
+    source_version,
+    patient_age,
+    patient_year_of_birth,
+    patient_zip3,
+    patient_state,
+    patient_gender,
+    source_record_id,
+    source_record_qual,
+    source_record_date, 
+    event,
+    event_val,
+    event_val_uom,
+    event_date,
+    NULL as logical_delete_reason,
     part_provider,
     CASE WHEN part_best_date IN ('NULL', '0_PREDATES_HVM_HISTORY')
     THEN '0_PREDATES_HVM_HISTORY'
