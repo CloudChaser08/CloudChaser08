c8863ee706cf3dee476680f9a06de349fd4d038b
Joe Illuminati <illumijo@gmail.com>
2018-04-19 11:21:57 -0400

    tasks take longer than a minute to complete

--- spark/stats/bin/generate_stats.sh
+++ spark/stats/bin/generate_stats.sh
@@ -30,7 +30,7 @@ function generate_stats()
 
     echo "Generating stats for feed $feed_id"
 
-    spark-submit --py-files ../../target/dewey.zip --conf spark.executor.instances=80 --conf spark.executor.cores=4 --conf spark.executor.memory=13G --conf spark.driver.memory=13G --conf spark.yarn.executor.memoryOverhead=1024 --conf spark.scheduler.minRegisteredResourcesRatio=1 --conf spark.scheduler.maxRegisteredResourcesWaitingTime=60s --conf spark.hadoop.fs.s3.connection.maximum=500 --conf spark.files.useFileCache=false ../stats_runner.py --feed_id $feed_id --quarter $QUARTER --start_date $START_DATE --end_date $END_DATE
+    spark-submit --py-files ../../target/dewey.zip --conf spark.executor.instances=80 --conf spark.executor.cores=4 --conf spark.executor.memory=13G --conf spark.driver.memory=13G --conf spark.yarn.executor.memoryOverhead=1024 --conf spark.scheduler.minRegisteredResourcesRatio=1 --conf spark.hadoop.fs.s3.connection.maximum=500 --conf spark.files.useFileCache=false ../stats_runner.py --feed_id $feed_id --quarter $QUARTER --start_date $START_DATE --end_date $END_DATE
 }
 
 # Medical Claims (Old Model)
