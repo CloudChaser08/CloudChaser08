165cc999f2ca1b3af87b405af68af50a176bb9d0
Joe Illuminati <illumijo@gmail.com>
2018-05-01 15:30:50 -0400

    alliance normalization routine

---
+++ spark/providers/alliance/events/normalize.sql
@@ -0,0 +1,46 @@
+SELECT
+    p.hvid                          AS hvid,
+    p.age                           AS patient_age,
+    p.yearOfBirth                   AS patient_year_of_birth,
+    SUBSTR(TRIM(p.threeDigitZip))   AS patient_zip3,
+    TRIM(UPPER(p.state))            AS patient_state,
+    p.gender                        AS patient_gender,
+    CASE
+        WHEN SUBSTR(TRIM(t.naics_code), 1, 3) = '813' THEN NULL
+        ELSE t.merchant_name
+    END                             AS event,
+    extract_date(
+        t.transaction_date,
+        '%Y-%m-%d'
+    )                               AS event_date,
+    SUBSTR(TRIM(t.zip5), 1, 3)      AS event_zip,
+    CASE
+        WHEN CAST(ROUND(t.transaction_usd, 0) AS INTEGER) < -1000 THEN -1000
+        WHEN CAST(ROUND(t.transaction_usd, 0) AS INTEGER) > 1000 THEN 1000
+        ELSE CAST(ROUND(t.transaction_usd, 0) AS INTEGER)
+    END                             AS event_revenue,
+    CASE
+        WHEN SUBSTR(TRIM(t.naics_code), 1, 3) = '813' THEN NULL
+        ELSE t.naics_code
+    END                             AS event_category_code,
+    CASE
+        WHEN SUBSTR(TRIM(t.naics_code), 1, 3) = '813' THEN NULL
+        WHEN t.naics_code IS NULL THEN NULL
+        ELSE 'NAICS_CODE'
+    END                             AS event_category_code_qual,
+    CASE
+        WHEN SUBSTR(TRIM(t.naics_code), 1, 3) = '813' THEN NULL
+        WHEN t.naics_code IS NULL THEN NULL
+        ELSE t.naics_desc
+    END                             AS event_category_name,
+    CASE
+        WHEN SUBSTR(TRIM(t.cpn_ind), 1, 1) IN ('0', 'N') THEN 'N'
+        WHEN SUBSTR(TRIM(t.cpn_ind), 1, 1) IN ('1', 'Y') THEN 'Y'
+        ELSE NULL
+    END                             AS event_category_flag
+    CASE
+        WHEN SUBSTR(TRIM(t.cpn_ind), 1, 1) IN ('0', '1', 'Y', 'N') THEN 'CNP_IND'
+        ELSE NULL
+    END                             AS event_category_flag_qual
+FROM alliance_transactions t
+INNER JOIN matching payload p ON t.agility_id = p.personid
---
+++ spark/providers/alliance/events/sparkNormalizeAlliance.py
@@ -0,0 +1,134 @@
+import argparse
+import subprocess
+from datetime import date, datetime
+from dateutil.relativedelta import relativedelta
+from spark.runner import Runner
+from spark.spark_setup import init
+from spark.common.pharmacyclaims_common_model_v6 import schema
+import spark.helpers.file_utils as file_utils
+import spark.helpers.payload_loader as payload_loader
+import spark.helpers.normalized_records_unloader as normalized_records_unloader
+import spark.helpers.external_table_loader as external_table_loader
+import spark.helpers.schema_enforcer as schema_enforcer
+import spark.helpers.explode as explode
+import spark.helpers.postprocessor as postprocessor
+import spark.helpers.privacy.pharmacyclaims as pharm_priv
+from pyspark.sql.functions import col, lit, upper
+
+def run(spark, runner, date_input, test=False, airflow_test=False):
+    setid = 'hvfeedfile_po_record_deid_{}.hvout'.format(date_input.replace('-', ''))
+
+    script_path = __file__
+
+    if test:
+        input_path = file_utils.get_abs_path(
+            script_path, '../../../test/providers/alliance/events/resources/input/'
+        )
+        matching_path = file_utils.get_abs_path(
+            script_path, '../../../test/providers/alliance/events/resources/matching/'
+        )
+    elif airflow_test:
+        input_path = 's3://salusv/testing/dewey/airflow/e2e/alliance/out/{}/'.format(
+            date_input.replace('-', '/')
+        )
+        matching_path = 's3://salusv/testing/dewey/airflow/e2e/alliance/payload/{}/'.format(
+            date_input.replace('-', '/')
+        )
+    else:
+        input_path = 's3://salusv/incoming/consumer/alliance/{}/'.format(
+            date_input.replace('-', '/')
+        )
+        matching_path = 's3://salusv/matching/payload/consumer/alliance/{}/'.format(
+            date_input.replace('-', '/')
+        )
+
+    if not test:
+        external_table_loader.load_ref_gen_ref(runner.sqlContext)
+
+    min_date = postprocessor.coalesce_dates(
+        runner.sqlContext,
+        '56',
+        None,
+        'EARLIEST_VALID_SERVICE_DATE'
+    )
+    if min_date:
+        min_date = min_date.isoformat()
+
+    max_date = date_input
+
+    payload_loader.load(runner, matching_path, ['claimId', 'personId', 'hvJoinKey'])
+
+    import spark.providers.alliance.events.load_transactions as load_transactions
+    load_transactions.load(runner, input_path)
+
+    # Normalize the source data
+    normalized_df = runner.run_spark_script(
+        'normalize.sql',
+        [],
+        return_output=True
+    )
+
+    # Post-processing on the normalized data
+    alliance_data_final = postprocessor.compose(
+        schema_enforcer.apply_schema_func(schema),
+        postprocessor.add_universal_columns(
+            feed_id='56',
+            vendor_id='243',
+            filename=setid,
+            model_version_number='07'
+        ),
+        postprocessor.nullify,
+        postprocessor.apply_date_cap(
+            runner.sqlContext,
+            'date_service',
+            max_date,
+            '56',
+            None,
+            min_date
+        ),
+        pharm_priv.filter
+    )(
+        normalized_df
+    )
+
+    if not test:
+        hvm_historical = postprocessor.coalesce_dates(
+            runner.sqlContext,
+            '56',
+            date(1900, 1, 1),
+            'HVM_AVAILABLE_HISTORY_START_DATE',
+            'EARLIST_VALID_SERVICE_DATE'
+        )
+
+        # TODO: use new normalized records unloader here
+        normalized_records_unloader.unload(
+            spark, runner, alliance_data_final, 'event_date', 
+        )
+
+    else:
+        alliance_data_final.createOrReplaceTempView('events_common_model')
+
+
+def main(args):
+    spark, sqlContext = init('Alliance Normalization')
+
+    runner = Runner(sqlContext)
+
+    run(spark, runner, args.date, airflow_test=args.airflow_test)
+
+    spark.stop()
+
+    if args.airflow_test:
+        output_path = 's3://salusv/testing/dewey/airflow/e2e/alliance/spark-output/'
+    else:
+        output_path = 's3://salusv/warehouse/parquet/consumer/2018-08-02/'
+
+    normalized_records_unloader.distcp(output_path)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--date', type=str)
+    parser.add_argument('--airflow_test', default=False, action='store_true')
+    args = parser.parse_args()
+    main(args)
