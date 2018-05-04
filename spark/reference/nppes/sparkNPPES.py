536a4d08fc5ba618b1fc64e6600198743c08933f
gzangakis <gzangakis@healthverity.com>
2018-05-03 21:49:38 -0400

    spark script

--- spark/reference/nppes/sparkNPPES.py
+++ spark/reference/nppes/sparkNPPES.py
@@ -1,18 +1,37 @@
-def pyspark_routine():
-    spark = SparkSession.builder.master("local").appName("reference_nppes").getOrCreate()
+from spark.runner import Runner
+from spark.spark_setup import init
+from nppes_schema import *
 
-    sqlContext = SQLContext(spark.sparkContext)
+def run(spark, runner, date_input, num_output_files=1, test=False, airflow_test=False):
+    
+    PARQUET_S3_LOCATION = args.s3_parquet_loc
 
-    df = sqlContext.read.csv(file_path, header=True, schema=nppes_schema)
+    # Load monthly replacement file into dataframe with schema
+    df = runner.sqlContext.read.csv(file_path, header=True, schema=nppes_schema)
 
-    # need to alias columns, not enough to remove whitespace
-    # example "Employer_Identification_Number_(EIN)" -> "ein" -- done
+    # write parquet files to s3 location
+    df.repartition(num_output_files).write.mode('overwrite').parquet(PARQUET_S3_LOCATION)
 
-    df.repartition(20).write.parquet()
 
-    df.createOrReplaceTempView("temp_nppes")
 
-    sqlContext.sql("create table ref_nppes as select * from temp_nppes")
+def main(args):
+    # Initialize Spark
+    spark, sqlContext = init("NPPES")
 
-    # first drop current ref_nppes table - be sure no data is lost
-    sql_new_template = """ ALTER TABLE table_name set location """ + PARQUET_S3_LOCATION
+    # Initialize the Spark Runner
+    runner = Runner(sqlContext)
+
+    # Run the spark routine
+    run(spark, runner, args.date, airflow_test=args.airflow_test, \
+            num_output_files=args.num_output_files)
+
+    # Tell spark to shutdown
+    spark.stop()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--s3_parquet_loc', type=str)
+    parser.add_argument('--airflow_test', default=False, action='store_true')
+    parser.add_argument('--num_output_files', default=20, type=int)
+    args = parser.parse_args()
+    main(args)
