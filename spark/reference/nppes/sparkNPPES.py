f205ae86d82681eba029e24494e7ef03af75a0fe
gzangakis <gzangakis@healthverity.com>
2018-05-08 09:55:02 -0400

    read schema from ref_nppes table

--- spark/reference/nppes/sparkNPPES.py
+++ spark/reference/nppes/sparkNPPES.py
@@ -3,15 +3,14 @@ from spark.spark_setup import init
 from nppes_schema import *
 
 
-def run(spark, runner, date_input, num_output_files, test=False, airflow_test=False):
-    file_path = args.nppes_csv_path
-    S3_PARQUET_LOCATION = args.s3_parquet_loc
-
+def run(spark, runner, input_file_path, output_location, table_schema, num_output_files, test=False, airflow_test=False):
+    # get current table schema
+    ref_nppes_df = spark.sqlContext.sql("SELECT * FROM ref_nppes")
     # Load monthly replacement file into dataframe with schema
-    df = runner.sqlContext.read.csv(file_path, header=True, schema=nppes_schema)
+    df = runner.sqlContext.read.csv(input_file_path, schema=table_schema)
 
     # write parquet files to s3 location
-    df.repartition(num_output_files).write.mode('overwrite').parquet(S3_PARQUET_LOCATION)
+    df.repartition(num_output_files).write.parquet(output_location)
 
 
 def main(args):
@@ -22,7 +21,8 @@ def main(args):
     runner = Runner(sqlContext)
 
     # Run the spark routine
-    run(spark, runner, args.date, airflow_test=args.airflow_test,
+    run(spark, runner, input_file_path=args.nppes_csv_path,
+        output_location=args.s3_parquet_loc, airflow_test=args.airflow_test,
         num_output_files=args.num_output_files)
 
     # Tell spark to shutdown
@@ -31,6 +31,7 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--nppes_csv_path', type=str)
     parser.add_argument('--s3_parquet_loc', type=str)
     parser.add_argument('--airflow_test', default=False, action='store_true')
     parser.add_argument('--num_output_files', default=20, type=int)
