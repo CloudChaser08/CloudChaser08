820553394177133cd8ef15e8431091580c277be5
Joe Illuminati <illumijo@gmail.com>
2018-06-01 14:35:10 -0400

    remove header param in load transactions

--- spark/providers/amazingcharts/emr/load_transactions.py
+++ spark/providers/amazingcharts/emr/load_transactions.py
@@ -3,7 +3,7 @@ import spark.helpers.records_loader as records_loader
 
 def load(runner, table_locs):
     for table, input_path in table_locs.items():
-        df = records_loader.load(runner, input_path, TABLE_COLS[table], 'csv', '|', header=True)
+        df = records_loader.load(runner, input_path, TABLE_COLS[table], 'csv', '|')
         postprocessor.compose(
             postprocessor.trimmify,
             postprocessor.nullify
