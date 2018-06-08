7a1f3c477457984480ab54c27a7a6ed3d9b67565
Ilia Fishbein <ifishbein@healthverity.com>
2018-06-08 14:49:18 -0400

    Adjusted formatting and logic of mapping

--- spark/providers/liquidhub/custom/normalize.sql
+++ spark/providers/liquidhub/custom/normalize.sql
@@ -1,11 +1,13 @@
 SELECT
-    LOWER(obfuscate_hvid(hvid, 'LHv2'))         AS hvid,
+    CASE WHEN hvid IS NOT NULL THEN LOWER(obfuscate_hvid(hvid, 'LHv2'))
+    END                                         AS hvid,
     claimId                                     AS lhid,
     pharmacy_name                               AS pharmacy_name,
     brand                                       AS brand,
-    isWeak and isInvalid                        AS weak_match,
-    LOWER(obfuscate_hvid(providerMatchId, 'LHv2'))
-                                                AS provider_specific_id,
+    CASE WHEN isWeak THEN 'true'
+    ELSE 'false' END                            AS weak_match,
+    CASE WHEN providerMatchId IS NOT NULL THEN  LOWER(obfuscate_hvid(providerMatchId, 'LHv2'))
+    END                                         AS provider_specific_id,
     CASE WHEN matchStatus = 'multi_match' THEN
         LOWER(to_json(obfuscate_candidate_hvids(topCandidates, 'LHv2')))
     END                                         AS matching_meta_data
