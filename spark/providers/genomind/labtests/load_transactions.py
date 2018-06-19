0c7ba1ddb02fd1eb9085dfb9f2a5b9b97db05cfc
Joe Illuminati <illumijo@gmail.com>
2018-06-19 16:45:52 -0400

    driver script

--- spark/providers/genomind/labtests/load_transactions.py
+++ spark/providers/genomind/labtests/load_transactions.py
@@ -2,68 +2,89 @@ import spark.helpers.postprocessor as postprocessor
 import spark.helpers.records_loader as records_loader
 
 def load(runner, input_paths):
-    for table, input_path, file_type, delimiter in input_paths:
-        df = records_loader.load(runner, input_path, TABLE_COLS[table], file_type, delimiter)
+    for table, input_path in input_paths:
+        df = records_loader.load(runner, input_path, TABLE_CONF[table]['columns'],
+                                 TABLE_CONF[table]['file_type'], TABLE_CONF[table]['separator'])
         postprocessor.compose(
             postprocessor.trimmify,
             postprocessor.nullify
         )(df).createOrReplaceTempView(table)
 
 
-TABLE_COLS = {
-    'clinicians': [
-        'UID',
-        'PatientKey',
-        'FirstName',
-        'LastName',
-        'ProviderId',
-        'Specialization',
-        'Address1',
-        'Address2',
-        'City',
-        'State',
-        'Zipcode',
-        'Country',
-        'LocationName'
-    ],
-    'diagnosis': [
-        'UID',
-        'PatientKey',
-        'ICDCodeType',
-        'ICDCode',
-        'Diagnosis'
-    ],
-    'genes': [
-        'UID',
-        'PatientKey',
-        'GeneName',
-        'PKPhenotype'
-    ],
-    'medications': [
-        'UID',
-        'PatientKey',
-        'GenericName',
-        'Dosage'
-    ],
-    'patient': [
-        'PatientKey',
-        'FirstName',
-        'LastName',
-        'MiddleName',
-        'Initials',
-        'Date_Of_Birth',
-        'Gender',
-        'Email',
-        'AddressLine1',
-        'AddressLine2',
-        'City',
-        'StateProvidence',
-        'Country',
-        'Zip',
-        'Phone',
-        'Suffix',
-        'Social_Security_Number',
-        'DNAAvailable',
-        'hvJoinKey'
-    ]
+TABLE_CONF = {
+    'clinicians': {
+        'columns': [
+            'UID',
+            'PatientKey',
+            'FirstName',
+            'LastName',
+            'ProviderId',
+            'Specialization',
+            'Address1',
+            'Address2',
+            'City',
+            'State',
+            'Zipcode',
+            'Country',
+            'LocationName'
+        ],
+        'file_type': 'json',
+        'separator': None
+    },
+    'diagnosis': {
+        'columns': [
+            'UID',
+            'PatientKey',
+            'ICDCodeType',
+            'ICDCode',
+            'Diagnosis'
+        ],
+        'file_type': 'json',
+        'separator': None
+    },
+    'genes': {
+        'columns': [
+            'UID',
+            'PatientKey',
+            'GeneName',
+            'PKPhenotype'
+        ],
+        'file_type': 'json',
+        'separator': None
+    },
+    'medications': {
+        'columns': [
+            'UID',
+            'PatientKey',
+            'GenericName',
+            'Dosage'
+        ],
+        'file_type': 'json',
+        'separator': None
+    },
+    'patient': {
+        'columns': [
+            'PatientKey',
+            'FirstName',
+            'LastName',
+            'MiddleName',
+            'Initials',
+            'Date_Of_Birth',
+            'Gender',
+            'Email',
+            'AddressLine1',
+            'AddressLine2',
+            'City',
+            'StateProvidence',
+            'Country',
+            'Zip',
+            'Phone',
+            'Suffix',
+            'Social_Security_Number',
+            'DNAAvailable',
+            'hvJoinKey'
+        ],
+        'file_type': 'csv',
+        'separator': '|'
+    }
 }
---
+++ spark/providers/genomind/labtests/sparkNormalizeGenomind.py
@@ -0,0 +1,148 @@
+import argparse
+from datetime import datetime, date
+
+from spark.runner import Runner
+from spark.spark_setup import init
+from spark.common.lab_common_model import schema_v7 as schema
+
+import spark.helpers.file_utils as file_utils
+import spark.helpers.payload_loader as payload_loader
+import spark.helpers.normalized_records_unloader as normalized_records_unloader
+import spark.helpers.external_table_loader as external_table_loader
+import spark.helpers.schema_enforcer as schema_enforcer
+import spark.helpers.postprocessor as postprocessor
+import spark.helpers.privacy.labtests as lab_priv
+
+FEED_ID = '76'
+VENDOR_ID = '314'
+
+def run(spark, runner, date_input, test=False, airflow_test=False):
+    script_path = __file__
+    setid = 'PatientData.out'       #TODO: fill in correctly when known
+    max_cap = date_input
+    max_cap_obj = datetime.strptime(max_cap, '%Y-%m-%d')
+
+    input_tables = [
+        'clinicians',
+        'diagnosis',
+        'genes',
+        'medications',
+        'patient'
+    ]
+
+    input_paths = {}
+    if test:
+        for t in input_tables:
+            input_paths[t] = file_utils.get_abs_path(
+                script_path, '../../../test/providers/genomind/labtests/resources/input/{}'.format(t)
+            )
+        matching_path = file_utils.get_abs_path(
+            script_path, '../../../test/providers/genomind/labtests/resources/payload/'
+        )
+    elif airflow_test:
+        for t in input_tables:
+            input_paths[t] = 's3://salusv/testing/dewey/airflow/e2e/genomind/input/{}'.format(t)
+        matching_path = 's3://salusv/testing/dewey/airflow/e2e/genomind/payload/{}/'.format(
+            date_input.replace('-', '/')
+        )
+    else:
+        for t in input_tables:
+            input_paths[t] = 's3://salusv/incoming/labtests/genomind/{}/{}/'.format(
+                date_input.replace('-', '/'), t
+            )
+        matching_path = 's3://salusv/matching/payload/labtests/genomind/{}/'.format(
+            date_input.replace('-', '/')
+        )
+
+    if not test:
+        external_table_loader.load_ref_gen_ref(runner.sqlContext)
+
+    min_date = postprocessor.coalesce_dates(
+        runner.sqlContext,
+        FEED_ID,
+        None,
+        'HVM_AVAILABLE_HISTORY_START_DATE'
+    )
+
+    if min_date:
+        min_date = min_date.isoformat()
+
+    payload_loader.load(runner, matching_path, ['claimId', 'personId', 'patientId', 'hvJoinKey'])
+
+    import spark.providers.genomind.labtests.load_transactions as load_transactions
+    load_transactions.load(runner, input_paths)
+
+    # De-duplicate the source tables
+    runner.sqlContext.sql('select * from diagnosis') \
+                     .dropDuplicates(['patientkey', 'icdcodetype', 'icdcode']) \
+                     .createOrReplaceTempView('diagnosis_dedup')
+    runner.sqlContext.sql('select * from genes') \
+                     .dropDuplicates(['patientkey', 'genename', 'pkphenotype']) \
+                     .createOrReplaceTempView('genes_dedup')
+    runner.sqlContext.sql('select * from medications') \
+                     .dropDuplicates(['patientkey', 'genericname', 'dosage']) \
+                     .createOrReplaceTempView('medications_dedup')
+
+    # Run the normalization
+    normalized_df = runner.run_spark_script(
+        'normalize.sql',
+        [],
+        return_output=True
+    )
+
+    postprocessor.compose(
+        schema_enforcer.apply_schema_func(schema),
+        postprocessor.add_universal_columns(
+            feed_id=FEED_ID,
+            vendor_id=VENDOR_ID,
+            filename=setid,
+            model_version_number='07'
+        ),
+        postprocessor.nullify,
+        lab_priv.filter
+    )(
+        normalized_df
+    ).createOrReplaceTempView('labtests_common_model')
+
+    if not test:
+        hvm_historical = postprocessor.coalesce_dates(
+            runner.sqlContext,
+            FEED_ID,
+            date(1900, 1, 1),
+            'HVM_AVAILABLE_HISTORY_START_DATE',
+            'EARLIST_VALID_SERVICE_DATE'
+        )
+
+        normalized_records_unloader.partition_and_rename(
+            spark, runner, 'pharmacyclaims', 'pharmacyclaims_common_model_v6.sql',
+            'mckesson_macro_helix', 'pharmacyclaims_common_model',
+            'date_service', date_input,
+            hvm_historical_date=datetime(hvm_historical.year,
+                                         hvm_historical.month,
+                                         hvm_historical.day)
+        )
+
+
+def main(args):
+    spark, sqlContext = init('Genomind Normalization')
+
+    runner = Runner(sqlContext)
+
+    run(spark, runner, args.date, airflow_test=args.airflow_test)
+
+    spark.stop()
+
+    if args.airflow_test:
+        output_path = 's3://salusv/testing/dewey/airflow/e2e/genomind/labtests/spark-output/'
+    else:
+        output_path = 's3://salusv/warehosue/parquet/labtests/2018-02-09/'
+
+    normalized_records_unloader.distcp(output_path)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--date', type=str)
+    parser.add_argument('--airflow_test', default=False, action='store_true')
+    args = parser.parse_args()
+    main(args)
