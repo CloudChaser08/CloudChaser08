d81a4f757f089eac510255718a5f860da0ea3d21
Matt Usifer <musifer@healthverity.com>
2018-06-19 13:11:22 -0400

    Fix events privacy filter

--- spark/helpers/privacy/events.py
+++ spark/helpers/privacy/events.py
@@ -2,6 +2,10 @@ import spark.helpers.privacy.common as priv_common
 import spark.helpers.udf.post_normalization_cleanup as post_norm_cleanup
 
 event_transformer = priv_common.Transformer(
+    patient_age=[
+        priv_common.TransformFunction(post_norm_cleanup.cap_age, ['patient_age']),
+        priv_common.TransformFunction(post_norm_cleanup.validate_age, ['patient_age', 'event_date', 'patient_year_of_birth'])
+    ],
     patient_year_of_birth=[
         priv_common.TransformFunction(
             post_norm_cleanup.cap_year_of_birth, ['patient_age', 'event_date', 'patient_year_of_birth']
