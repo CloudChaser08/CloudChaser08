af72ddaf00212053d2d6678555b005724a5e85a0
Joe Illuminati <illumijo@gmail.com>
2018-06-20 14:51:48 -0400

    support specified StructType for records loader

--- spark/helpers/records_loader.py
+++ spark/helpers/records_loader.py
@@ -6,7 +6,10 @@ def load(runner, location, columns, file_type, delimiter=',', header=False):
     Load transaction data for a provider
     """
 
-    schema = StructType([StructField(c, StringType(), True) for c in columns])
+    if isinstance(columns, StructType):
+        schema = columns
+    else:
+        schema = StructType([StructField(c, StringType(), True) for c in columns])
 
     if file_type == 'csv':
         df = runner.sqlContext.read.csv(location, schema=schema, sep=delimiter, header=header)
