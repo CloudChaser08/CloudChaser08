026277266d3d2111cf3e630a1b9b7fe603df8d59
Ilia Fishbein <ifishbein@healthverity.com>
2018-07-16 16:33:24 -0400

    Correctly parse file name

--- spark/providers/nextgen/emr/prepare_demographics.py
+++ spark/providers/nextgen/emr/prepare_demographics.py
@@ -6,7 +6,7 @@ def prepare(spark, runner, s3_crosswalk_reference):
     dem = spark.table('new_demographics') \
             .withColumn('patientpseudonym', lit(None).cast('string')) # NULL out this column. It can contain DeID data
     mat = spark.table('matching_payload') \
-            .withColumn('reportingenterpriseid', regexp_extract('input_file_name', 'NG_LSSA_([^_]*)_[^\.]*.txt', 1))
+            .withColumn('reportingenterpriseid', regexp_extract('input_file_name', '(HV|NG)_LSSA_([^_]*)_[^\.]*.txt', 2))
     dem_merged = dem.join(mat, ((dem.nextgengroupid == mat.hvJoinKey)
                 & (dem.reportingenterpriseid == mat.reportingenterpriseid)), 'left_outer') \
         .select(*([dem[c] for c in dem.columns] + [mat['hvid']]))
