b44c71a899b58ba81355e42879ccbab2a7fcc725
Ilia Fishbein <ifishbein@healthverity.com>
2018-07-19 10:16:30 -0400

    spark-submit wrapper

---
+++ spark/bin/run.sh
@@ -0,0 +1,4 @@
+prefix=$(echo $0 | sed 's/bin\/run.sh$/./')
+prefix=$(echo $prefix | sed 's/\/bin\/run.sh$//')
+prefix=$(echo $prefix | sed 's/.\/run.sh$/../')
+echo spark-submit --py-file $prefix/target/dewey.zip --jars $prefix/common/json-serde-1.3.7-jar-with-dependencies.jar,$prefix/common/HiveJDBC41.jar --conf spark.driver.memory=10G --conf spark.executor.cores=4 --conf spark.file.useFetchCache=false --conf spark.hadoop.fs.s3a.connection.maximum=1000 $@
