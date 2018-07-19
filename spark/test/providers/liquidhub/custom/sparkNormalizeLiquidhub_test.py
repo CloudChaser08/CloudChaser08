3bac11adb77d229957a358dbd85b21177f6faf61
Ilia Fishbein <ifishbein@healthverity.com>
2018-07-19 15:10:41 -0400

    New and updated tests for liquidhub

--- spark/test/providers/liquidhub/custom/sparkNormalizeLiquidhub_test.py
+++ spark/test/providers/liquidhub/custom/sparkNormalizeLiquidhub_test.py
@@ -6,49 +6,67 @@ import json
 import spark.providers.liquidhub.custom.sparkNormalizeLiquidhub as liquidhub
 from spark.helpers.udf.general_helpers import obfuscate_hvid
 
-results = []
+results = {}
+
+GROUPS = [
+    'LHV1_Source1_PatDemo_20180717_v1',         # t1
+    'LHV1_Manufacturer1_Source1_20180717_v1',   # t2
+    'LHV2_Source1_PatDemo_20180717_v1',         # t3
+    'LHV2_Manufacturer1_Source1_20180717_v1'    # t4
+]
 
 def cleanup(spark):
     pass
 
 @pytest.mark.usefixtures("spark")
 def test_init(spark):
-    # new model run
-    liquidhub.run(spark['spark'], spark['runner'], 'TERRADATA', '2018-06-01', True)
     global results
-    results = spark['sqlContext'].sql('select * from liquidhub_deliverable') \
-                                        .collect()
+    # new model run
+    for g in GROUPS:
+        liquidhub.run(spark['spark'], spark['runner'], g, 1, True)
+        results[g] = spark['sqlContext'].table('liquidhub_deliverable').collect()
 
 def test_hvid_obfuscation():
-    assert filter(lambda r: r.lhid == 'claim-1', results)[0] \
+    # Manufacturer is Amgen, salt is just 'LHv2'
+    assert filter(lambda r: r.source_patient_id == 'claim-1', results[GROUPS[2]])[0] \
         .hvid == str(obfuscate_hvid('206845800', 'LHv2')).lower()
 
+    # Manufacturer is not Amgen, salt is a combination of 'LHv2' and manufacturer name
+
+    # Manufacturer name in file content
+    assert filter(lambda r: r.source_patient_id == 'claim-2', results[GROUPS[2]])[0] \
+        .hvid == str(obfuscate_hvid('161340392', 'LHv2' + 'NOVARTIS'.lower())).lower()
+
+    # Manufacturer name in file file
+    assert filter(lambda r: r.source_patient_id == 'claim-2', results[GROUPS[3]])[0] \
+        .hvid == str(obfuscate_hvid('161340392', 'LHv2' + 'Manufacturer1'.lower())).lower()
+
 def test_multimatch_candidates_present():
-    claim3 = filter(lambda r: r.lhid == 'claim-3', results)[0]
-    candidates = json.loads(claim3.matching_meta_data)
+    claim3 = filter(lambda r: r.source_patient_id == 'claim-3', results[GROUPS[2]])[0]
+    candidates = json.loads(claim3.matching_meta)
     assert sorted(candidates, key=lambda x: x[0]) == \
             [
-                ["0280a5d0d8d2eb49e0b3e8edeb8a2a48", 0.0001113792],
-                ["5da109bef0ef3764ae70dfd5930ad508", 0.0006224556],
-                ["6bd6a1da587e4a88449fe4e1e002dc46", 0.0],
-                ["b8a6a6ef2941a0a18dca74de4331e6ff", 0.0],
-                ["e42f92d8690b8eb232c6439f44e97801", 0.0]
+                [obfuscate_hvid("95878097", 'LHv2').lower(), 0.0001113792],
+                [obfuscate_hvid("463818609", 'LHv2').lower(), 0.0006224556],
+                [obfuscate_hvid("228607344", 'LHv2').lower(), 0.0],
+                [obfuscate_hvid("36024084", 'LHv2').lower(), 0.0],
+                [obfuscate_hvid("138450964", 'LHv2').lower(), 0.0]
             ]
 
 def test_extact_match_candidates_empty():
-    claim8 = filter(lambda r: r.lhid == 'claim-8', results)[0]
-    assert 'matching_meta_data' in claim8
-    assert claim8.matching_meta_data == None
+    claim8 = filter(lambda r: r.source_patient_id == 'claim-8', results[GROUPS[2]])[0]
+    assert 'matching_meta' in claim8
+    assert claim8.matching_meta == None
 
 def test_candidate_hvid_obfuscation():
-    claim4 = filter(lambda r: r.lhid == 'claim-4', results)[0]
-    claim4_candidate_hvids = map(lambda x: x[0], json.loads(claim4.matching_meta_data))
+    claim4 = filter(lambda r: r.source_patient_id == 'claim-4', results[GROUPS[2]])[0]
+    claim4_candidate_hvids = map(lambda x: x[0], json.loads(claim4.matching_meta))
     assert str(obfuscate_hvid('36024084', 'LHv2')).lower() in claim4_candidate_hvids
 
 def test_header_row():
-    assert results[0].lhid == 'LHID'
-    assert results[0].hvid == 'HVID'
-    assert results[0].matching_meta_data == 'Matching Meta Data'
+    assert results[GROUPS[2]][0].source_patient_id == 'Source Patient Id'
+    assert results[GROUPS[2]][0].hvid == 'HVID'
+    assert results[GROUPS[2]][0].matching_meta == 'Matching Meta'
 
 def test_cleanup(spark):
     cleanup(spark)
