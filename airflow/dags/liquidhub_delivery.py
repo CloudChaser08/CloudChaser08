69c871d3c767afd5ddb7387a96527190ceb9c175
Ilia Fishbein <ifishbein@healthverity.com>
2018-07-25 14:23:23 -0400

    Various minor fixes

--- airflow/dags/liquidhub_delivery.py
+++ airflow/dags/liquidhub_delivery.py
@@ -10,16 +10,16 @@ import subdags.detect_move_normalize as detect_move_normalize
 import util.s3_utils as s3_utils
 import util.sftp_utils as sftp_utils
 
-for m in [detect_move_normalize, s3_utils, sftp_utils]
+for m in [detect_move_normalize, s3_utils, sftp_utils]:
     reload(m)
 
 # Applies to all files
 TMP_PATH_TEMPLATE = '/tmp/liquidhub/custom/{}/'
-DAG_NAME = 'liquidhub_pipeline'
+DAG_NAME = 'liquidhub_delivery_pipeline'
 
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2018, 6, 10, 12, 42),
+    'start_date': datetime(2018, 7, 25),
     'depends_on_past': False,
     'retries': 3,
     'retry_delay': timedelta(minutes=2)
@@ -27,7 +27,7 @@ default_args = {
 
 mdag = HVDAG.HVDAG(
     dag_id=DAG_NAME,
-    schedule_interval="42 12 * * *",
+    schedule_interval=None,
     default_args=default_args
 )
 
@@ -39,18 +39,6 @@ else:
     S3_PAYLOAD_DEST = 's3://salusv/matching/payload/custom/lhv2/'
     S3_NORMALIZED_FILE_URL_TEMPLATE = 's3://salusv/deliverable/lhv2/{}/'
 
-# Transaction Addon file
-TRANSACTION_TMP_PATH_TEMPLATE = TMP_PATH_TEMPLATE + 'raw/'
-TRANSACTION_FILE_DESCRIPTION = 'Liquidhub transaction file'
-TRANSACTION_FILE_NAME_TEMPLATE = 'LH_{}_LiquidHub_{{}}{{}}{{}}_V1_plainoutput.txt'
-
-# Deid file
-DEID_FILE_DESCRIPTION = 'Liquidhub deid file'
-DEID_FILE_NAME_TEMPLATE = 'LH_{}_LiquidHub_{{}}{{}}{{}}_V1_output.txt'
-
-# Return file
-RETURN_FILE_NAME_TEMPLATE = 'LH_Amgen_{}_{{}}{{}}{{}}_FILEID.txt.gz'
-
 # Identify the group_id passed into this DagRun and push it to xcom
 def do_persist_group_id(ds, **kwargs):
     group_id = kwargs['dag_run'].conf['group_id']
@@ -58,19 +46,19 @@ def do_persist_group_id(ds, **kwargs):
     kwargs['ti'].xcom_push(key='group_id', value=group_id)
 
 persist_group_id = PythonOperator(
-    task_id='get_group_id',
+    task_id='persist_group_id',
     provide_context=True,
     python_callable=do_persist_group_id,
     dag=mdag
 )
 
 get_group_id = lambda ds, k: k['ti'].xcom_pull(dag_id=DAG_NAME, task_ids='persist_group_id', key='group_id')
-get_tmp_dir  = lambda ds, k: TMP_PATH_TEMPLATE.format(get_group_id(ds, kwargs))
+get_tmp_dir  = lambda ds, k: TMP_PATH_TEMPLATE.format(get_group_id(ds, k))
 
 create_tmp_dir = PythonOperator(
     task_id='create_tmp_dir',
     provide_context=True,
-    python_callable=lambda ds, **k: os.makedirs(get_tmp_dir(ds, k))
+    python_callable=lambda ds, **k: os.makedirs(get_tmp_dir(ds, k)),
     dag=mdag
 )
 
@@ -109,9 +97,9 @@ detect_move_normalize_dag = SubDagOperator(
 def do_fetch_return_file(ds, **kwargs):
     gid = get_group_id(ds, kwargs)
 
-    s3_url = S3_NORMALIZED_FILE_URL_TEPMLATE.format(gid)
+    s3_url = S3_NORMALIZED_FILE_URL_TEMPLATE.format(gid)
     return_file = [f for f in 
-            s3_utils.list_s3_bucket_files(S3_NORMALIZED_FILE_URL_TEPMLATE.format(gid))
+            s3_utils.list_s3_bucket_files(S3_NORMALIZED_FILE_URL_TEMPLATE.format(gid))
             if f.startswith('LHV')][0]
     kwargs['ti'].xcom_push(key='return_file', value=return_file)
 
