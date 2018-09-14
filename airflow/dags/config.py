c8d4429591f3e37d39a064725d2a1a8ec79ed6da
gzangakis <gzangakis@healthverity.com>
2018-09-14 14:35:53 -0400

    remove channel name from config

--- airflow/dags/config.py
+++ airflow/dags/config.py
@@ -2,7 +2,6 @@
 
 AIRFLOW_ALERTS_CHANNEL = '#airflow_alerts'
 PROVIDER_ALERTS_CHANNEL = '#provider_alerts'
-CONSENT_ALERTS_CHANNEL = '#consent-alerts'
 
 DECRYPTOR_JAR_LOCATION = 's3://healthverityreleases/vanderbilt/HVDecryptor.jar'
 DECRYPTOR_KEY_LOCATION = 's3://salusv/keys/hv_record_private.base64.reformat'
