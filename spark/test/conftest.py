142b265aab94b5b9850a98f70b08e5c96787691b
Ilia Fishbein <ifishbein@healthverity.com>
2018-09-20 14:02:09 -0400

    Log runtime duration of each test module

--- spark/test/conftest.py
+++ spark/test/conftest.py
@@ -28,3 +28,25 @@ def spark():
     }
 
     spark.stop()
+
+@pytest.fixture(scope='module', autouse=True)
+def testcase_result(request):
+    def fin():
+        print("Test module '{}' DURATION={}".format(request.node.nodeid, request.node.duration))
+    request.addfinalizer(fin)
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    # execute all other hooks to obtain the report object
+    outcome = yield
+    rep = outcome.get_result()
+
+    # set a report attribute for each phase of a call, which can
+    # be "setup", "call", "teardown"
+
+    tot_duration = 0
+    if hasattr(item.parent, 'duration'):
+        tot_duration = item.parent.duration + rep.duration
+    else:
+        tot_duration = rep.duration
+    setattr(item.parent, "duration", tot_duration)
