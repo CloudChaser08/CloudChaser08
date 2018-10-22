b3ce6dc8cbe57b3322906fea1c09656dc24d838b
Akim Akimov <aakimov@healthverity.com>
2018-10-22 19:27:23 -0400

    Group locations

--- spark/reference/icd10/sparkTransformToParquet.py
+++ spark/reference/icd10/sparkTransformToParquet.py
@@ -9,6 +9,11 @@ def run(spark, year):
     CM_INPUT = 's3://salusv/incoming/reference/icd10/cm/{}/'.format(year)
     CM_OUTPUT = 's3://salusv/reference/parquet/icd10/{}/cm/'.format(year)
 
+    PCS_GRP_INPUT = 's3://salusv/incoming/reference/icd10/pcs-cat/{}/'.format(year)
+    PCS_GRP_OUTPUT = 's3://salusv/reference/parquet/icd10/{}/pcs-cat/'.format(year)
+    CM_GRP_INPUT = 's3://salusv/incoming/reference/icd10/cm/{}/'.format(year)
+    CM_GRP_OUTPUT = 's3://salusv/reference/parquet/icd10/{}/cm/'.format(year)
+
     pcs = spark.read.text(PCS_INPUT)
     cm = spark.read.text(CM_INPUT)
     '''
