e37d0574f7aa02f01399d150c32e90f7f3effaee
Ilia Fishbein <ifishbein@healthverity.com>
2018-10-22 22:37:07 -0400

    Update Nextgen crosswalk generation script
    Update to generate data from the production matching runs

--- spark/providers/nextgen/crosswalk/sparkCreateCrosswalk.py
+++ spark/providers/nextgen/crosswalk/sparkCreateCrosswalk.py
@@ -1,32 +1,43 @@
 import argparse
+import pyspark.sql.functions as F
+import spark.helpers.payload_loader as payload_loader
 
 from spark.runner import Runner
 from spark.spark_setup import init
 from pyspark.sql import Window
-from pyspark.sql.functions import lit, col, concat_ws, input_file_name, udf, isnull, lead
-import spark.helpers.payload_loader as payload_loader
 
 def get_enterprise_id(filename):
-    return filename.split('/')[-1].split('_')[2]
+    filename = filename.replace('.xwalk', '')
+    filename = filename.replace('NG_HV', 'HV_LSSA')
+    return filename.split('/')[-1].split('.')[0].split('_')[2]
 
+def get_batch_date(filename):
+    filename = filename.replace('.xwalk', '')
+    filename = filename.replace('NG_HV', 'HV_LSSA')
+    return filename.split('/')[-1].split('.')[0].split('_')[3]
 
-def run(spark, runner, source):
-    payload_df = payload_loader.load(runner, source, ['hvJoinKey', 'matchScore'], return_output=True)
-    
-    enterprise_udf = udf(get_enterprise_id)
+def run(spark, runner):
+    SOURCE_1 = 's3://salusv/matching/payload/emr/nextgen/{2017/12,2018/01,2018/02,2018/03,2018/04,2018/05}/*/recurring/'
+    SOURCE_2 = 's3://salusv/matching/payload/emr/nextgen/2018/*/*/crosswalk/'
+    payload_df = payload_loader.load(runner, SOURCE_1, return_output=True, load_file_name=True)\
+        .union(payload_loader.load(runner, SOURCE_2, return_output=True, load_file_name=True))
+
+    enterprise_udf = F.udf(get_enterprise_id)
+    batch_date_udf = F.udf(get_batch_date)
 
     # We will use this window to make sure we select the HVID-NGID pair
     # that had the highest matchScore.
-    window = Window.orderBy('matchScore').partitionBy('nextgen_id')
+    window = Window.orderBy('batch_date').partitionBy('nextgen_id')
 
-    payload_df.select(['hvid', 'hvJoinKey', 'matchScore']) \
-              .withColumn('enterprise_id', enterprise_udf(input_file_name())) \
-              .withColumn('nextgen_id', concat_ws('_', lit('NG'), col('enterprise_id'), col('hvJoinKey'))) \
-              .select(['hvid', 'matchScore', 'nextgen_id']) \
-              .withColumn('next_match_score', lead('matchScore', 1).over(window)) \
-              .where(isnull('next_match_score')) \
-              .drop('next_match_score') \
-              .where(~isnull('hvid')) \
+    payload_df.select('hvid', 'hvJoinKey', 'input_file_name') \
+              .withColumn('enterprise_id', enterprise_udf(F.col('input_file_name'))) \
+              .withColumn('batch_date', batch_date_udf(F.col('input_file_name'))) \
+              .withColumn('nextgen_id', F.concat_ws('_', F.lit('118'), F.col('enterprise_id'), F.col('hvJoinKey'))) \
+              .select(['hvid', 'batch_date', 'nextgen_id']) \
+              .withColumn('next_date', F.lead('batch_date', 1).over(window)) \
+              .where(F.isnull('next_date')) \
+              .drop('next_date', 'batch_date') \
+              .where(~F.isnull('hvid')) \
               .createOrReplaceTempView('nextgen_crosswalk')
 
 
@@ -37,7 +48,7 @@ def main(args):
     # initialize runner
     runner = Runner(sqlContext)
 
-    run(spark, runner, args.source)
+    run(spark, runner)
 
     sqlContext.sql('select * from nextgen_crosswalk') \
               .repartition(1000) \
@@ -46,8 +57,6 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--source', type=str)
     parser.add_argument('--dest', type=str)
     args = parser.parse_args()
     main(args)
-
