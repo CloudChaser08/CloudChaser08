0b279cc8b0d0d93e7c16b77b837142ee689f006c
Joe Illuminati <illumijo@gmail.com>
2018-10-26 14:02:06 -0400

    preserve codes that are not included in new data

--- spark/reference/cpt/sparkCreateCPTParquet.py
+++ spark/reference/cpt/sparkCreateCPTParquet.py
@@ -2,6 +2,7 @@ import argparse
 import pyspark.sql.functions as F
 from spark.runner import Runner
 from spark.spark_setup import init
+import spark.helpers.external_table_loader as external_table_loader
 import spark.helpers.records_loader as records_loader
 import file_schemas as file_schemas
 
@@ -10,6 +11,7 @@ def run(spark, runner, year):
     CPT_OUTPUT = 's3://salusv/reference/parquet/cpt/{}/'.format(year)
 
     records_loader.load_and_clean_all_v2(runner, CPT_INPUT, file_schemas)
+    external_table_loader.load_cpt_codes(runner.sqlContext)
 
     cpt_long = spark.table('long')
     cpt_short = spark.table('short')
@@ -21,8 +23,10 @@ def run(spark, runner, year):
         1. Start with the long description and short description table.  Join them on the cpt code to
            get a base set of cpt_codes that contain: code, short_desc, long_desc
         2. Union the CPT-PLA codes to the base set created in step 1.
-        3. Union the Modifiers to get the final set of cpt_codes.  The Modifiers table only
+        3. Union the Modifiers to the cpt_code set.  The Modifiers table only
            has a long description column, so set the short_desc to NULL when performing the union
+        4. Take missing rows from current ref_cpt table and union them to get the final set
+           of cpt_codes
 
     NOTE: This logic IS subject to change year by year, so make sure to always verify with
           analytics that the logic remains the same based on the files we recived.
@@ -39,8 +43,16 @@ def run(spark, runner, year):
                        F.col('long_description')
                       )
     )
-    cpt_plus_modifiers.repartition(1).write.parquet(CPT_OUTPUT)
 
+    current_cpt_table = spark.table('cpt_codes')
+    missing_current_cpt_codes = current_cpt_table.join(cpt_plus_modifiers,
+                                                       current_cpt_table.code == cpt_plus_modifiers.code,
+                                                       'leftanti'
+                                                      )
+
+    cpt_final = cpt_plus_modifiers.union(missing_current_cpt_codes)
+
+    cpt_final.repartion(1).write.parquet(CPT_OUTPUT)
 
 def main(args):
     spark, sqlContext = init('Reference CPT')
