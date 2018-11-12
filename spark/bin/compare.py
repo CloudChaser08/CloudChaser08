c9f45479f3ef120331e33d4631ee2ecab4652f5b
Andrew McMullen <amcmullen@healthverity.com>
2018-11-12 14:44:54 -0500

    Respond to feedback

--- spark/bin/compare.py
+++ spark/bin/compare.py
@@ -1,4 +1,4 @@
-from pyspark.sql import SparkSession
+from spark.spark_setup import init
 from pyspark.sql.functions import row_number 
 from pyspark.sql.window import Window
 import argparse
@@ -6,106 +6,123 @@ import os
 
 """ Compare two datasets stored in s3. 
 
-This script loads source and target data sets, specified as an s3 path, into spark dataframes. 
-The steps include:
-    1. Compare schemas (column name, type)
-    2. Drop user specified columns 
-    3. Noramlize tables to have same schema
-    4. Compare row counts. 
-    5. Compare table contents. 
-
-This script is run as a spark-job via spark-submit. e.g.
-
-$ spark-submit compare.py path1 path2 --drop colA, colB, colC
+	:Example: 
+	$ spark-submit compare.py path1 path2 --drop colA, colB, colC
 """
 
-# Parse arguments. 
-parser = argparse.ArgumentParser()
-parser.add_argument("src_loc", type=str, help="source location")
-parser.add_argument("trg_loc", type=str, help="target location")
-parser.add_argument("--drop", type=str, nargs='+', help="the list of columns to drop from both tables")
-parser.add_argument("--local", type=bool, help="local mode")
-args = parser.parse_args()
+def compare_schemas(source, target): 
+    """ Compare schemas of two dataframes. 
+        
+        Check that the target schema is a subset of the source schema. 
+        We check both column names and types.
 
-src_loc = args.src_loc 
-trg_loc = args.trg_loc
-local = args.local or True
-drop = args.drop or []  
-
-# Initialize Spark Session.
-spark = SparkSession.builder \
-			.master("local[*]" if local else "yarn") \
-			.appName("Compare DataFrames") \
-			.getOrCreate()
+        :type source: pyspark.sql.DataFrame
+        :type target: pyspark.sql.DataFrame
+        :rtype: boolean  
+    """
+    for col in target.dtypes:
+        if col not in source.dtypes:
+            return False
+    else:
+        return True
 
 
-# Load source and target data.
-# TODO: add support for csv, psv, json, ect..
-source = spark.read.parquet(src_loc)
-target = spark.read.parquet(trg_loc)
+def normalize_schemas(source, target):
+    """ Normalize schemas of source and target dataframes. 
 
-# Compare schemas.
-#
-# We only need to verify that the target schema is a subset of the source schema. 
-# We check both column names and types. 
-#
-for col in target.dtypes:
-	if col not in source.dtypes:
-		print("Compare Schemas: FAIL")
-		os.exit(1)
-else:
-	print("Compare Schemas: OK")
+    Convert the source schema to match the target by dropping
+    any columns from the source that are not present in the target.
 
-# Drop user specified columns from both source and target.
-# 
-# Certain columns may never match, e.g. creation_date. so here we drop those columns
-# 
-for col in drop: 
-    source = source.drop(drop)
-    target = target.drop(drop)
-
-# Normalize source table.
-#
-# Drop columns from source that aren't in target, so we can compare. 
-# 
-for col in target.columns:
-    if col not in target.columns:
-        source = source.drop(col)
-
-# Compare row counts.
-source_row_count = source.count()
-target_row_count = target.count()
-print("Source row count: " + str(source_row_count))
-print("Target row count: " + str(target_row_count))
-if (source_row_count != target_row_count): 
-	print("Row Count Compare: FAIL")
-else: 
-	print("Row Count Compare: OK")
-
-# Compare duplicate counts.
-source_unique_row_count = source.distinct().count()
-target_unique_row_count = target.distinct().count()
-print("Source Duplicate Row Count: " + str(source_row_count - source_unique_row_count))
-print("Target Duplicate Row Count: " + str(target_row_count - target_unique_row_count))
-
-# Show percentage match.
-#
-# Since df.intersect(df2) only collects the distinct elements in common, first we add a row count to each
-# duplicate, so that way we include those duplicates. 
-#
-source = source.withColumn("row_count", row_number().over(Window.partitionBy(source.columns).orderBy(source.columns)))
-target = target.withColumn("row_count", row_number().over(Window.partitionBy(target.columns).orderBy(target.columns)))
-intersection = source.intersect(target)
-
-source_percent_match = intersection.count() / float(source.count())
-target_percent_match = intersection.count() / float(target.count())
-print("Source percent match: " + str(source_percent_match))
-print("Target percent match: " + str(target_percent_match))
-if source_percent_match != 1.0 and \
-   target_percent_match != 1.0:
-   print("Data Comparison: FAIL")
-else: 
-   print("Data Comparison: OK")
+    :type source: pyspark.sql.DataFrame
+    :type target: pyspark.sql.DataFrame
+    :rtype: pyspark.sql.DataFrame
+    """
+    for col in target.columns:
+        if col not in target.columns: 
+            source = source.drop(col)
+    return source
 
 
+def percent_match(source, target):
+    """ Calculate the percentage of rows that match.
 
+    Label rows with a row count for unique elements to preserve duplicates. 
+
+    :Example: 
+        Row('John', 'Smith', row_count=1)
+        Row('Jane', 'Doe',   row_count=1)
+        Row('Mike', 'Snow',  row_count=1)
+        Row('John', 'Smith', row_count=2)
+        Row('John', 'Smith', row_count=3)
+        Row('Jane', 'Doe',   row_count=2)
+    
+    Next, perform an intersection - collecting the unique elements in common to both source and target. 
+
+    :return: intersection.count() / source.count()
+    :return: intersection.count() / target.count()
+
+    :type source: pyspark.sql.DataFrame
+    :type target: pyspark.sql.DataFrame
+    :rtype: (float, float)
+    """
+    source = source.withColumn("row_count", row_number().over(Window.partitionBy(source.columns).orderBy(source.columns)))
+    target = target.withColumn("row_count", row_number().over(Window.partitionBy(target.columns).orderBy(target.columns)))
+    intersection = source.intersect(target)
+    source_percent_match = intersection.count() / float(source.count())
+    target_percent_match = intersection.count() / float(target.count())
+    return source_percent_match, target_percent_match
+
+
+if __name__ == '__main__':
+
+    # Parse arguments. 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("src_loc", type=str, help="source location")
+    parser.add_argument("trg_loc", type=str, help="target location")
+    parser.add_argument("--drop", type=str, nargs='+', help="the list of columns to drop from both tables")
+    args = parser.parse_args()
+
+    # initialize spark context.
+    spark, sqlContext = init("Comparison")
+
+    # load data into dataframes.
+    source = spark.read.parquet(args.src_loc)
+    target = spark.read.parquet(args.trg_loc)
+
+    # Drop columns
+    if args.drop: 
+	for col in args.drop: 
+    	    source = source.drop(drop)
+    	    target = target.drop(drop)
+    
+    # compare schemas.
+    schema_match = compare_schemas(source, target)
+    if schema_match:
+    	print("Schema Match: OK")
+    else: 
+    	print("Schema Match: FAIL")
+    	os.exit(-1)
+
+    # normalize source schema.
+    source = normalize_schemas(source, target)
+
+    # Compare row counts.
+    source_row_count = source.count()
+    target_row_count = target.count()
+    print("Source row count: " + str(source_row_count))
+    print("Target row count: " + str(target_row_count))
+
+    # compare duplicate counts.
+    source_unique_row_count = source.distinct().count()
+    target_unique_row_count = target.distinct().count()
+    print("Source Duplicate Row Count: " + str(source_row_count - source_unique_row_count))
+    print("Target Duplicate Row Count: " + str(target_row_count - target_unique_row_count))
+
+    # compare data content.
+    src_match, trg_match = percent_match(source, target)
+    print("Source Table Match: " + str(src_match))
+    print("Target Table Match: " + str(trg_match))
+    if (src_match != 1.0 or trg_match != 1.0):
+	print("Compare: FAIL")
+    else:
+	print("Compare: OK")
