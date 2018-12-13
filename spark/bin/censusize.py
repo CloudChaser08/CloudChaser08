5e7dc3ffef8ca14355781a69cdecc839c219ca78
Joe Illuminati <illumijo@gmail.com>
2018-12-13 17:08:34 -0500

    feat: Add support for custom salt to censusize.py

--- spark/bin/censusize.py
+++ spark/bin/censusize.py
@@ -6,7 +6,7 @@ import spark.common
 from datetime import datetime, date
 from spark.common.census_driver import CensusDriver
 
-def main(date, client_name=None, opportunity_id=None, census_module=None, end_to_end_test=False):
+def main(date, client_name=None, opportunity_id=None, salt=None, census_module=None, end_to_end_test=False):
     """
     Run standard census driver script or one from the provided census module
     """
@@ -24,7 +24,7 @@ def main(date, client_name=None, opportunity_id=None, census_module=None, end_to
         if not driver:
             raise AttributeError("Module {} does not contain a CensusDriver subclass".format(census_module))
     else:
-        driver = CensusDriver(client_name, opportunity_id)
+        driver = CensusDriver(client_name, opportunity_id, salt=salt)
 
 
     batch_date = datetime.strptime(date, '%Y-%m-%d').date()
@@ -39,14 +39,16 @@ if __name__ == "__main__":
     parser.add_argument('date', type=str, help="Date of census data batch")
     parser.add_argument('--client_name', type=str, default=None, help="Client name")
     parser.add_argument('--opportunity_id', type=str, default=None, help="Opportunity ID")
+    parser.add_argument('--salt', type=str, default=None, help="HVID obfuscation salt")
     parser.add_argument('--census_module', type=str, default=None, help="Census module name")
     parser.add_argument('--end_to_end_test', default=False, action='store_true')
     args = parser.parse_args()
 
     if not args.client_name and not args.opportunity_id and not args.census_module:
         print ("Client name and opportunity ID (standard Census) or census "
-                "module name is required")
+               "module name is required")
         parser.print_help()
         sys.exit(1)
 
-    main(args.date, args.client_name, args.opportunity_id, args.census_module, args.end_to_end_test)
+    main(args.date, args.client_name, args.opportunity_id,
+         args.salt, args.census_module, args.end_to_end_test)
