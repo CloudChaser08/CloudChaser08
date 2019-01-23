78d1df59da024405fc1267aef980cbdecd14ef5d
Joe Illuminati <illumijo@gmail.com>
2019-01-23 14:52:13 -0500

    fix: Correct error made when doing find-replace

--- spark/providers/visonex/clean_up_visonex_v2.sql
+++ spark/providers/visonex/clean_up_visonex_v2.sql
@@ -17,7 +17,7 @@ SELECT
     primarymodality,
     primarymodality_original,
     primarydialysissetting,
-    extract_date(substring(datefirstdialysis, 1, 10), '%Y-%m-%d') asfirstdialysis,
+    extract_date(substring(datefirstdialysis, 1, 10), '%Y-%m-%d') as datefirstdialysis,
     extract_date(substring(laststatuschangedate, 1, 10), '%Y-%m-%d') as laststatuschangedate,
     tribecode,
     extract_date(substring(inactivatedate, 1, 10), '%Y-%m-%d') as inactivatedate,
@@ -27,7 +27,7 @@ SELECT
     medicalcoveragemedicare,
     extract_date(substring(medicalcoveragemedicareeffectivedate, 1, 10), '%Y-%m-%d') as medicalcoveragemedicareeffectivedate,
     masterpatientidnumber,
-    extract_date(substring(datefirstdialysiscurrentunit, 1, 10), '%Y-%m-%d') asfirstdialysiscurrentunit
+    extract_date(substring(datefirstdialysiscurrentunit, 1, 10), '%Y-%m-%d') as datefirstdialysiscurrentunit
 FROM patientdata pd
 LEFT JOIN matching_payload ON analyticrowidnumber = claimId
 ;
