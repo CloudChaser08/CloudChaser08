690b6b330e8b6a56425313cf4a56f1ffeb1c1ecd
Andrew McMullen <amcmullen@healthverity.com>
2019-01-26 16:46:24 -0500

    Fix bug in mckesson_macro_hellix spark routine.
    In a recent commit, there was a import enum. However, this package
    is not supported in Python 2.7.13, which is the version used in the AWS
    EMR cluster instances.

--- spark/providers/mckesson_macro_helix/pharmacyclaims/load_transactions.py
+++ spark/providers/mckesson_macro_helix/pharmacyclaims/load_transactions.py
@@ -2,9 +2,8 @@ import spark.helpers.postprocessor as postprocessor
 import spark.helpers.records_loader as records_loader
 import schema_v1
 import schema_v2
-from enum import Enum
 
-class Schema(Enum):
+class Schema:
     v1 = schema_v1.TABLES
     v2 = schema_v2.TABLES
 
@@ -12,7 +11,7 @@ def load(runner, input_path_prefix, schema=Schema.v1):
     '''
     Load in the transactions to an in memory table.
     '''
-    for table, columns in schema.value.items():
+    for table, columns in schema.items():
         df = records_loader \
                 .load(runner, input_path_prefix, columns, 'csv', '|')
 
