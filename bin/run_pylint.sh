22d6b1ca73c57e44ba31b47483ff00d7bc87609b
Kyle Halpin <khalpin@healthverity.com>
2019-01-28 17:16:37 -0500

    Attempting to break pylint into distinct tasks to debug the run
    Pylint is giving different results in jenkins vs running by hand, by
    shooting the output to a file we can determine what exactly is
    different between the jenkins run, and the manual run.

--- bin/run_pylint.sh
+++ bin/run_pylint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # get the score
-pylint_score=$(find . -name '*.py' | xargs python3 -m pylint --rcfile=/etc/pylint/pylint.rc  | grep 'Your code' | sed -e 's/Your\ code\ has\ been\ rated\ at\ //' -e 's/\/.*//')
+find . -name '*.py' | xargs python3 -m pylint --rcfile=/etc/pylint/pylint.rc  | grep 'Your code' | sed -e 's/Your\ code\ has\ been\ rated\ at\ //' -e 's/\/.*//' > pylint_output.txt
+cat pylint_output.txt grep 'Your code' | sed -e 's/Your\ code\ has\ been\ rated\ at\ //' -e 's/\/.*//'
+pylint_score=$(cat pylint_output.txt grep 'Your code' | sed -e 's/Your\ code\ has\ been\ rated\ at\ //' -e 's/\/.*//')
+
+echo "Got pylint score of $pylint_score"
 
 # shoot it into datadog
 echo "pylint.score.dewey:${pylint_score}|g"| nc -v -q2 -u localhost 8125
