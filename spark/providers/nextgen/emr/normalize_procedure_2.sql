84b16992b5ed40aae4f879bcfc165b707e6ea6fd
Joe Illuminati <illumijo@gmail.com>
2019-04-04 18:09:51 -0400

    chore: Update to nextgen-v15

---
+++ spark/providers/nextgen/emr/normalize_procedure_2.sql
@@ -0,0 +1,67 @@
+SELECT
+    '09'                                    AS mdl_vrsn_num,
+    ord.dataset                             AS data_set_nm,
+    ord.reportingenterpriseid               AS vdr_org_id,
+    COALESCE(dem.hvid, concat_ws('_', '118',
+        ord.reportingenterpriseid,
+        ord.nextgengroupid))                AS hvid,
+    dem.birthyear                           AS ptnt_birth_yr,
+    CASE WHEN dem.gender = 'M' THEN 'M'
+        WHEN dem.gender = 'F' THEN 'F'
+        ELSE 'U' END                        AS ptnt_gender_cd,
+    dem.zip3                                AS ptnt_zip3_cd,
+    concat_ws('_', '35',
+        ord.reportingenterpriseid,
+        ord.encounter_id)                   AS hv_enc_id,
+    extract_date(
+        substring(ord.encounterdate, 1, 8), '%Y%m%d', CAST({min_date} AS DATE), CAST({max_date} AS DATE)
+        )                                   AS enc_dt,
+    extract_date(
+        substring(ord.orderdate, 1, 8), '%Y%m%d', CAST({min_date} AS DATE), CAST({max_date} AS DATE)
+        )                                   AS proc_dt,
+    ord.orderinghcpprimarytaxonomy          AS proc_rndrg_prov_nucc_taxnmy_cd,
+    ord.orderinghcpzipcode                  AS proc_rndrg_prov_zip_cd,
+    ARRAY(
+        ord.vcxcode,
+        ord.real_actcode
+    )[e.n]                                  AS proc_cd,
+    TRIM(UPPER(
+        ARRAY(
+            'CVX',
+            'ACTCODE'
+        )[e.n]
+    ))                                      AS proc_cd_qual,
+    ord.actdiagnosiscode                    AS proc_diag_cd,
+    TRIM(UPPER(ord.actstatus))              AS proc_stat_cd,
+    CASE
+        WHEN ord.actstatus IS NOT NULL AND LENGTH(TRIM(ord.actstatus)) <> 0 THEN 'ACTSTATUS'
+        ELSE NULL
+    END                                     AS proc_stat_cd_qual,
+    ord.actclass                            AS proc_typ_cd,
+    CASE
+        WHEN ord.actclass IS NOT NULL AND LENGTH(TRIM(ord.actclass)) <> 0 THEN 'ACTCLASS'
+        ELSE NULL
+    END                                     AS proc_typ_cd_qual,
+    'order'                                 AS prmy_src_tbl_nm
+FROM `procedure_order_real_actcode` ord
+    LEFT JOIN demographics_local dem 
+    CROSS JOIN procedure_2_exploder e
+    ON ord.ReportingEnterpriseID = dem.ReportingEnterpriseID
+        AND ord.NextGenGroupID = dem.NextGenGroupID
+        AND COALESCE(
+                substring(ord.encounterdate, 1, 8),
+                substring(ord.referencedatetime, 1, 8)
+            ) >= substring(dem.recorddate, 1, 8)
+        AND (COALESCE(
+                substring(ord.encounterdate, 1, 8),
+                substring(ord.referencedatetime, 1, 8)
+            ) < substring(dem.nextrecorddate, 1, 8)
+            OR dem.nextrecorddate IS NULL)
+    LEFT JOIN ref_gen_ref ref1 ON ref1.hvm_vdr_feed_id = 35
+        AND ref1.gen_ref_domn_nm = 'order.actmood'
+        AND ord.actmood = ref1.gen_ref_cd
+        AND ref1.whtlst_flg = 'Y'
+    LEFT JOIN ref_gen_ref ref2 ON ref2.hvm_vdr_feed_id = 35
+        AND ref2.gen_ref_domn_nm = 'order.actclass'
+        AND ord.actclass = ref2.gen_ref_cd
+        AND ref2.whtlst_flg = 'Y'
---
+++ spark/providers/nextgen/emr/normalize_procedure_2_prenorm.sql
@@ -0,0 +1,58 @@
+SELECT
+    ord.*,
+    CASE 
+        WHEN cpt_codes.code IS NOT NULL
+            THEN cpt_codes.code
+        WHEN hcpcs_codes.hcpc IS NOT NULL
+            THEN hcpcs_codes.hcpc
+        WHEN icd_diag_codes.code IS NOT NULL
+            THEN clean_up_diagnosis_code(
+                icd_diag_codes.code,
+                NULL,
+                COALESCE(ord.prov_ord_dt, ord.enc_dt)
+            )
+        WHEN icd_proc_codes.code IS NOT NULL
+            THEN icd_proc_codes.code
+        WHEN loinc_codes.loinc_num IS NOT NULL
+            THEN loinc_codes.loinc_num
+        WHEN regexp_extract(ord.clean_actcode, '(^NG[0-9]+$)') != ''
+            THEN ord.clean_actcode
+        WHEN ref3.gen_ref_cd IS NOT NULL
+            THEN ref3.gen_ref_cd
+        ELSE NULL END                               AS real_actcode
+FROM (SELECT *,
+             UPPER(actcode) as clean_actcode,
+             clean_up_freetext(actdiagnosiscode, false) as clean_actdiagnosiscode,
+             extract_date(
+                 substring(encounterdate, 1, 8), '%Y%m%d', CAST({min_date} AS DATE), CAST({max_date} AS DATE)
+                 ) as enc_dt,
+             extract_date(
+                 substring(orderdate, 1, 8), '%Y%m%d', CAST({min_date} AS DATE), CAST({max_date} AS DATE)
+                 ) as prov_ord_dt
+      FROM order) ord
+    -- NG is a very common value in this field and slows down transformation significantly
+    -- (It's 20x more common then the next most common value)
+    LEFT JOIN cpt_codes ON ord.clean_actcode = cpt_codes.code
+        AND ord.clean_actcode IS NOT NULL AND ord.clean_actcode != ''
+        AND ord.clean_actcode != 'NG'
+    LEFT JOIN hcpcs_codes ON ord.clean_actcode = hcpc
+        AND ord.clean_actcode IS NOT NULL AND ord.clean_actcode != ''
+        AND ord.clean_actcode != 'NG'
+    LEFT JOIN icd_diag_codes ON ord.clean_actcode = icd_diag_codes.code
+        AND ord.clean_actcode IS NOT NULL AND ord.clean_actcode != ''
+        AND ord.clean_actcode != 'NG'
+    LEFT JOIN icd_proc_codes ON ord.clean_actcode = icd_proc_codes.code
+        AND ord.clean_actcode IS NOT NULL AND ord.clean_actcode != ''
+        AND ord.clean_actcode != 'NG'
+    LEFT JOIN loinc_codes ON translate(ord.clean_actcode, '-', '') = loinc_codes.loinc_num
+        AND ord.clean_actcode IS NOT NULL AND ord.clean_actcode != ''
+        AND ord.clean_actcode != 'NG'
+    LEFT JOIN (SELECT DISTINCT gen_ref_cd
+            FROM ref_gen_ref
+            WHERE gen_ref_domn_nm = 'emr_prov_ord.prov_ord_cd'
+                AND whtlst_flg = 'Y'
+        ) ref3
+        ON ord.clean_actcode = ref3.gen_ref_cd
+            AND ord.clean_actcode IS NOT NULL AND ord.clean_actcode != ''
+    LEFT JOIN icd_diag_codes diag2 ON clean_up_freetext(ord.actdiagnosiscode, true) = diag2.code
+        AND ord.clean_actdiagnosiscode IS NOT NULL AND ord.clean_actdiagnosiscode != ''
