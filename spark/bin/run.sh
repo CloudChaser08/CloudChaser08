45e711f7eda93eff967822b08af5a0f533d9c634
Ilia Fishbein <ifishbein@healthverity.com>
2019-04-16 10:23:25 -0400

    Increase broadcast join threshold
    200MB allows far more things to be broadcast and improves performance

--- spark/bin/run.sh
+++ spark/bin/run.sh
@@ -35,4 +35,10 @@ prefix=$(echo $0 | sed 's/bin\/run.sh$/./')
 prefix=$(echo $prefix | sed 's/\/bin\/run.sh$//')
 prefix=$(echo $prefix | sed 's/.\/run.sh$/../')
 mem_cfg=$(get_optimal_memory_config)
-spark-submit --py-files $prefix/target/dewey.zip --jars $prefix/common/json-serde-1.3.7-jar-with-dependencies.jar,$prefix/common/HiveJDBC41.jar --conf spark.driver.memory=10G --conf spark.executor.cores=4 --conf spark.sql.shuffle.partitions=5000 --conf spark.default.parallelism=5000 --conf spark.file.useFetchCache=false --conf spark.hadoop.fs.s3a.connection.maximum=1000 $mem_cfg $@
+spark-submit --py-files $prefix/target/dewey.zip \
+    --jars $prefix/common/json-serde-1.3.7-jar-with-dependencies.jar,$prefix/common/HiveJDBC41.jar \
+    --conf spark.driver.memory=10G --conf spark.executor.cores=4 --conf spark.sql.shuffle.partitions=5000 \
+    --conf spark.default.parallelism=5000 --conf spark.file.useFetchCache=false \
+    --conf spark.hadoop.fs.s3a.connection.maximum=1000 \
+    --conf spark.sql.autoBroadcastJoinThreshold=209715200 \
+    $mem_cfg $@
