b7f855bf5218270dc774a332f2125ed42d8a7395
Jacob Griswold-Moran <jgriswold@healthverity.com>
2019-08-09 10:01:51 -0400

    adds stats provider data models
    Adds models for provider config using the `attrs` library, intended to
    help identify all fields on the provider config to make iteration on it
    more obvious

---
+++ spark/stats/models/__init__.py
@@ -0,0 +1,150 @@
+"""
+    Data models for stats generation
+"""
+# pylint: disable=too-few-public-methods
+import attr
+
+from .validators import is_bool, optional_instance_of, is_date_str
+
+from .converters import (
+    model_converter,
+    model_map_converter,
+    model_list_converter,
+)
+from .fields import (
+    create_required_str_field,
+    create_optional_str_field,
+    create_optional_bool_field,
+    create_model_field,
+    create_str_list_field
+)
+
+
+# Set of valid data types
+VALID_DATATYPES = {
+    'emr',
+    'emr_clin_obsn',
+    'emr_diag',
+    'emr_enc',
+    'emr_lab_test',
+    'emr_medctn',
+    'emr_proc',
+    'emr_prov_ord',
+    'events',
+    'labtests',
+    'medicalclaims',
+    'pharmacyclaims',
+}
+
+
+class _BaseModel(object):
+
+    def copy_with(self, **kwargs):
+        """ Copies the model instance, with additional args """
+        return attr.evolve(self, **kwargs)
+
+
+@attr.s(frozen=True)
+class Column(_BaseModel):
+    """ Column data """
+    name = create_required_str_field()
+    field_id = create_required_str_field()
+    sequence = create_required_str_field()
+
+
+@attr.s(frozen=True)
+class FillRateConfig(_BaseModel):
+    """ Fill rate configuration """
+    columns = attr.ib(converter=model_map_converter(Column))
+
+
+@attr.s(frozen=True)
+class TopValuesConfig(_BaseModel):
+    """ Fill rate configuration """
+    columns = attr.ib(converter=model_map_converter(Column))
+    max_values = attr.ib(
+        validator=attr.validators.instance_of(int),
+        default=10
+    )
+
+
+@attr.s(frozen=True)
+class LongitudinalityConfig(_BaseModel):
+    """ Longitudinality configuration """
+    patient_id_field = create_required_str_field()
+
+
+@attr.s(frozen=True)
+class YearOverYearConfig(_BaseModel):
+    """ Year-over-year configuration """
+    patient_id_field = create_required_str_field()
+
+
+@attr.s(frozen=True)
+class KeyStatsConfig(_BaseModel):
+    """ Year-over-year configuration """
+    patient_field = create_required_str_field()
+    record_field = create_required_str_field()
+
+
+@attr.s(frozen=True)
+class EPICalcsConfig(_BaseModel):
+    """ EMI Calcs configuration (currently empty) """
+
+    fields = create_str_list_field()
+
+
+@attr.s(frozen=True)
+class ProviderModel(_BaseModel):
+    """ A provider config model object """
+
+    # Required fields
+    datatype = attr.ib(validator=attr.validators.in_(VALID_DATATYPES))
+    row_field = create_required_str_field()
+
+    # Optional fields
+    date_fields = create_str_list_field()
+    fill_rate = create_optional_bool_field()
+    top_values = create_optional_bool_field()
+
+    fill_rate_conf = create_model_field(FillRateConfig)
+    top_values_conf = create_model_field(TopValuesConfig)
+
+
+@attr.s(frozen=True)
+class Provider(_BaseModel):
+    """ A provider config object """
+
+    # Required fields
+    name = create_required_str_field()
+    datafeed_id = attr.ib(validator=lambda _, __, value: int(value))
+    earliest_date = attr.ib(validator=is_date_str)
+    datatype = attr.ib(validator=attr.validators.in_(VALID_DATATYPES))
+
+    # Optional fields
+    date_fields = create_str_list_field()
+    record_field = create_optional_str_field()
+    fill_rate = create_optional_bool_field()
+    key_stats = create_optional_bool_field()
+    top_values = create_optional_bool_field()
+    longitudinality = create_optional_bool_field()
+    year_over_year = create_optional_bool_field()
+    epi_calcs = create_optional_bool_field()
+    index_all_dates = create_optional_bool_field()
+    index_null_dates = create_optional_bool_field()
+    models = attr.ib(
+        converter=model_list_converter(ProviderModel), default=None
+    )
+    custom_schema = create_optional_str_field()
+    custom_table = create_optional_str_field()
+
+    fill_rate_conf = create_model_field(FillRateConfig)
+    top_values_conf = create_model_field(TopValuesConfig)
+    epi_calcs_conf = create_model_field(EPICalcsConfig)
+
+    longitudinality_conf = create_model_field(LongitudinalityConfig)
+    longitudinality_conf_file = create_optional_str_field()
+    year_over_year_conf = create_model_field(YearOverYearConfig)
+    year_over_year_conf_file = create_optional_str_field()
+    key_stats_conf = create_model_field(KeyStatsConfig)
+    key_stats_conf_file = create_optional_str_field()
---
+++ spark/stats/models/converters.py
@@ -0,0 +1,58 @@
+"""
+    Converters for use in models
+"""
+def model_converter(model_cls, optional=True):
+    """
+        Generates a converter that casts the field value to an instance of
+        model_cls class.
+
+        Note that if the value is already an instance of the model_cls, no
+        conversion will occur.
+
+        If optional is set to True, then None values will also be allowed
+    """
+    def _converter(val):
+        if isinstance(val, model_cls) or (optional and val is None):
+            return val
+        if isinstance(val, dict):
+            return model_cls(**val)
+        raise ValueError(
+            '{} is neither an instance of {} or a dict'.format(val, model_cls)
+        )
+    return _converter
+
+
+def model_list_converter(model_cls):
+    """
+        Generates a converter that casts a list of values to a list of
+        model_cls instances.
+
+        Note that if the field value is None, it will be cast to an empty list
+    """
+    _model_converter = model_converter(model_cls)
+    def _converter(val):
+        if not val:
+            return []
+        if isinstance(val, list):
+            return [_model_converter(item) for item in val]
+        raise ValueError('Expecting a list of values')
+    return _converter
+
+
+def model_map_converter(model):
+    """
+        Generates a converter that casts a list of values to a mapping of
+        {<key>: <model_cls instance>} value pairs.
+
+        Note that if the field value is None, it will be cast to an empty dict
+    """
+    _model_converter = model_converter(model)
+    def _converter(val):
+        if not val:
+            return {}
+        if isinstance(val, dict):
+            return {
+                key: _model_converter(item) for key, item in val.iteritems()
+            }
+        raise ValueError('Expecting a mapping')
+    return _converter
