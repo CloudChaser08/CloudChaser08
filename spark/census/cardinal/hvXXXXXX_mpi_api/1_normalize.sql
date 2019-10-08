98d7bd8a10c36a40bb479bab31aec097fca9592c
Joe Illuminati <illumijo@gmail.com>
2019-10-08 14:51:37 -0400

    feat: Change message output of census-api
    In preparation for Census API R2 release, we are altering the message
    format to match what will be sent out of the result sender to the
    lambdas so that less will have to be changed on release

--- spark/census/cardinal/hvXXXXXX_mpi_api/1_normalize.sql
+++ spark/census/cardinal/hvXXXXXX_mpi_api/1_normalize.sql
@@ -1,10 +1,32 @@
 SELECT
-    slightly_obfuscate_hvid(cast(pay.hvid as integer), 'Cardinal_MPI-0')   as hvid,
-    pay.claimId                                                            as record_id,
-    txn.client_id                                                          as client_id,
-    txn.job_id                                                             as job_id,
-    txn.callback_data                                                      as callback,
-    pay.errors                                                             as errors
+    named_struct(
+        "census-api",
+        named_struct(
+            "client_id",
+            txn.client_id,
+            "job_id",
+            txn.job_id,
+            "stage",
+            {stage},
+            "callback",
+            txn.callback_data
+        )
+    )                                                   as message_body,
+    named_struct(
+        "payload",
+        named_struct(
+            "hvid",
+            pay.hvid,
+            "claimId",
+            pay.claimId,
+            "errors",
+            pay.errors
+        )
+    )                                                   as content,
+    named_struct(
+        "messageId",
+        txn.job_id
+    )                                                   as metadata
 FROM cardinal_mpi_api_transactions txn
 LEFT OUTER JOIN matching_payload pay
     ON txn.job_id = pay.hvJoinKey
