85a67c31679ec925e5cfb5183d49d14420822abe
Brian Malvey <bmalvey@healthverity.com>
2019-10-28 09:24:21 -0400

    Fix: Make Genoa Normalization run

--- spark/providers/genoa/pharmacyclaims/normalize.py
+++
@@ -1,9 +0,0 @@
-import spark.common.pharmacyclaims_common_model_v6 as pharmacyclaims_common_model
-import spark.helpers.schema_enforcer as schema_enforcer
-
-def run(runner):
-    norm_pharmacy = runner.run_spark_script('mapping.sql', return_output=True)
-
-    schema_enforcer.apply_schema(norm_pharmacy, pharmacyclaims_common_model.schema) \
-        .createOrReplaceTempView('pharmacyclaims_common_model')
-
