717e69aa77f85b365c389a4557839246ad507db9
Ilia Fishbein <ifishbein@healthverity.com>
2019-10-30 10:01:30 -0400

    Exclude Allscripts EMR data from Humana extracts
    At Veradigm's request, Allscripts data will no longer be sent to Humana

--- spark/delivery/humana_000468/11_synthetic_medicalclaims.sql
+++ spark/delivery/humana_000468/11_synthetic_medicalclaims.sql
@@ -1,11 +1,13 @@
-SELECT * FROM allscripts_med_claims_1
-UNION
-SELECT * FROM allscripts_med_claims_2
-UNION
-SELECT * FROM allscripts_med_claims_3
-UNION
-SELECT * FROM allscripts_med_claims_4
-UNION
+--2019-10-30 Allscripts asked to sending their EMR
+--data to Humana
+--SELECT * FROM allscripts_med_claims_1
+--UNION
+--SELECT * FROM allscripts_med_claims_2
+--UNION
+--SELECT * FROM allscripts_med_claims_3
+--UNION
+--SELECT * FROM allscripts_med_claims_4
+--UNION
 SELECT * FROM nextgen_med_claims_1
 UNION
 SELECT * FROM nextgen_med_claims_2
--- spark/delivery/humana_000468/12_synthetic_pharmacyclaims.sql
+++ spark/delivery/humana_000468/12_synthetic_pharmacyclaims.sql
@@ -1,3 +1,5 @@
-SELECT * FROM allscripts_pharma_claims_1
-UNION
+--2019-10-30 Allscripts asked to sending their EMR
+--data to Humana
+--SELECT * FROM allscripts_pharma_claims_1
+--UNION
 SELECT * FROM nextgen_pharma_claims_1
