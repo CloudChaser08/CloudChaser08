4be4657d0c26193254c4f37ff6bbdf151be2428c
Joe Illuminati <illumijo@gmail.com>
2019-11-04 14:07:28 -0500

    chore: Remove synthetic claims from humana extract

--- spark/delivery/humana_000468/11_synthetic_medicalclaims.sql
+++ spark/delivery/humana_000468/11_synthetic_medicalclaims.sql
@@ -1,4 +1,4 @@
---2019-10-30 Allscripts asked to sending their EMR
+--2019-10-30 Allscripts/NextGen asked to sending their EMR
 --data to Humana
 --SELECT * FROM allscripts_med_claims_1
 --UNION
@@ -8,10 +8,10 @@
 --UNION
 --SELECT * FROM allscripts_med_claims_4
 --UNION
-SELECT * FROM nextgen_med_claims_1
-UNION
-SELECT * FROM nextgen_med_claims_2
-UNION
-SELECT * FROM nextgen_med_claims_3
-UNION
-SELECT * FROM nextgen_med_claims_4
+--SELECT * FROM nextgen_med_claims_1
+--UNION
+--SELECT * FROM nextgen_med_claims_2
+--UNION
+--SELECT * FROM nextgen_med_claims_3
+--UNION
+--SELECT * FROM nextgen_med_claims_4
--- spark/delivery/humana_000468/12_synthetic_pharmacyclaims.sql
+++ spark/delivery/humana_000468/12_synthetic_pharmacyclaims.sql
@@ -1,5 +1,5 @@
---2019-10-30 Allscripts asked to sending their EMR
+--2019-10-30 Allscripts/NextGen asked to sending their EMR
 --data to Humana
 --SELECT * FROM allscripts_pharma_claims_1
 --UNION
-SELECT * FROM nextgen_pharma_claims_1
+--SELECT * FROM nextgen_pharma_claims_1
