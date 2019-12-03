aee18c519cf2b5ea32c694e1fa344c89a3fa4c24
Ilia Fishbein <ifishbein@healthverity.com>
2019-12-03 16:43:55 -0500

    Override new output file naming scheme
    For some old census feeds, use the old file naming scheme

--- spark/census/hc1/hv000710/driver.py
+++ spark/census/hc1/hv000710/driver.py
@@ -13,3 +13,5 @@ class Hc1CensusDriver(CensusDriver):
     def __init__(self, end_to_end_test=False):
         super(Hc1CensusDriver, self).__init__(self.CLIENT_NAME, self.OPPORTUNITY_ID,
                                               end_to_end_test=end_to_end_test)
+
+        self._output_file_name_template = '{batch_id_value}_response.gz'
