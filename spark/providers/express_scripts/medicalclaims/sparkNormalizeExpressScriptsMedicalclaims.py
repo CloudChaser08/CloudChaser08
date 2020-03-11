e13d63271c5b08e3c425e1b8f7413f09f3a6c06c
Jacob Bouffard <jbouffard@healthverity.com>
2020-03-11 13:23:51 -0400

    Fixed more typos

--- spark/providers/express_scripts/medicalclaims/sparkNormalizeExpressScriptsMedicalclaims.py
+++ spark/providers/express_scripts/medicalclaims/sparkNormalizeExpressScriptsMedicalclaims.py
@@ -84,17 +84,15 @@ if __name__ == "__main__":
             '--recursive'
         ]
 
-        ls_output = subprocess.check_output(s3_list_command)
+        ls_output = subprocess.check_output(s3_list_command).decode().strip().split('\n')
 
-        words = [line.split('/') for line in ls_output.split('\n') if '.json' in line]
+        words = [line.split('/') for line in ls_output if '.json' in line]
 
         all_dates = set(
             [datetime.strptime(word[4] + word[5] + word[6], '%Y%m%d') for word in words]
         )
 
-        input_date = datetime.strptime(driver.input_date, "%Y-%m-%d")
-
-        dates_less_than_input = [date for date in all_dates if date <= input_date]
+        dates_less_than_input = [date for date in all_dates if date.date() <= driver.date_input]
 
         max_date = max(dates_less_than_input)
         max_date_str = max_date.strftime('%Y/%m/%d/')
