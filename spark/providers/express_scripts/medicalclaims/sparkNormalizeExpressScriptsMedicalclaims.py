ef254556d7e04c3adcc38cc74e4101bc9fca30f5
Jacob Bouffard <jbouffard@healthverity.com>
2020-04-02 09:07:01 -0400

    Cleaned up the code further

--- spark/providers/express_scripts/medicalclaims/sparkNormalizeExpressScriptsMedicalclaims.py
+++ spark/providers/express_scripts/medicalclaims/sparkNormalizeExpressScriptsMedicalclaims.py
@@ -57,7 +57,6 @@ if __name__ == "__main__":
         vdr_feed_id=155,
         use_ref_gen_values=True
     )
-    driver.init_spark_context()
 
     def load_data():
         logger.log('Loading data:')
@@ -66,6 +65,8 @@ if __name__ == "__main__":
         # logger.log(' -Loading unmatched records')
         # driver.spark.read.parquet(S3_UNMATCHED_REFERENCE) \
         #     .createOrReplaceTempView('historic_unmatched_records')
+
+        num_buckets = int(driver.spark.sparkContext.getConf().get("spark.executor.instances"))
 
         logger.log(' -Loading ref_gen_ref table')
         external_table_loader.load_ref_gen_ref(driver.runner.sqlContext)
@@ -77,66 +78,33 @@ if __name__ == "__main__":
         driver.spark.table('transactions').withColumn('input_file_name', F.input_file_name())\
             .createOrReplaceTempView('txn')
 
-        #driver.spark.table('txn').write.mode('overwrite').format('orc').bucketBy(21, 'hvjoinkey').saveAsTable('txn_bucketed')
-
-        #driver.spark.table('txn_bucketed').createOrReplaceTempView('txn')
-
-        # BPM - This shouldn't need to happen - We should just pull in the Rx reference location
-        # logger.log('- Loading new PHI data')
-        # # new_phi is the latest RX Matching
-        #
-        # s3_list_command = [
-        #     'aws',
-        #     's3',
-        #     'ls',
-        #     S3_EXPRESS_SCRIPTS_RX_MATCHING.format(S3),
-        #     '--recursive'
-        # ]
-        #
-        # ls_output = subprocess.check_output(s3_list_command).decode().strip().split('\n')
-        #
-        # words = [line.split('/') for line in ls_output if '.json' in line]
-        #
-        # all_dates = set(
-        #     [datetime.strptime(word[4] + word[5] + word[6], '%Y%m%d') for word in words]
-        # )
-        #
-        # dates_less_than_input = [date for date in all_dates if date.date() <= driver.date_input]
-        #
-        # max_date = max(dates_less_than_input)
-        # max_date_str = max_date.strftime('%Y/%m/%d/')
-        #
-        # new_phi_path = S3_EXPRESS_SCRIPTS_RX_MATCHING.format(S3A) + max_date_str
-        # payload_loader.load(driver.runner, new_phi_path, ['hvJoinKey', 'patientId'])
-        # driver.runner.run_spark_query('ALTER TABLE matching_payload RENAME TO new_phi')
         logger.log('Load Rx payload reference location')
         driver.spark.read.parquet(S3_REF_PHI).createOrReplaceTempView('rx_payloads')
 
-        driver.spark.table('rx_payloads').write.mode('overwrite').format('orc').bucketBy(21, 'patient_id').saveAsTable('rx_bucketed')
+        driver\
+                .spark\
+                .table('rx_payloads')\
+                .write\
+                .mode('overwrite')\
+                .format('orc')\
+                .bucketBy(num_buckets, 'patient_id')\
+                .saveAsTable('rx_bucketed')
 
         driver.spark.table('rx_bucketed').createOrReplaceTempView('rx_payloads')
 
         logger.log('- Loading Dx matching_payload data')
-        # matching_payload is the associate input_date payload
-
-        # BPM - there shouldn't be a need to manually read in the Dx payload
-        # driver.runner.run_spark_script('sql_loaders/load_matching_payloads.sql',
-        #                                [['matching_path', S3_MATCHING_KEY]])
         payload_loader.load(driver.runner, driver.matching_path, load_file_name=True)
 
-        driver.spark.table('matching_payload').write.mode('overwrite').format('orc').bucketBy(21, 'patientid').saveAsTable('payload_bucketed')
+        driver\
+                .spark\
+                .table('matching_payload')\
+                .write\
+                .mode('overwrite')\
+                .format('orc')\
+                .bucketBy(num_buckets, 'patientid')\
+                .saveAsTable('payload_bucketed')
 
         driver.spark.table('payload_bucketed').createOrReplaceTempView('matching_payload')
-
-        # BPM - This doesn't need to happen because we're not bringing in the lates Rx payload
-        # BPM - local_phi won't exist anymore
-        # logger.log('Combining PHI data tables')
-        # # This reads in the reference location matching data and combines it with new Rx matching
-        # driver.runner.run_spark_script('sql_loaders/load_and_combine_phi.sql', [
-        #     ['local_phi_path', LOCAL_REF_PHI],
-        #     ['s3_phi_path', S3A_REF_PHI],
-        #     ['partitions', driver.spark.conf.get('spark.sql.shuffle.partitions'), False]
-        # ])
 
         logger.log('Done loading data')
 
@@ -148,15 +116,14 @@ if __name__ == "__main__":
         # save matched transactions to disk
         driver.save_to_disk()
 
-        '''
         # output the matched historic records.
         # Ensure that the original file_date is maintained on the parquet file
-        logger.log('Rename newly matched historic files:')
-        staging_dir = constants.hdfs_staging_dir + 'matched/'
-        partition_by = ['file_date', 'part_provider', 'part_best_date']
-        driver.spark.table('esi_final_matched_historic_dx').coalesce(20).write.parquet(
-            staging_dir, partitionBy=partition_by, compression='gzip', mode='append'
-        )
+        #logger.log('Rename newly matched historic files:')
+        #staging_dir = constants.hdfs_staging_dir + 'matched/'
+        #partition_by = ['file_date', 'part_provider', 'part_best_date']
+        #driver.spark.table('esi_final_matched_historic_dx').coalesce(20).write.parquet(
+            #staging_dir, partitionBy=partition_by, compression='gzip', mode='append'
+        #)
 
         def mk_move_file_preserve_file_date():
             mv_cmd = ['hadoop', 'fs', '-mv']
@@ -187,12 +154,11 @@ if __name__ == "__main__":
 
             return move_file
 
-        part_files_cmd = ['hadoop', 'fs', '-ls', '-R', staging_dir]
-        part_files = subprocess.check_output(part_files_cmd).decode().strip().split("\n")
-        driver.spark.sparkContext.parallelize(part_files).repartition(1000).foreach(
-            mk_move_file_preserve_file_date()
-        )
-        '''
+        #part_files_cmd = ['hadoop', 'fs', '-ls', '-R', staging_dir]
+        #part_files = subprocess.check_output(part_files_cmd).decode().strip().split("\n")
+        #driver.spark.sparkContext.parallelize(part_files).repartition(1000).foreach(
+            #mk_move_file_preserve_file_date()
+        #)
 
         logger.log('Save unmatched reference records to: /unmatched/')
         driver\
@@ -204,14 +170,12 @@ if __name__ == "__main__":
 
     def overwrite_reference_data():
         if not driver.end_to_end_test:
-            '''
-            logger.log('Deleting the PHI reference data from s3: ' + S3_REF_PHI)
-            subprocess.check_call(['aws', 's3', 'rm', '--recursive', S3_REF_PHI])
-            logger.log('Rewriting the updated PHI reference data to s3: ' + S3A_REF_PHI)
-            subprocess.check_call(
-                ['s3-dist-cp', '--s3ServerSideEncryption', '--src', LOCAL_REF_PHI, '--dest',
-                 S3A_REF_PHI])
-            '''
+            #logger.log('Deleting the PHI reference data from s3: ' + S3_REF_PHI)
+            #subprocess.check_call(['aws', 's3', 'rm', '--recursive', S3_REF_PHI])
+            #logger.log('Rewriting the updated PHI reference data to s3: ' + S3A_REF_PHI)
+            #subprocess.check_call(
+                #['s3-dist-cp', '--s3ServerSideEncryption', '--src', LOCAL_REF_PHI, '--dest',
+                 #S3A_REF_PHI])
 
             logger.log('Deleting the unmatched reference data from s3: ' + S3_UNMATCHED_REFERENCE)
             subprocess.check_call(['aws', 's3', 'rm', '--recursive', S3_UNMATCHED_REFERENCE])
@@ -219,6 +183,7 @@ if __name__ == "__main__":
             normalized_records_unloader.distcp(S3_UNMATCHED_REFERENCE, LOCAL_UNMATCHED)
 
     # Run the job
+    driver.init_spark_context()
     load_data()
     transform_data()
     save_to_disk()
