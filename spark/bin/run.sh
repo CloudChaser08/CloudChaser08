dafaebb2b1701516c04658bd1a65ce7ab582be8b
Ilia Fishbein <ifishbein@healthverity.com>
2020-08-10 18:21:40 -0400

    config: Increase memory allocated to driver
    The master node is now bigger (r5.2x) and can support more memory dedicated to the driver

--- spark/bin/run.sh
+++ spark/bin/run.sh
@@ -44,7 +44,7 @@ prefix=$(echo $prefix | sed 's/.\/run.sh$/../')
 mem_cfg=$(get_optimal_memory_config)
 spark-submit --py-files $prefix/target/dewey.zip \
     --jars $prefix/common/json-serde-1.3.7-jar-with-dependencies.jar,$prefix/common/HiveJDBC41.jar \
-    --conf spark.driver.memory=10G --conf spark.executor.cores=4 --conf spark.sql.shuffle.partitions=5000 \
+    --conf spark.driver.memory=30G --conf spark.executor.cores=4 --conf spark.sql.shuffle.partitions=5000 \
     --conf spark.default.parallelism=5000 --conf spark.file.useFetchCache=false \
     --conf spark.hadoop.fs.s3a.connection.maximum=1000 \
     --conf spark.sql.autoBroadcastJoinThreshold=209715200 \
