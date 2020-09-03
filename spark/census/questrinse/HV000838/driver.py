6c98549cc233cd3905c9f01fffb2659af42ff149
Ilia Fishbein <ifishbein@healthverity.com>
2020-09-03 10:54:05 -0400

    refactor: Refactor Quest RINSE driver script

--- spark/census/questrinse/HV000838/driver.py
+++ spark/census/questrinse/HV000838/driver.py
@@ -1,143 +1,76 @@
-import argparse
-import spark.providers.quest_rinse.labtests.transactional_schemas as source_table_schemas
-from spark.common.marketplace_driver import MarketplaceDriver
-from spark.common.lab_common_model import schemas as labtests_schemas
+from spark.common.census_driver import CensusDriver
 import spark.helpers.hdfs_tools as hdfs_utils
 import spark.helpers.file_utils as file_utils
 import re
-import spark.helpers.constants as constants
 import spark.helpers.external_table_loader as external_table_loader
-import spark.helpers.normalized_records_unloader as normalized_records_unloader
 import spark.common.utility.logger as logger
-from spark.common.utility.run_recorder import RunRecorder
-from pyspark.sql.functions import col
 
-_parquet_file_split_size = 1024 * 1024 * 1024
-_ref_loinc_schema = 'darch'
-_ref_loinc_table = 'hv_loinc_20191204'
+PARQUET_FILE_SIZE = 1024 * 1024 * 1024
+REF_LOINC_SCHEMA = 'darch'
+REF_LOINC_TABLE = 'hv_loinc_20191204'
 
-if __name__ == "__main__":
+class QuestRinseCensusDriver(CensusDriver):
+    def load(self, batch_date, batch_id, chunk_records_files=None):
+        super().load(batch_date, batch_id, chunk_records_files)
 
-    # ------------------------ Provider specific configuration -----------------------
-    provider_name = 'quest_rinse'
-    output_table_names_to_schemas = {
-        'labtest_quest_rinse_census_final': labtests_schemas['schema_v9']
-    }
-    provider_partition_name = 'quest_rinse'
+        logger.log('Loading external table: ref_geo_state')
+        external_table_loader.load_analytics_db_table(
+            self._sqlContext, 'dw', 'ref_geo_state', 'ref_geo_state'
+        )
+        self._spark.table('ref_geo_state').cache().createOrReplaceTempView('ref_geo_state')
+        self._spark.table('ref_geo_state').count()
 
-    # ------------------------ Common for all providers -----------------------
+        logger.log('Loading external table: {}.{}'.format(REF_LOINC_SCHEMA, REF_LOINC_TABLE))
+        external_table_loader.load_analytics_db_table(
+            self._sqlContext, REF_LOINC_SCHEMA, REF_LOINC_TABLE, 'loinc'
+        )
+        self._spark.table('loinc').cache().createOrReplaceTempView('loinc')
+        self._spark.table('loinc').count()
 
-    # Parse input arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--date', type=str)
-    parser.add_argument('--end_to_end_test', default=False, action='store_true')
-    args = parser.parse_args()
-    date_input = args.date
-    end_to_end_test = args.end_to_end_test
+        df = self._spark.table('order_result')
+        df = df.repartition(df.rdd.getNumPartitions())
+        df = df.cache_and_track('order_result')
+        df.createOrReplaceTempView('order_result')
+        df.count()
 
-    # Create and run driver
-    driver = MarketplaceDriver(
-        provider_name,
-        provider_partition_name,
-        source_table_schemas,
-        output_table_names_to_schemas,
-        date_input,
-        end_to_end_test,
-        vdr_feed_id=187,
-        use_ref_gen_values=True
-    )
+    def save(self, dataframe, batch_date, batch_id, chunk_idx=None, header=True):
+        # This data goes right to the provider. They want the data in parquet without
+        # column partitions.
+        logger.log('Saving data to the local file system')
+        dataframe.persist()
+        dataframe.count()
+        _batch_id_path, _batch_id_value = self._get_batch_info(batch_date, batch_id)
+        local_output_path = '/staging/{batch_id_path}/'.format(batch_id_path=_batch_id_path)
+        dataframe.repartition(100).write.parquet(local_output_path, compression='gzip', mode='overwrite')
 
-    # init
-    conf_parameters = {
-        'spark.default.parallelism': 1000,
-        'spark.sql.shuffle.partitions': 1000,
-        'spark.sql.autoBroadcastJoinThreshold': 10485760,
-        'spark.executor.memoryOverhead': 1024,
-        'spark.driver.memoryOverhead': 1024,
-        'spark.driver.extraJavaOptions': '-XX:+UseG1GC',
-        'spark.executor.extraJavaOptions': '-XX:+UseG1GC',
-        'spark.files.maxPartitionBytes': 3221225472,
-        'spark.reducer.maxBlocksInFlightPerAddress': 64,
-        'spark.reducer.maxReqsInFlight': 192,
-        'spark.shuffle.service.enabled': 'true',
-        'spark.shuffle.sasl.timeout': 60000,
-        'spark.network.timeout': '480s',
-        'spark.port.maxRetries': 64,
-        'spark.yarn.maxAppAttempts': 1,
-        'spark.task.maxFailures': 8,
-        'spark.max.executor.failures': 800
-    }
+        # Delivery requirement: max file size of 1GB
+        # Calculate number of partitions required to maintain a max file size of 1GB
+        repartition_cnt = int(round(hdfs_utils.get_hdfs_file_path_size(local_output_path) / PARQUET_FILE_SIZE))
+        logger.log('Repartition into {} partitions'.format(repartition_cnt))
 
-    driver.init_spark_context(conf_parameters=conf_parameters)
+        dataframe.repartition(repartition_cnt).write.parquet(local_output_path, compression='gzip', mode='overwrite')
 
-    # ------------------------ Load Reference Tables -----------------------
-    logger.log('Loading external table: ref_geo_state')
-    external_table_loader.load_analytics_db_table(
-        driver.sql_context, 'dw', 'ref_geo_state', 'ref_geo_state'
-    )
-    driver.spark.table('ref_geo_state').cache().createOrReplaceTempView('ref_geo_state')
+        logger.log("Renaming files")
+        output_file_name_template = 'Data_Set_{}_response_{{:05d}}.gz.parquet'.format(batch_id)
 
-    logger.log('Loading external table: {}.{}'.format(_ref_loinc_schema, _ref_loinc_table))
-    external_table_loader.load_analytics_db_table(
-        driver.sql_context, _ref_loinc_schema, _ref_loinc_table, 'loinc'
-    )
-    driver.spark.table('loinc').cache().createOrReplaceTempView('loinc')
-    # ------------------------ End Reference Tables Load -----------------------
+        for filename in [f for f in hdfs_utils.get_files_from_hdfs_path(local_output_path)
+                         if not f.startswith('.') and f != "_SUCCESS"]:
+            part_number = re.match('''part-([0-9]+)[.-].*''', filename).group(1)
+            new_name = output_file_name_template.format(int(part_number))
+            file_utils.rename_file_hdfs(local_output_path + filename, local_output_path + new_name)
 
-    driver.load()
-    driver.transform()
+        logger.log('Creating manifest file with counts')
+        manifest_file_name = 'Data_Set_{}_manifest.tsv'.format(batch_id)
+        file_utils.create_parquet_row_count_file(
+            self._spark, local_output_path,
+            self._output_path + '{batch_id_path}/'.format(batch_id_path=_batch_id_path),
+            manifest_file_name, True
+        )
 
-    # This data goes right to the provider. They want the data in parquet without
-    # column partitions.
-    logger.log('Saving data to the local file system')
-    output_table = driver.spark.table("labtest_quest_rinse_census_final")
-    delivery_date = date_input.replace('-', '')
-    delivery_path='s3://salusv/deliverable/questrinse/{}/'.format(delivery_date)
-    hdfs_output_path = 'hdfs:///staging/'
-    file_utils.clean_up_output_hdfs(hdfs_output_path)
+    def copy_to_s3(self, batch_date=None, batch_id=None):
+        super().copy_to_s3(batch_date, batch_id)
 
-    """
-    requirement to delivery with 1 GB (1024 * 1024 * 1024) per file
-    (split into 1GB files)
-    """
-    hdfs_output_temp_path = 'hdfs:///staging_temp/'
-    file_utils.clean_up_output_hdfs(hdfs_output_temp_path)
-    output_table.repartition(20).write.parquet(hdfs_output_temp_path, compression='gzip', mode='append')
-
-    # Collect total files size
-    repartition_cnt = int(round(hdfs_utils.get_hdfs_file_path_size(hdfs_output_temp_path) / _parquet_file_split_size))
-    driver.spark.sql("SET spark.default.parallelism={}".format(repartition_cnt))
-    driver.spark.sql("SET spark.shuffle.partitions={}".format(repartition_cnt))
-
-    logger.log('Repartition with given specific file size. New number of files {}'.format(str(repartition_cnt)))
-    driver.spark.read.parquet(hdfs_output_temp_path).createOrReplaceTempView('staging_temp_view')
-    output_table = driver.spark.sql('select * from staging_temp_view order by 1')
-
-    output_table.repartition(repartition_cnt).write.parquet(hdfs_output_path, compression='gzip', mode='append')
-
-    file_utils.clean_up_output_hdfs(hdfs_output_temp_path)
-
-    driver.log_run()
-
-    logger.log("Renaming files")
-    output_file_name_template = '{}_response_{{}}'.format(delivery_date)
-
-    for filename in [f for f in hdfs_utils.get_files_from_hdfs_path(hdfs_output_path)
-                     if f[0] != '.' and f != "_SUCCESS"]:
-        part_number = re.match('''part-([0-9]+)[.-].*''', filename).group(1)
-        new_name = output_file_name_template.format(str(part_number).zfill(5)) + '.gz.parquet'
-        file_utils.rename_file_hdfs(hdfs_output_path + filename, hdfs_output_path + new_name)
-
-    # Re-initialize spark in order to provide parquet row counts in manifest file
-    logger.log('Creating manifest file with counts')
-    manifest_file_name = '{delivery_date}_manifest.tsv'.format(delivery_date=delivery_date)
-    file_utils.create_parquet_row_count_file(driver.spark, '/staging/', delivery_path, manifest_file_name, True)
-    driver.stop_spark()
-
-    driver.copy_to_output_path(output_location=delivery_path)
-
-    # Quest doesn't want to see the _SUCCESS file that spark prints out
-    logger.log('Deleting _SUCCESS file')
-    file_utils.delete_success_file(delivery_path)
-
-    logger.log('Done')
+        # Quest doesn't want to see the _SUCCESS file that spark prints out
+        logger.log('Deleting _SUCCESS file')
+        _batch_id_path, _batch_id_value = self._get_batch_info(batch_date, batch_id)
+        file_utils.delete_success_file(self._output_path + '{batch_id_path}/'.format(batch_id_path=_batch_id_path))
