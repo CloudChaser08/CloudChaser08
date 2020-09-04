4e8f22a1d5da67d91031a3f6605444640a9e24b8
Ilia Fishbein <ifishbein@healthverity.com>
2020-09-04 17:08:41 -0400

    refactor: Add sorting of diagnosis codes
    This will make the field value consistent between runs

--- spark/census/questrinse/HV000838/1_labtest_quest_rinse_census_pre_final_02.sql
+++ spark/census/questrinse/HV000838/1_labtest_quest_rinse_census_pre_final_02.sql
@@ -2,12 +2,12 @@ SELECT
     unique_accession_id     ,
     CASE
         WHEN CONCAT_WS(' | ', COLLECT_SET(s_diag_code_codeset_ind)) = '' THEN NULL
-    ELSE CONCAT_WS(' | ', COLLECT_SET(s_diag_code_codeset_ind))
+    ELSE CONCAT_WS(' | ', SORT_ARRAY(COLLECT_SET(s_diag_code_codeset_ind)))
     END as s_diag_code_codeset_ind,
 
     CASE
         WHEN CONCAT_WS(' | ', COLLECT_SET(HV_ONE_diagnosis_code)) = '' THEN NULL
-    ELSE CONCAT_WS(' | ', COLLECT_SET(HV_ONE_diagnosis_code))
+    ELSE CONCAT_WS(' | ', SORT_ARRAY(COLLECT_SET(HV_ONE_diagnosis_code)))
     END as HV_s_diag_code_codeset_ind
 
 FROM labtest_quest_rinse_census_pre_final_01
