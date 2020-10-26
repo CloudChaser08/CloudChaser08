ab5e120676053423974ab456381f17432805c8a8
Jeremy Stewart <jstewart@healthverity.com>
2020-10-26 11:52:11 -0400

    Chore: Fixes pylint script Errant commands and piping cleaned up so this won't throw errors

--- bin/run_pylint.sh
+++ bin/run_pylint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# get the score
+# run pylint and get the score
 find . -name '*.py' | xargs python3 -m pylint --rcfile=/etc/pylint/pylint.rc  | grep 'Your code' | sed -e 's/Your\ code\ has\ been\ rated\ at\ //' -e 's/\/.*//' > pylint_output.txt
-cat pylint_output.txt grep 'Your code' | sed -e 's/Your\ code\ has\ been\ rated\ at\ //' -e 's/\/.*//'
-pylint_score=$(cat pylint_output.txt grep 'Your code' | sed -e 's/Your\ code\ has\ been\ rated\ at\ //' -e 's/\/.*//')
+pylint_score=$(cat pylint_output.txt)
 
 echo "Got pylint score of $pylint_score"
 
