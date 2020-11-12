7589811bde6276f3a873865db1e115ced75ed2c6
Nataline Rose Kolbe <nkolbe@healthverity.com>
2020-11-12 17:07:24 -0500

    group by all except batch_id

--- spark/datamart/covid19/labtests/sql/2_lab_collect_results2575.sql
+++ spark/datamart/covid19/labtests/sql/2_lab_collect_results2575.sql
@@ -40,10 +40,10 @@ SELECT
     ,abnormal_ind
     ,hipaa_comment
     ,fasting_indicator
-    ,FIRST(batch_id) as batch_id
+    ,MIN(batch_id) as batch_id
     ,CAST(mod(case when concat(accn_id,'_',dos_id) is null then 0.0 else substring(concat(accn_id,'_',dos_id),1,instr(concat(accn_id,'_',dos_id),'_')-1) end, CAST({nbr_of_buckets} AS INT)) AS INT) AS claim_bucket_id
-    ,FIRST({part_provider}) AS part_provider
-    ,FIRST(CONCAT(SUBSTR(dos,1,4),'-',SUBSTR(dos,5,2))) AS part_mth
+    ,{part_provider} AS part_provider
+    ,CONCAT(SUBSTR(dos,1,4),'-',SUBSTR(dos,5,2)) AS part_mth
 FROM
     aet2575.HVRequest_output_002575
 WHERE
@@ -55,4 +55,4 @@ GROUP BY -- Remove duplicates based on these columns from aet2575 staging source
     11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
     21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
     31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-    41, 43
+    41, 43, 44, 45
