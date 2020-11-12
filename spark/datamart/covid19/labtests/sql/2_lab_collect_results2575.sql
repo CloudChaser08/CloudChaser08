d7e8c9012b191695951286e56ae4070832c8a73d
Nataline Rose Kolbe <nkolbe@healthverity.com>
2020-11-12 15:23:31 -0500

    group by numbers

--- spark/datamart/covid19/labtests/sql/2_lab_collect_results2575.sql
+++ spark/datamart/covid19/labtests/sql/2_lab_collect_results2575.sql
@@ -51,45 +51,8 @@ WHERE
     AND UPPER(dos) <> 'DOS'
     AND CONCAT(SUBSTR(dos,1,4),'-',SUBSTR(dos,5,2))  IN ({list_of_part_mth})
 GROUP BY -- Remove duplicates based on these columns from aet2575 staging source
-    accn_id
-    ,dos
-    ,dos_id
-    ,claim_id
-    ,lab_code
-    ,qbs_payor_cd
-    ,insurance_billing_type
-    ,local_profile_code
-    ,standard_profile_code
-    ,profile_name
-    ,local_order_code
-    ,standard_order_code
-    ,order_name
-    ,hipaa_zip
-    ,hipaa_dob
-    ,hipaa_age
-    ,gender
-    ,acct_id
-    ,acct_name
-    ,acct_address_1
-    ,acct_address_2
-    ,acct_city
-    ,acct_state
-    ,acct_zip
-    ,phy_name
-    ,npi
-    ,market_type
-    ,specialty
-    ,diagnosis_code
-    ,icd_codeset_ind
-    ,loinc_code
-    ,local_result_code
-    ,result_name
-    ,result_value_a
-    ,units
-    ,ref_range_low
-    ,ref_range_high
-    ,ref_range_alpha
-    ,abnormal_ind
-    ,hipaa_comment
-    ,fasting_indicator
-    
+    1, 2,  3,  4,  5,  6,  7,  8,  9,  10,
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+    31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41
