52e95deed656788a7f4dd24dd54c513367648b0e
Nataline Rose Kolbe <nkolbe@healthverity.com>
2020-11-12 15:33:10 -0500

    ensuring distinct claim_bucket_id

--- spark/datamart/covid19/labtests/sql/2_lab_collect_results2575.sql
+++ spark/datamart/covid19/labtests/sql/2_lab_collect_results2575.sql
@@ -41,7 +41,7 @@ SELECT
     ,hipaa_comment
     ,fasting_indicator
     ,FIRST(batch_id) as batch_id
-    ,FIRST(CAST(mod(case when concat(accn_id,'_',dos_id) is null then 0.0 else substring(concat(accn_id,'_',dos_id),1,instr(concat(accn_id,'_',dos_id),'_')-1) end, CAST({nbr_of_buckets} AS INT)) AS INT)) AS claim_bucket_id
+    ,CAST(mod(case when concat(accn_id,'_',dos_id) is null then 0.0 else substring(concat(accn_id,'_',dos_id),1,instr(concat(accn_id,'_',dos_id),'_')-1) end, CAST({nbr_of_buckets} AS INT)) AS INT) AS claim_bucket_id
     ,FIRST({part_provider}) AS part_provider
     ,FIRST(CONCAT(SUBSTR(dos,1,4),'-',SUBSTR(dos,5,2))) AS part_mth
 FROM
@@ -55,4 +55,4 @@ GROUP BY -- Remove duplicates based on these columns from aet2575 staging source
     11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
     21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
     31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-    41
+    41, 43
