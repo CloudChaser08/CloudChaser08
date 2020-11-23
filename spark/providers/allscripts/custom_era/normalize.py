1688eb6fd3c8b36199de96bf3c2c948dc518ce6e
Jacob Bouffard <jbouffard@healthverity.com>
2020-11-23 16:34:05 -0500

    Added a new normalization script for Allscripts 835

---
+++ spark/providers/allscripts/custom_era/normalize.py
@@ -0,0 +1,57 @@
+import argparse
+import spark.providers.allscripts.custom_era.transactional_schemas as source_table_schemas
+from spark.common.marketplace_driver import MarketplaceDriver
+from spark.common.era.detail import schemas as detail_schemas
+from spark.common.era.summary import schemas as summary_schemas
+import spark.helpers.external_table_loader as external_table_loader
+import spark.common.utility.logger as logger
+
+if __name__ == "__main__":
+
+    # ------------------------ Provider specific configuration -----------------------
+    provider_name = 'allscripts'
+    output_table_names_to_schemas = {
+        'allscripts_era_detail': detail_schemas['schema_v5'],
+        'allscripts_era_summary': summary_schemas['schema_v5']
+    }
+    provider_partition_name = 'allscripts'
+
+    # ------------------------ Common for all providers -----------------------
+
+    # Parse input arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--date', type=str)
+    parser.add_argument('--end_to_end_test', default=False, action='store_true')
+    args = parser.parse_args()
+    date_input = args.date
+    end_to_end_test = args.end_to_end_test
+
+    # Create and run driver
+    driver = MarketplaceDriver(
+        provider_name,
+        provider_partition_name,
+        source_table_schemas,
+        output_table_names_to_schemas,
+        date_input,
+        end_to_end_test,
+        use_ref_gen_values=True,
+        vdr_feed_id=83
+    )
+
+    '''
+    conf_parameters = {
+        'spark.default.parallelism': 160,
+        'spark.sql.shuffle.partitions': 160,
+        'spark.sql.autoBroadcastJoinThreshold': 10485760,
+        'spark.executor.extraJavaOptions': '-XX:+UseG1GC'
+    }
+
+    driver.init_spark_context(conf_parameters=conf_parameters)
+
+    logger.log('Loading external table: ref_geo_state')
+    external_table_loader.load_analytics_db_table(
+        driver.sql_context, 'dw', 'ref_geo_state', 'ref_geo_state'
+    )
+    '''
+
+    driver.run()
