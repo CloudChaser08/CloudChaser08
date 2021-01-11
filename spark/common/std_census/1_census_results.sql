9312a5f20048ab0355792fc8ea58bff7d113ed18
Chris Keyser <ckeyser@healthverity.com>
2021-01-11 09:55:52 -0500

    fix styling issues in census results sql query

--- spark/common/std_census/1_census_results.sql
+++ spark/common/std_census/1_census_results.sql
@@ -1,15 +1,15 @@
 SELECT
     claimId                                                                 AS rowid,
     -- if matched using flexible matching AND matchStatus is multi_match, null out the HVID
-    (CASE flexibleMatchingUsed
+    CASE flexibleMatchingUsed
         WHEN TRUE THEN
-            (CASE matchStatus
-            WHEN 'multi_match' THEN
-                NULL
-            ELSE
-                obfuscate_hvid(hvid, CONCAT('hvid', {salt}))
-            END)
+            CASE matchStatus
+                WHEN 'multi_match' THEN
+                    NULL
+                ELSE
+                    obfuscate_hvid(hvid, CONCAT('hvid', {salt}))
+            END
         ELSE
             obfuscate_hvid(hvid, CONCAT('hvid', {salt}))
-    END)                                                                    AS hvid
+    END                                                                    AS hvid
 FROM matching_payload
