c204cbebbce0445462f98843870a8b2a2ffa108e
Brian Carlson <bcarlson@healthverity.com>
2021-02-11 14:41:36 -0500

    added back test file path

--- spark/providers/waystar/medicalclaims/normalize.py
+++ spark/providers/waystar/medicalclaims/normalize.py
@@ -55,10 +55,19 @@ def run(date_input, end_to_end_test=False, test=False, spark=None, runner=None):
     else:
         driver.spark = spark
         driver.runner = runner
+        
+        driver.input_path = file_utils.get_abs_path(
+            script_path, '../../../test/providers/waystar/medicalclaims/resources/input/'	
+        ) + '/'	
+        driver.matching_path = file_utils.get_abs_path(	
+            script_path, '../../../test/providers/waystar/medicalclaims/resources/matching/'	
+        ) + '/'
 
         augment_path = file_utils.get_abs_path(
             __file__, '../../../test/providers/waystar/medicalclaims/resources/augment/'
         ) + '/'
+
+
 
     augment_df = records_loader.load(
         driver.runner, augment_path, columns=['instanceid', 'accounttype'],
