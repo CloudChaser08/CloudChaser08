517816d1be0308307bb5508b91d947a117dfd7cd
Nataline Rose Kolbe <nkolbe@healthverity.com>
2021-02-16 10:11:54 -0500

    end of file newline

--- spark/helpers/s3_constants.py
+++ spark/helpers/s3_constants.py
@@ -1,11 +1,11 @@
 # Various s3 paths used for normalization output
 # If you need to use a new output path, consider adding it here instead of in a local variable
 
-E2E_OUTPUT_PATH = 's3://salusv/testing/dewey/airflow/e2e/'
 RECORDS_PATH = 's3://salusv/incoming/{data_type}/{provider_name}/{year}/{month:02d}/{day:02d}/'
 MATCHING_PATH = 's3://salusv/matching/payload/{data_type}/{provider_name}/{year}/{month:02d}/{day:02d}/'
 PRODUCTION_PATH = 's3://salusv/warehouse/parquet/'
 TRANSFORM_PATH = 's3://salusv/warehouse/transformed/'
 RESTRICTED_PATH = 's3://salusv/warehouse/restricted/'
 DATAMART_PATH = 's3://salusv/warehouse/datamart/'
+E2E_OUTPUT_PATH = 's3://salusv/testing/dewey/airflow/e2e/'
 E2E_DATAMART_PATH = 's3://salusv/testing/dewey/airflow/e2e/datamart/'
