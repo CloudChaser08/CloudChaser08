45288fb5c29750cfd68a3af6a02db7b3e194fe17
Nataline Rose Kolbe <nkolbe@healthverity.com>
2021-02-26 14:02:08 -0500

    accession id

--- spark/providers/labcorp_covid/labtests/transactional_schemas.py
+++ spark/providers/labcorp_covid/labtests/transactional_schemas.py
@@ -34,7 +34,7 @@ TABLE_CONF = {
             , 'npi'
             , 'specialty_code'
             , 'report_zip'
-            , 'accession_id'
+            , 'accession_id' # this field is not yet mapped
             , 'hvjoinkey'
         ]
     )
