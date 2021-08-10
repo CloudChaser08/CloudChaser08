4a993260a266f4efb5cfcb69081b477dee885ff6
Douglas H. King <douglasking215@gmail.com>
2021-08-10 17:20:05 -0400

    feat: adds columns and orders common rx token bridge model.

--- spark/common/datamart/dhc/custom/custom_common_model_rx_token_bridge_v1.py
+++ spark/common/datamart/dhc/custom/custom_common_model_rx_token_bridge_v1.py
@@ -13,16 +13,19 @@ schema = Schema(
     provider_partition_column='part_provider',
     date_partition_column='part_best_date',
     schema_structure=StructType([
+        StructField('claim_id', StringType(), True),
         StructField('concat_unique_fields', StringType(), True),
+        StructField('datavant_token1', StringType(), True),
+        StructField('datavant_token2', StringType(), True),
+        StructField('hvid', StringType(), True),
         StructField('date_service', DateType(), True),
         StructField('rx_number', StringType(), True),
         StructField('cob_count', StringType(), True),
         StructField('pharmacy_other_id', StringType(), True),
         StructField('response_code_vendor', StringType(), True),
         StructField('ndc_code', StringType(), True),
-        StructField('datavant_token1', StringType(), True),
-        StructField('datavant_token2', StringType(), True),
-        StructField('hvid', StringType(), True)
+        StructField('created', DateType(), True),
+        StructField('data_vendor', StringType(), True)
     ])
 )
 
