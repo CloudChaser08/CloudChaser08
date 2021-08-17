cfc8a0fd5134db83d15f8c78ca60aad126f8d840
Sakthi Pitchaiah <spitchaiah@healthverity.com>
2021-08-17 09:53:37 -0400

    add: change-rx-dhc rx-tokenbridge

---
+++ spark/providers/change_rx_dhc/custom_mart/1_change_rx_norm_final.sql
@@ -0,0 +1,12 @@
+SELECT
+    norm.*,
+    'change' AS part_provider,
+    /* part_best_date */
+    CONCAT
+	            (
+                    SUBSTR('{VDR_FILE_DT}', 1, 4), '-',
+                    SUBSTR('{VDR_FILE_DT}', 6, 2), '-01'
+                ) AS part_best_date
+FROM
+    changhe_rx_norm norm
+
