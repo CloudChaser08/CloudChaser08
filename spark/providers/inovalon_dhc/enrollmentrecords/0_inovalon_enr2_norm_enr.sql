7a16328052b75b5df20d94896fbe429965b46b99
Sakthi Pitchaiah <spitchaiah@healthverity.com>
2021-09-08 10:45:02 -0400

    add inovalon dhc enrollmentrecords

---
+++ spark/providers/inovalon_dhc/enrollmentrecords/0_inovalon_enr2_norm_enr.sql
@@ -0,0 +1,123 @@
+SELECT DISTINCT
+    enr.memberuid,
+    enr.effectivedate                                                                   AS orig_effectivedate,
+    enr.terminationdate                                                                 AS orig_terminationdate,
+    --------------------------------------------------------------------------------------------------------------
+    -----------------------------------New Logic introduced (JKS - 2021-08-17)------------------------------------
+    --------------------------------------------------------------------------------------------------------------
+    CASE
+        WHEN enr.effectivedate >= enr.claimadjustedeffectivedate   AND enr.effectivedate                >= enr.rxclaimadjustedeffectivedate THEN enr.effectivedate
+        WHEN enr.claimadjustedeffectivedate >= enr.effectivedate   AND enr.claimadjustedeffectivedate   >= enr.rxclaimadjustedeffectivedate THEN enr.claimadjustedeffectivedate
+        WHEN enr.rxclaimadjustedeffectivedate >= enr.effectivedate AND enr.rxclaimadjustedeffectivedate >= enr.claimadjustedeffectivedate   THEN enr.rxclaimadjustedeffectivedate
+        ELSE NULL
+    END                                                                                  AS effectivedate,
+
+    CASE
+        WHEN enr.terminationdate <= claimadjustedterminationdate   AND enr.terminationdate                <= enr.rxclaimadjustedterminationdate THEN enr.terminationdate
+        WHEN enr.claimadjustedterminationdate <= terminationdate   AND enr.claimadjustedterminationdate   <= enr.rxclaimadjustedterminationdate THEN enr.claimadjustedterminationdate
+        WHEN enr.rxclaimadjustedterminationdate <= terminationdate AND enr.rxclaimadjustedterminationdate <= enr.terminationdate                THEN enr.rxclaimadjustedterminationdate
+        ELSE NULL
+    END                                                                                  AS terminationdate,
+
+    /* effectivedate Below codes are removed after discussion with Will 2021-08-17*/
+    -- CASE
+    --     WHEN enr.effectivedate > DATE_ADD('{VDR_FILE_DT}', 365)  THEN NULL
+    --     WHEN enr.effectivedate < '{EARLIEST_SERVICE_DATE}' THEN NULL
+    --     ELSE enr.effectivedate
+    -- END                                                                                 AS effectivedate,
+    -- /* terminationdate */
+    -- CASE
+    --     WHEN enr.effectivedate > DATE_ADD('{VDR_FILE_DT}', 365)                THEN NULL
+    --     WHEN enr.effectivedate < '{EARLIEST_SERVICE_DATE}'               THEN NULL
+    --     WHEN enr.terminationdate > CONCAT(1 + YEAR('{VDR_FILE_DT}'), '-12-31') THEN CONCAT(1 + YEAR('{VDR_FILE_DT}'), '-12-31')
+    --     ELSE enr.terminationdate
+    -- END                                                                                 AS terminationdate,
+    enr.createddate,
+    enr.productcode,
+    enr.groupplantypecode,
+    enr.acaindicator,
+    enr.acaissuerstatecode,
+    enr.acaonexchangeindicator,
+    enr.acaactuarialvalue,
+    enr.payergroupcode,
+    enr.payertypecode,
+    enr.input_file_name,
+    mbr.statecode,
+    mbr.birthyear,
+    mbr.zip3value,
+    mbr.gendercode,
+    pay.age,
+    pay.state,
+    pay.hvid,
+    pay.yearofbirth,
+    pay.threedigitzip,
+    pay.gender,
+    'MEDICAL'                                                                           AS benefit_type
+ FROM enr
+LEFT OUTER JOIN mbr ON enr.memberuid = mbr.memberuid
+LEFT OUTER JOIN matching_payload pay ON enr.memberuid = pay.claimid
+
+WHERE UPPER(COALESCE(enr.memberuid, 'X')) <> 'MEMBERUID'
+  AND COALESCE(medicalindicator, 'X') = '1'
+UNION ALL
+SELECT DISTINCT
+    enr.memberuid,
+    enr.effectivedate                                                                   AS orig_effectivedate,
+    enr.terminationdate                                                                 AS orig_terminationdate,
+    --------------------------------------------------------------------------------------------------------------
+    -----------------------------------New Logic introduced (JKS - 2021-08-17)------------------------------------
+    -------------------Latest date is the start date -------------------------------------------------------------
+    CASE
+        WHEN enr.effectivedate >= enr.claimadjustedeffectivedate   AND enr.effectivedate                >= enr.rxclaimadjustedeffectivedate THEN enr.effectivedate
+        WHEN enr.claimadjustedeffectivedate >= enr.effectivedate   AND enr.claimadjustedeffectivedate   >= enr.rxclaimadjustedeffectivedate THEN enr.claimadjustedeffectivedate
+        WHEN enr.rxclaimadjustedeffectivedate >= enr.effectivedate AND enr.rxclaimadjustedeffectivedate >= enr.claimadjustedeffectivedate   THEN enr.rxclaimadjustedeffectivedate
+        ELSE NULL
+    END                                                                                  AS effectivedate,
+    -------------------Earliest date is the terminate date -------------------------------------------------------
+    CASE
+        WHEN enr.terminationdate <= claimadjustedterminationdate   AND enr.terminationdate                <= enr.rxclaimadjustedterminationdate THEN enr.terminationdate
+        WHEN enr.claimadjustedterminationdate <= terminationdate   AND enr.claimadjustedterminationdate   <= enr.rxclaimadjustedterminationdate THEN enr.claimadjustedterminationdate
+        WHEN enr.rxclaimadjustedterminationdate <= terminationdate AND enr.rxclaimadjustedterminationdate <= enr.terminationdate                THEN enr.rxclaimadjustedterminationdate
+        ELSE NULL
+    END                                                                                  AS terminationdate,
+
+    /* effectivedate Below codes are removed after discussion with Will 2021-08-17*/
+    -- CASE
+    --     WHEN enr.effectivedate > DATE_ADD('{VDR_FILE_DT}', 365)  THEN NULL
+    --     WHEN enr.effectivedate < '{EARLIEST_SERVICE_DATE}' THEN NULL
+    --     ELSE enr.effectivedate
+    -- END                                                                                 AS effectivedate,
+    -- /* terminationdate */
+    -- CASE
+    --     WHEN enr.effectivedate > DATE_ADD('{VDR_FILE_DT}', 365)  THEN NULL
+    --     WHEN enr.effectivedate <'{EARLIEST_SERVICE_DATE}'  THEN NULL
+    --     WHEN enr.terminationdate > CONCAT(1 + YEAR('{VDR_FILE_DT}'), '-12-31') THEN CONCAT(1 + YEAR('{VDR_FILE_DT}'), '-12-31')
+    --     ELSE enr.terminationdate
+    -- END                                                                                 AS terminationdate,
+    enr.createddate,
+    enr.productcode,
+    enr.groupplantypecode,
+    enr.acaindicator,
+    enr.acaissuerstatecode,
+    enr.acaonexchangeindicator,
+    enr.acaactuarialvalue,
+    enr.payergroupcode,
+    enr.payertypecode,
+    enr.input_file_name,
+    mbr.statecode,
+    mbr.birthyear,
+    mbr.zip3value,
+    mbr.gendercode,
+    pay.age,
+    pay.state,
+    pay.hvid,
+    pay.yearofbirth,
+    pay.threedigitzip,
+    pay.gender,
+    'PHARMACY'                                                                          AS benefit_type
+FROM enr
+LEFT OUTER JOIN mbr ON enr.memberuid = mbr.memberuid
+LEFT OUTER JOIN matching_payload pay ON enr.memberuid = pay.claimid
+
+WHERE UPPER(COALESCE(enr.memberuid, 'X')) <> 'MEMBERUID'
+  AND COALESCE(rxindicator, 'X') = '1'
---
+++ spark/providers/inovalon_dhc/enrollmentrecords/3_inovalon_enr2_norm_final.sql
@@ -0,0 +1,4 @@
+SELECT
+    MONOTONICALLY_INCREASING_ID()  AS record_id,
+    *
+FROM inovalon_enr2_pre_norm_final_dedup txn
