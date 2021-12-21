b0004af95356e6fb37cb4caf73abd29a7870c006
Sakthi Pitchaiah <spitchaiah@healthverity.com>
2021-12-21 05:36:55 -0500

    fix: sql restate - veritas events

--- spark/providers/veritas/events/0_veritas_norm_event_1.sql
+++ spark/providers/veritas/events/0_veritas_norm_event_1.sql
@@ -3,21 +3,30 @@ SELECT
     payl.hvid                                                                               AS hvid,
     CURRENT_DATE()                                                                          AS created,
     '11'                                                                                    AS model_version,
-    --SPLIT(payl.input_file_name, '/')[SIZE(SPLIT(payl.input_file_name, '/')) - 1]            AS data_set,
-    'veritas'                                                                               AS data_set,
+--    SUBSTR(SPLIT(payl.input_file_name, '/')[SIZE(SPLIT(payl.input_file_name, '/')) - 1], 21)
+--                                                                                            AS data_set,
+    REPLACE(SPLIT(payl.input_file_name, '/')[SIZE(SPLIT(payl.input_file_name, '/')) - 1], 'veritas', '')
+                                                                                            AS data_set,
+
     '263'                                                                                   AS data_feed,
     '1861'                                                                                  AS data_vendor,
-    CAP_AGE
-    (
-        VALIDATE_AGE(
-             SUBSTR(CURRENT_DATE(), 1,4) - CAST(payl.yearofbirth AS INT),
-           CAST(
-                SUBSTR(EXTRACT_DATE(payl.deathMonth, 1, 10), '%Y-%m-%d' )
-                AS DATE),
-            SUBSTR(payl.yearofbirth, 1, 4)
-        )
-    )                                                                                       AS patient_age,
-    payl.yearofbirth                                                                        AS patient_year_of_birth,
+
+
+CAP_YEAR_OF_BIRTH  -- Cap year of birth 1927 if age is 85 and over
+      (0,
+    VALIDATE_AGE(0,
+       CAST(
+            SUBSTR(EXTRACT_DATE(payl.deathMonth, 1, 10), '%Y-%m-%d' )
+            AS DATE),
+        SUBSTR(payl.yearofbirth, 1, 4)
+    ),
+    CASE
+        WHEN SUBSTR(CURRENT_DATE(), 1,4) - CAST(payl.yearofbirth AS INT) >= 85 THEN 1927
+        ELSE payl.yearofbirth
+    END
+)                                                                                           AS patient_year_of_birth,
+
+--    payl.yearofbirth                                                                        AS patient_year_of_birth,
 	MASK_ZIP_CODE(SUBSTR(payl.threedigitzip, 1, 3))                                         AS patient_zip3,
     VALIDATE_STATE_CODE(UPPER(payl.state))                                                  AS patient_state,
     CASE
@@ -49,6 +58,7 @@ SELECT
 FROM    matching_payload payl
 WHERE   payl.claimid NOT IN ('traceabilitynbr')
 AND     VALIDATE_STATE_CODE(UPPER(payl.state)) IS NOT NULL
-AND     lower(cast(payl.multiMatch as string)) IN ('true')
+AND     payl.matchStatus <> "multiMatch"
+-- AND     payl.multiMatch IN ('true')
 -- AND     multiMatch = 'false'
 and     (hvid is null or not exists (select d.hvid from matching_payload d where d.hvid = payl.hvid group by d.hvid having count(1) > 1))
