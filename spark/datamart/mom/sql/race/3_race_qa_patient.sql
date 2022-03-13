181fc82ea9e0ec8cdb887f84a2f551dbc3498e7f
Sakthi Pitchaiah <spitchaiah@healthverity.com>
2022-03-13 00:42:11 -0500

    add: mom publish3c

--- spark/datamart/mom/sql/race/3_race_qa_patient.sql
+++
@@ -1,7 +0,0 @@
-----------------------------------------------------------
---Row count must match unique patient count
-----------------------------------------------------------
-select
-    count(*) as recs,
-    count(distinct adult_hvid) as adults
-from race
