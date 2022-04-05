84b84895e80d05ed3532768c9e9d2e9413291845
Christopher Wolfe <cwolfe@healthverity.com>
2022-04-05 15:37:38 -0400

    fix: cast hvid to integer in Cardinal Vandelay transform

--- spark/census/cardinalhealth/HV005269/1_census_results.sql
+++ spark/census/cardinalhealth/HV005269/1_census_results.sql
@@ -1,4 +1,4 @@
 SELECT
     claimId                                               AS rowid,
-    slightly_obfuscate_hvid(hvid, CONCAT('hvid', {salt})) AS hvid
+    slightly_obfuscate_hvid(cast(hvid as integer), CONCAT('hvid', {salt})) AS hvid
 FROM matching_payload
